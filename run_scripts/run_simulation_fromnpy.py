93d0e2fcb8b51c08a5847b66fa41502ae21a4921
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2019-06-14 09:45:48 +0200

    Few modifications to choose the output directory for run_simulation_fromnpy.py Modified the notebook SNSimulation.ipynb to account for recent changes in the metric class (X0_norm)

--- run_scripts/run_simulation_fromnpy.py
+++ run_scripts/run_simulation_fromnpy.py
@@ -12,29 +12,30 @@ import yaml
 import os
 
 
-def makeYaml(input_file,dbDir,dbName,nside,nproc,num):
+def makeYaml(input_file, dbDir, dbName, nside, nproc, num, outDir):
 
     with open(input_file, 'r') as file:
         filedata = file.read()
 
-    prodid = '{}_{}'.format(dbName,num)
-    fullDbName = '{}/{}.npy'.format(dbDir,dbName)
+    prodid = '{}_{}'.format(dbName, num)
+    fullDbName = '{}/{}.npy'.format(dbDir, dbName)
     filedata = filedata.replace('prodid', prodid)
     filedata = filedata.replace('fullDbName', fullDbName)
     filedata = filedata.replace('nnproc', str(nproc))
     filedata = filedata.replace('nnside', str(nside))
-    return yaml.load(filedata,Loader=yaml.FullLoader)
-    
+    filedata = filedata.replace('outputDir', outDir)
+    return yaml.load(filedata, Loader=yaml.FullLoader)
 
-def loop(healpixels,obsFocalPlane,dbDir,dbName,nside,x0_tab,j=0, output_q=None):
 
-    config = makeYaml('input/param_simulation_gen.yaml',dbDir,dbName,nside,1,j)
-    metric = SNMetric(config=config,x0_norm=x0_tab)
-    for io,healpixID in enumerate(healpixels):
+def loop(healpixels, obsFocalPlane, dbDir, dbName, outDir, nside, x0_tab, j=0, output_q=None):
+
+    config = makeYaml('input/param_simulation_gen.yaml',
+                      dbDir, dbName, nside, 1, j, outDir)
+    metric = SNMetric(config=config, x0_norm=x0_tab)
+    for io, healpixID in enumerate(healpixels):
         obsMatch = obsFocalPlane.matchFast(healpixID)
         metric.run(season(obsMatch))
-        if io > 50:
-            break
+
     if metric.save_status:
         metric.simu.Finish()
     """
@@ -52,37 +53,48 @@ def loop(healpixels,obsFocalPlane,dbDir,dbName,nside,x0_tab,j=0, output_q=None):
         return resfi
     """
 
+
 parser = OptionParser()
 
-parser.add_option("--dbName", type="str", default='alt_sched', help="db name [%default]")
+parser.add_option("--dbName", type="str", default='alt_sched',
+                  help="db name [%default]")
 parser.add_option("--dbDir", type="str", default='', help="db dir [%default]")
-parser.add_option("--nside", type="int", default=64, help="healpix nside [%default]")
-parser.add_option("--nproc", type="int", default='8', help="number of proc  [%default]")
+parser.add_option("--outDir", type="str", default='',
+                  help="output dir [%default]")
+parser.add_option("--nside", type="int", default=64,
+                  help="healpix nside [%default]")
+parser.add_option("--nproc", type="int", default='8',
+                  help="number of proc  [%default]")
 
 opts, args = parser.parse_args()
 
 print('Start processing...')
 
 
-#Load file
+# Load file
 
 dbDir = opts.dbDir
 if dbDir == '':
     dbDir = '/sps/lsst/cadence/LSST_SN_CADENCE/cadence_db'
+outDir = opts.outDir
+if outDir == '':
+    outDir = ' /sps/lsst/users/gris/Output_Simu_pipeline'
+
+
 dbName = opts.dbName
 
 nside = opts.nside
 seasons = -1
 nproc = opts.nproc
-outDir = '/sps/lsst/users/gris/MetricOutput'
 
 # List of (instance of) metrics to process
 metricList = []
 
-#Load the (config) yaml file
-config = makeYaml('input/param_simulation_gen.yaml',dbDir,dbName,nside,1,-1)
+# Load the (config) yaml file
+config = makeYaml('input/param_simulation_gen.yaml',
+                  dbDir, dbName, nside, 1, -1, outDir)
 
-#check whether X0_norm file exist or not (and generate it if necessary)
+# check whether X0_norm file exist or not (and generate it if necessary)
 absMag = config['SN parameters']['absmag']
 salt2Dir = config['SN parameters']['salt2Dir']
 model = config['Simulator']['model']
@@ -92,31 +104,33 @@ x0normFile = 'reference_files/X0_norm_{}.npy'.format(absMag)
 
 if not os.path.isfile(x0normFile):
     from sn_tools.sn_utils import X0_norm
-    X0_norm(salt2Dir=salt2Dir,model=model, version=version, absmag=absMag,outfile=x0normFile)
+    X0_norm(salt2Dir=salt2Dir, model=model, version=version,
+            absmag=absMag, outfile=x0normFile)
 
 x0_tab = np.load(x0normFile)
 
-#metricList.append(SNMetric(config=config,x0_norm=x0_tab))
+# metricList.append(SNMetric(config=config,x0_norm=x0_tab))
 
-#loading observations
+# loading observations
 
 observations = np.load('{}/{}.npy'.format(dbDir, dbName))
 observations = renameFields(observations)
 
 # this is a "simple" tessalation using healpix
-pixels = pixelate(observations, nside,RaCol='fieldRA',DecCol='fieldDec')
+pixels = pixelate(observations, nside, RaCol='fieldRA', DecCol='fieldDec')
 
 # this is a more complicated tessalation using a LSST Focal Plane
-## Get the shape to identify overlapping obs
+# Get the shape to identify overlapping obs
 shape = GetShape(nside)
 scanzone = shape.shape()
-## 
-obsFocalPlane = ObsPixel(nside=nside,data=observations,scanzone=scanzone, RaCol='fieldRA',DecCol='fieldDec')
+##
+obsFocalPlane = ObsPixel(nside=nside, data=observations,
+                         scanzone=scanzone, RaCol='fieldRA', DecCol='fieldDec')
 
 #healpixels = np.unique(obsPixels['healpixID'])[:10]
 #res = loop(healpixels, obsFocalPlane, band, metricList)
-    
-#print(res)
+
+# print(res)
 
 timeref = time.time()
 
@@ -126,25 +140,26 @@ delta = npixels
 if nproc > 1:
     delta = int(delta/(nproc))
 
-tabpix = range(0,npixels,delta)
+tabpix = range(0, npixels, delta)
 if npixels not in tabpix:
-    tabpix = np.append(tabpix,npixels)
+    tabpix = np.append(tabpix, npixels)
 
 tabpix = tabpix.tolist()
 
-if tabpix[-1]-tabpix[-2]<= 100:
+if tabpix[-1]-tabpix[-2] <= 100:
     tabpix.remove(tabpix[-2])
 
-print(tabpix,len(tabpix))
+print(tabpix, len(tabpix))
 result_queue = multiprocessing.Queue()
 for j in range(len(tabpix)-1):
-#for j in range(5,6):
+    # for j in range(5,6):
     ida = tabpix[j]
     idb = tabpix[j+1]
-    p=multiprocessing.Process(name='Subprocess-'+str(j),target=loop,args=(healpixels[ida:idb],obsFocalPlane,dbDir,dbName,nside,x0_tab,j,result_queue))
+    p = multiprocessing.Process(name='Subprocess-'+str(j), target=loop, args=(
+        healpixels[ida:idb], obsFocalPlane, dbDir, dbName, outDir, nside, x0_tab, j, result_queue))
     p.start()
-    break
-    
+
+
 """
 resultdict = {}
 for i in range(len(tabpix)-1):
@@ -168,5 +183,4 @@ for key,vals in resultdict.items():
 for key, vals in restot.items():
     np.save('{}/{}_{}_{}.npy'.format(outDir,dbName,key,band),np.copy(vals))
 """
-print('Done',time.time()-timeref)
-
+print('Done', time.time()-timeref)
