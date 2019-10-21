ab7010f63605a85c040e28604662f256c63f4950
pgris <gris@clermont.in2p3.fr>
2019-10-21 08:41:51 +0200

    Code modified to account for the new method to scan the sky (gnomonic proj.)

--- run_scripts/simulation/run_simulation_fromnpy.py
+++ run_scripts/simulation/run_simulation_fromnpy.py
@@ -4,7 +4,7 @@ import numpy as np
 import healpy as hp
 #from metricWrapper import CadenceMetricWrapper, SNRMetricWrapper
 from sn_mafsim.sn_maf_simulation import SNMetric
-from sn_tools.sn_obs import renameFields, pixelate, season, GetShape, ObsPixel
+from sn_tools.sn_obs import renameFields, pixelate, season, GetShape, ObsPixel,ProcessArea
 from optparse import OptionParser
 import time
 import multiprocessing
@@ -12,6 +12,8 @@ import yaml
 import os
 from sn_tools.sn_utils import GetReference
 import pandas as pd
+from sn_tools.sn_obs import pavingSky,getFields
+import glob
 
 def selectDD(obs, nside):
 
@@ -48,12 +50,12 @@ def selectDD(obs, nside):
             return group_DD.to_records(index=False)
 
 
-def makeYaml(input_file, dbDir, dbName, nside, nproc, num, diffflux,seasnum,outDir,fieldtype,x1,color,zmin,zmax,simu):
+def makeYaml(input_file, dbDir, dbName, nside, nproc, diffflux,seasnum,outDir,fieldType,x1,color,zmin,zmax,simu):
 
     with open(input_file, 'r') as file:
         filedata = file.read()
 
-    prodid = '{}_{}_{}_seas_{}_{}_{}_{}'.format(simu,fieldtype,dbName, seasnum, num, x1,color)
+    prodid = '{}_{}_{}_seas_{}_{}_{}'.format(simu,fieldType,dbName, seasnum, x1,color)
     fullDbName = '{}/{}.npy'.format(dbDir, dbName)
     filedata = filedata.replace('prodid', prodid)
     filedata = filedata.replace('fullDbName', fullDbName)
@@ -62,24 +64,52 @@ def makeYaml(input_file, dbDir, dbName, nside, nproc, num, diffflux,seasnum,outD
     filedata = filedata.replace('outputDir', outDir)
     filedata = filedata.replace('diffflux', str(diffflux))
     filedata = filedata.replace('seasval', str(seasnum))
-    filedata = filedata.replace('ftype', fieldtype)
+    filedata = filedata.replace('ftype', fieldType)
     filedata = filedata.replace('x1val', str(x1))
     filedata = filedata.replace('colorval', str(color))
     filedata = filedata.replace('zmin', str(zmin))
     filedata = filedata.replace('zmax', str(zmax))
 
 
-    if fieldtype == 'DD':
+    if fieldType == 'DD':
         filedata = filedata.replace('fcoadd', 'True')
     else:
         filedata = filedata.replace('fcoadd', 'False') 
     return yaml.load(filedata, Loader=yaml.FullLoader)
 
 
-def loop(x1,color,zmin,zmax,healpixels, obsFocalPlane, dbDir, dbName, fieldtype, outDir, nside, diffflux,seasnum,x0_tab,reference_lc,simu,j=0, output_q=None):
+def loop_area(pointings,metricList, observations, nside, outDir,dbName,saveData,nodither,RaCol,DecCol,j=0, output_q=None):
 
-    config = makeYaml('input/param_simulation_gen.yaml',
-                      dbDir, dbName, nside, 8, j, diffflux, seasnum,outDir,fieldtype,x1,color,zmin,zmax,simu)
+    resfi = {}
+  
+    print('processing pointings',j,pointings)
+    
+    for metric in metricList:
+        resfi[metric.name] = None
+        listf = glob.glob('{}/*_{}_{}*'.format(outDir,metric.name,j))
+        if len(listf) > 0:
+            for val in listf:
+                os.system('rm {}'.format(val))
+
+    
+    time_ref = time.time()
+    
+    ipoint = 1
+    
+    myprocess = ProcessArea(nside,RaCol,DecCol,j,outDir,dbName,saveData)
+    for pointing in pointings:
+        ipoint += 1
+       
+        resdict = myprocess(observations, metricList, pointing['Ra'], pointing['Dec'], pointing['radius'], pointing['radius'],ipoint,nodither,display=False)
+        
+
+    print('end of processing for', j,time.time()-time_ref)
+
+
+def loop(x1,color,zmin,zmax,healpixels, obsFocalPlane, dbDir, dbName, fieldType, outDir, nside, diffflux,seasnum,x0_tab,reference_lc,simu,j=0, output_q=None):
+
+    config = makeYaml('input/simulation/param_simulation_gen.yaml',
+                      dbDir, dbName, nside, 8, diffflux, seasnum,outDir,fieldType,x1,color,zmin,zmax,simu)
     
     metric = SNMetric(config=config, x0_norm=x0_tab,reference_lc=reference_lc)
     for io, healpixID in enumerate(healpixels):
@@ -119,7 +149,7 @@ parser.add_option("--diffflux", type="int", default=0,
                   help="flag for diff flux[%default]")
 parser.add_option("--season", type="int", default=1,
                   help="season to process[%default]")
-parser.add_option("--fieldtype", type="str", default='DD', help="field - DD or WFD[%default]")
+parser.add_option("--fieldType", type="str", default='DD', help="field - DD or WFD[%default]")
 parser.add_option("--x1", type="float", default='0.0',
                   help="x1 SN [%default]")
 parser.add_option("--color", type="float", default='0.0',
@@ -128,6 +158,12 @@ parser.add_option("--zmin", type="float", default='0.0',
                   help="min redshift [%default]")
 parser.add_option("--zmax", type="float", default='1.0',
                   help="max redshift [%default]")
+parser.add_option("--saveData", type="int", default=1,
+                  help="to save data [%default]")
+parser.add_option("--nodither", type="int", default=0,
+                  help="to remove dithering [%default]")
+parser.add_option("--coadd", type="int", default=0,
+                  help="to coadd or not[%default]")
 
 opts, args = parser.parse_args()
 
@@ -151,20 +187,23 @@ seasons = -1
 nproc = opts.nproc
 diffflux = opts.diffflux
 seasnum = opts.season
-fieldtype = opts.fieldtype
+fieldType = opts.fieldType
 x1 = opts.x1
 color = opts.color
 zmin = opts.zmin
 zmax = opts.zmax
+saveData = opts.saveData
+nodither = opts.nodither
+coadd = opts.coadd
 
 simu = 'sncosmo'
 # List of (instance of) metrics to process
 metricList = []
 
 # Load the (config) yaml file
-config = makeYaml('input/param_simulation_gen.yaml',
-                  dbDir, dbName, nside, 1, -1, diffflux, 
-                  seasnum, outDir, fieldtype, x1, color,
+config = makeYaml('input/simulation/param_simulation_gen.yaml',
+                  dbDir, dbName, nside, 1,diffflux, 
+                  seasnum, outDir, fieldType, x1, color,
                   zmin,zmax,simu)
 
 # check whether X0_norm file exist or not (and generate it if necessary)
@@ -197,18 +236,59 @@ if 'sn_fast' in config['Simulator']['name']:
 observations = np.load('{}/{}.npy'.format(dbDir, dbName))
 observations = renameFields(observations)
 
-
+"""
 if fieldtype == 'DD':
     pixels = selectDD(observations,nside)
 else:
     pixels = pixelate(observations, nside, RaCol='fieldRA', DecCol='fieldDec')
+"""
+dictArea = {}
+radius = 5.
+RaCol = 'fieldRA'
+DecCol = 'fieldDec'
+if 'Ra' in observations.dtype.names:
+     RaCol = 'Ra'
+     DecCol = 'Dec'
 
-print('pixellisation done',type(pixels))
-# get the number of visits per pixels
-print(len(np.unique(pixels['healpixID'])))
+if fieldType == 'DD':
+    fieldIds = [290,744,1427, 2412, 2786]
+    observations = getFields(observations, fieldType, fieldIds,nside)
+    r = []
+        
+    r.append(('ELAIS_1', 744, 10.0, -45.52, radius))
+    r.append(('ELAIS_2', 744, 0.0, -45.52, radius))
+    r.append(('SPT', 290, 349.39, -63.32, radius))
+    r.append(('COSMOS', 2786, 150.36, 2.84, radius))
+    r.append(('XMM-LSS', 2412, 34.39, -5.09, radius))
+    r.append(('CDFS', 1427, 53.00, -27.44, radius))
+    areas = np.rec.fromrecords(
+        r, names=['name', 'fieldId', 'Ra', 'Dec', 'radius'])    
+ 
+
+else:
+    if fieldType == 'WFD':
+        observations = getFields(observations,'WFD')
+        minDec = decmin
+        maxDec = decmax
+        if decmin == -1.0 and decmax == -1.0:
+            #in that case min and max dec are given by obs strategy
+            minDec = np.min(observations['fieldDec'])-3.
+            maxDec = np.max(observations['fieldDec'])+3.
+        areas = pavingSky(ramin,ramax, minDec,maxDec, radius)
+        #areas = pavingSky(20., 40., -40., -30., radius)
+        print(observations.dtype)
+        
+    if fieldType == 'Fake':
+        #in that case: only one (Ra,Dec)
+        radius = 0.1
+        Ra = np.unique(observations[RaCol])[0]
+        Dec = np.unique(observations[DecCol])[0]
+        areas = pavingSky(Ra-radius/2.,Ra+radius/2.,Dec-radius/2.,Dec+radius/2.,radius)
+
+
 
 # remove pixels with a "too-high" E(B-V)
-
+"""
 if fieldtype != 'DD':
     idx = pixels['ebv']<= 0.25 
     pixels = pixels[idx]
@@ -220,6 +300,14 @@ scanzone = shape.shape()
 ##
 obsFocalPlane = ObsPixel(nside=nside, data=observations,
                          scanzone=scanzone, RaCol='fieldRA', DecCol='fieldDec')
+"""
+metricList = []
+
+config = makeYaml('input/simulation/param_simulation_gen.yaml',
+                  dbDir, dbName, nside, 8, diffflux, seasnum,outDir,fieldType,x1,color,zmin,zmax,simu)
+    
+print('hell',config)
+metricList.append(SNMetric(config=config, x0_norm=x0_tab,reference_lc=reference_lc,coadd=coadd))
 
 
 #healpixels = np.unique(obsPixels['healpixID'])[:10]
@@ -229,7 +317,7 @@ obsFocalPlane = ObsPixel(nside=nside, data=observations,
 
 timeref = time.time()
 
-healpixels = np.unique(pixels['healpixID'])
+healpixels = areas
 npixels = int(len(healpixels))
 print('number of pixels',npixels)
 delta = npixels
@@ -252,33 +340,12 @@ for j in range(len(tabpix)-1):
     # for j in range(5,6):
     ida = tabpix[j]
     idb = tabpix[j+1]
+    """
     p = multiprocessing.Process(name='Subprocess-'+str(j), target=loop, args=(x1,color,zmin,zmax,
         healpixels[ida:idb], obsFocalPlane, dbDir, dbName, fieldtype, outDir, nside, 
-        diffflux,seasnum,x0_tab,reference_lc,simu, j, result_queue))
+        diffflux,seasnum,x0_tab,reference_lc,simu, j, result_queue)) 
+    """
+    p = multiprocessing.Process(name='Subprocess-'+str(j), target=loop_area, args=(
+        healpixels[ida:idb],metricList, observations, nside,outDir,dbName, saveData,nodither,RaCol,DecCol,j, result_queue))
     p.start()
 
-
-"""
-resultdict = {}
-for i in range(len(tabpix)-1):
-    resultdict.update(result_queue.get())
-
-for p in multiprocessing.active_children():
-    p.join()
-
-restot = {}
-for metric in metricList:
-    restot[metric.name] = None
-
-for key,vals in resultdict.items():
-    for keyb in vals.keys():
-        if restot[keyb] is None:
-            restot[keyb] = vals[keyb]
-        else:
-            restot[keyb] = np.concatenate((restot[keyb],vals[keyb]))
-"""
-"""
-for key, vals in restot.items():
-    np.save('{}/{}_{}_{}.npy'.format(outDir,dbName,key,band),np.copy(vals))
-"""
-print('Done', time.time()-timeref)
