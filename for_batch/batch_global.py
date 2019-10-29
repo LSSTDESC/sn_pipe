ecbeb87106fc203bfbe6da296bb912b3c9f9dc01
pgris <gris@clermont.in2p3.fr>
2019-10-29 16:55:28 +0100

    adapt batch_global.py to include the possibility to process a db file generated by LSST scheduler

--- for_batch/batch_global.py
+++ for_batch/batch_global.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-def batch(dbDir,dbName,scriptref,nproc):
+def batch(dbDir,dbName,dbExtens,scriptref,nproc):
     cwd = os.getcwd()
     dirScript= cwd + "/scripts"
 
@@ -14,7 +14,7 @@ def batch(dbDir,dbName,scriptref,nproc):
     
     
     id='{}_global'.format(dbName)
-    name_id='metric_{}'.format(id)
+    name_id='metric_Global{}'.format(id)
     log = dirLog + '/'+name_id+'.log'
 
 
@@ -30,8 +30,9 @@ def batch(dbDir,dbName,scriptref,nproc):
     script.write(" echo 'sourcing setups' \n")
     script.write(" source setup_release.sh CCIN2P3\n")
     script.write("echo 'sourcing done' \n")
+    script.write(" source export.sh CCIN2P3\n")
 
-    cmd='python {}.py --dbDir {} --dbName {} --nproc {}'.format(scriptref,dbDir,dbName,nproc)
+    cmd='python {}.py --dbDir {} --dbName {} --dbExtens {} --nproc {}'.format(scriptref,dbDir,dbName,nproc,dbExtens)
     script.write(cmd+" \n")
     script.write("EOF" + "\n")
     script.close()
@@ -51,7 +52,6 @@ dbNames += ['baseline_1exp_nopairs_10yrs','baseline_1exp_pairsame_10yrs','baseli
           'simple_roll_mod3_mixed_10yrs','simple_roll_mod5_mixed_10yrs','twilight_1s10yrs',
           'altsched_1exp_pairsmix_10yrs','rotator_1exp_pairsmix_10yrs','hyak_baseline_1exp_nopairs_10yrs',
           'hyak_baseline_1exp_pairsame_10yrs']
-"""
 
 for dbName in dbNames:
     batch(dbDir,dbName,'run_scripts/run_global_metric',8)
@@ -62,3 +62,11 @@ dbNames = ['dec_1exp_pairsmix_10yrs']
 for dbName in dbNames:
     batch(dbDir,dbName,'run_scripts/run_global_metric',8)
 
+"""
+dbDir = '/sps/lsst/cadence/LSST_SN_PhG/cadence_db/fbs_1.3'
+dbNames = ['baseline_2snap_v1.3_10yrs','baseline_v1.3_10yrs','baseline_nomix_v1.3_10yrs','agnddf_illum60_v1.3_10yrs','agnddf_illum30_v1.3_10yrs','agnddf_illum10_v1.3_10yrs','agnddf_illum15_v1.3_10yrs','descddf_illum60_v1.3_10yrs','descddf_illum30_v1.3_10yrs','descddf_illum7_v1.3_10yrs','descddf_illum15_v1.3_10yrs','descddf_illum10_v1.3_10yrs','descddf_illum3_v1.3_10yrs','euclid_ddf_v1.3_10yrs','descddf_illum4_v1.3_10yrs','descddf_illum5_v1.3_10yrs','euclid_ddf_v1.3_10yrs']
+
+dbExtens = 'db'
+
+for dbName in dbNames:
+    batch(dbDir,dbName,dbExtens,'run_scripts/metrics/run_global_metric',8)
