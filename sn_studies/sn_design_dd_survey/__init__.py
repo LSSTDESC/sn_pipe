4b93fe53f02b560714ea980312e65a0d10055324
pgris <gris@clermont.in2p3.fr>
2019-12-20 16:51:31 +0100

    Remove sn_studies package that should not be here

--- sn_studies/sn_design_dd_survey/__init__.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-filtercolors = dict(zip('ugrizy',['b','c','g','y','r','m']))
-plt.rcParams['xtick.labelsize'] = 12
-plt.rcParams['ytick.labelsize'] = 12
-plt.rcParams['axes.labelsize'] = 12
