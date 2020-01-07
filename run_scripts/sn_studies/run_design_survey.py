4ebffc88e80033e2b759ef051b72c35937b88b11
pgris <gris@clermont.in2p3.fr>
2020-01-07 15:38:08 +0100

    Modified to account for new location of SN LC input file

--- run_scripts/sn_studies/run_design_survey.py
+++ run_scripts/sn_studies/run_design_survey.py
@@ -18,7 +18,7 @@ x1 = -2.0
 color = 0.2
 bands = 'rizy'
 
-theDir = 'MetricOutput/Fakes/NSN'
+theDir = 'input/sn_studies'
 fname = 'Fakes_NSNMetric_Fake_lc_nside_64_coadd_0_0.0_360.0_-1.0_-1.0_0.hdf5'
 
 
@@ -27,11 +27,10 @@ data = Data(theDir,fname,x1,color,blue_cutoff,red_cutoff,bands=bands)
 
 #few plots related to data
 
-
+"""
 data.plotzlim()
 data.plotFracFlux()
 data.plot_medm5()
-#plt.show()
 
 # this is to plot restframebands cutoff
 mybands = RestFrameBands(blue_cutoff=blue_cutoff,
@@ -39,6 +38,7 @@ mybands = RestFrameBands(blue_cutoff=blue_cutoff,
 print(mybands.zband)
 mybands.plot()
 plt.show()
+"""
 
 # Step 2: get the SNR requirements (minimal per band) with sigma_C<0.04
 #----------------------------------------------------------------------
@@ -63,7 +63,7 @@ snr_calc = SNR(SNRDir,data,SNR_par)
 
 #plot the results
 
-
+"""
 snrplot = SNR_plot('SNR_files',-2.0,0.2,2.0,380.,800.,3.)
 snrplot.plotSummary()
 snrplot.plotSummary_band()
@@ -72,7 +72,7 @@ for combi in ['fracflux_rizy', 'Nvisits_rizy', 'Nvisits_y_rizy']:
     snrplot.plotIndiv(combi,legy='Filter allocation')
 
 plt.show()
-
+"""
 
 #snr_calc.plot()
 #plt.show()
@@ -87,19 +87,22 @@ m5_type = 'median_m5_field_filter_season' # m5 values
 
 myvisits_seasons = Nvisits_cadence(snr_calc.SNR,cadence,m5_type,'Nvisits',bands)
 
-#myvisits_seasons.plot() #this is to plot the visits
+#this is to plot the variation of the number visits vs season
+"""
+myvisits_seasons.plot()
 
-#plt.show()
-
+plt.show()
+"""
 # load m5 reference file - here median per filter (over fields and seasons)
 m5_type='median_m5_filter'
 
 myvisits_ref = Nvisits_cadence(snr_calc.SNR,cadence,m5_type,'Nvisits',bands)
 
-#myvisits_ref.plot()
+"""
+myvisits_ref.plot()
 
-#plt.show()
-
+plt.show()
+"""
 #configName = 'DD_scen1.yaml'
 configName = 'DD_scen2.yaml'
 
