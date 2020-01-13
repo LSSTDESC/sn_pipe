0955dd6f856521cf5ef9fd994116828634ab5676
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-01-13 10:41:13 +0100

    code cleaning

--- run_scripts/sn_studies/run_design_survey.py
+++ run_scripts/sn_studies/run_design_survey.py
@@ -1,12 +1,11 @@
-from sn_design_dd_survey.wrapper import Data,Nvisits_cadence
+from sn_design_dd_survey.wrapper import Data, Nvisits_cadence
 from sn_design_dd_survey.budget import DD_Budget
-from sn_design_dd_survey.snr import SNR,SNR_plot
+from sn_design_dd_survey.snr import SNR, SNR_plot
 from sn_design_dd_survey.signal_bands import RestFrameBands
 from sn_design_dd_survey import plt
 
 import os
 import multiprocessing
-
 
 
 # Step 1: Load the data needed for analysis
@@ -22,10 +21,11 @@ theDir = 'input/sn_studies'
 fname = 'Fakes_NSNMetric_Fake_lc_nside_64_coadd_0_0.0_360.0_-1.0_-1.0_0.hdf5'
 m5file = 'medValues.npy'
 
-data = Data(theDir,fname,m5file,x1,color,blue_cutoff,red_cutoff,bands=bands)
+data = Data(theDir, fname, m5file, x1, color,
+            blue_cutoff, red_cutoff, bands=bands)
 
 
-#few plots related to data
+# few plots related to data
 
 """
 data.plotzlim()
@@ -41,7 +41,7 @@ plt.show()
 """
 
 # Step 2: get the SNR requirements (minimal per band) with sigma_C<0.04
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 # Create dir for SNR output
 # ---------------------------
@@ -50,18 +50,18 @@ if not os.path.isdir(SNRDir):
     os.makedirs(SNRDir)
 
 # criteria used for SNR choice
-# can be : 
+# can be :
 # Nvisits, Nvisits_y -> minimal number of visits (total) or in the y band
 # fracflux -> SNR distribution (per band) according to the flux distribution
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
 
 # choose one SNR distribution
-SNR_par = dict(zip(['max','step','choice'],[80.,2.,'Nvisits']))
+SNR_par = dict(zip(['max', 'step', 'choice'], [80., 2., 'Nvisits']))
 
-snr_calc = SNR(SNRDir,data,SNR_par)
+snr_calc = SNR(SNRDir, data, SNR_par)
 
-#plot the results
+# plot the results
 
 """
 snrplot = SNR_plot('SNR_files',-2.0,0.2,2.0,380.,800.,3.)
@@ -74,29 +74,31 @@ for combi in ['fracflux_rizy', 'Nvisits_rizy', 'Nvisits_y_rizy']:
 plt.show()
 """
 
-#snr_calc.plot()
-#plt.show()
+# snr_calc.plot()
+# plt.show()
 
 # Step 3: Estimate the DD-DESC budget
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
-cadence = 3. #define the cadence
+cadence = -1  # define the cadence -1 = all cadences
 
 # load m5 reference values - here: med value per field per filter per season
-m5_type = 'median_m5_field_filter_season' # m5 values
+m5_type = 'median_m5_field_filter_season'  # m5 values
 
-myvisits_seasons = Nvisits_cadence(snr_calc.SNR,cadence,theDir,m5file,m5_type,'Nvisits',bands)
+myvisits_seasons = Nvisits_cadence(
+    snr_calc.SNR, cadence, theDir, m5file, m5_type, 'Nvisits', bands)
 
-#this is to plot the variation of the number visits vs season
+# this is to plot the variation of the number visits vs season
 """
 myvisits_seasons.plot()
 
 plt.show()
 """
 # load m5 reference file - here median per filter (over fields and seasons)
-m5_type='median_m5_filter'
+m5_type = 'median_m5_filter'
 
-myvisits_ref = Nvisits_cadence(snr_calc.SNR,cadence,theDir,m5file,m5_type,'Nvisits',bands)
+myvisits_ref = Nvisits_cadence(
+    snr_calc.SNR, cadence, theDir, m5file, m5_type, 'Nvisits', bands)
 
 """
 myvisits_ref.plot()
@@ -106,9 +108,9 @@ plt.show()
 #configName = 'DD_scen1.yaml'
 configName = 'input/sn_studies/DD_scen2.yaml'
 
-mybud = DD_Budget(configName,myvisits_ref.nvisits_cadence,
-                      myvisits_seasons.nvisits_cadence,
-                      runtype='Nvisits_single')
-mybud.plot_budget(dd_value=0.06)
+mybud = DD_Budget(configName, myvisits_ref.nvisits_cadence,
+                  myvisits_seasons.nvisits_cadence,
+                  runtype='Nvisits_single')
+mybud.plot_budget(dd_value=0.06, fieldName='COSMOS', season=1)
 
 plt.show()
