cfbc2008f9ccf894765c0b22d981ade5d4212b89
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-01-17 17:47:29 +0100

    Update in the use of SNR_plot

--- run_scripts/sn_studies/run_design_survey.py
+++ run_scripts/sn_studies/run_design_survey.py
@@ -27,7 +27,7 @@ data = Data(theDir, fname, m5file, x1, color,
 
 # few plots related to data
 
-"""
+
 data.plotzlim()
 data.plotFracFlux()
 data.plot_medm5()
@@ -38,7 +38,7 @@ mybands = RestFrameBands(blue_cutoff=blue_cutoff,
 print(mybands.zband)
 mybands.plot()
 plt.show()
-"""
+
 
 # Step 2: get the SNR requirements (minimal per band) with sigma_C<0.04
 # ----------------------------------------------------------------------
@@ -63,13 +63,16 @@ snr_calc = SNR(SNRDir, data, SNR_par)
 
 # plot the results
 
+# load m5 reference values - here: med value per field per filter per season
+# m5_type = 'median_m5_field_filter_season'  # m5 values
 """
-snrplot = SNR_plot('SNR_files',-2.0,0.2,2.0,380.,800.,3.)
+snrplot = SNR_plot('SNR_files', -2.0, 0.2, 2.0, 380.,
+                   800., 3., theDir, m5file, 'median_m5_filter')
 snrplot.plotSummary()
 snrplot.plotSummary_band()
 for combi in ['fracflux_rizy', 'Nvisits_rizy', 'Nvisits_y_rizy']:
     snrplot.plotIndiv(combi)
-    snrplot.plotIndiv(combi,legy='Filter allocation')
+    snrplot.plotIndiv(combi, legy='Filter allocation')
 
 plt.show()
 """
@@ -84,7 +87,6 @@ cadence = -1  # define the cadence -1 = all cadences
 
 # load m5 reference values - here: med value per field per filter per season
 m5_type = 'median_m5_field_filter_season'  # m5 values
-
 myvisits_seasons = Nvisits_cadence(
     snr_calc.SNR, cadence, theDir, m5file, m5_type, 'Nvisits', bands)
 
@@ -106,17 +108,21 @@ myvisits_ref.plot()
 plt.show()
 """
 configName = 'input/sn_studies/DD_scen1.yaml'
-configName = 'input/sn_studies/DD_scen2.yaml'
-configName = 'input/sn_studies/DD_scen3.yaml'
+#configName = 'input/sn_studies/DD_scen2.yaml'
+#configName = 'input/sn_studies/DD_scen3.yaml'
 
 dd_budget = 0.06
 mybud = DD_Budget(configName, myvisits_ref.nvisits_cadence,
                   myvisits_seasons.nvisits_cadence,
                   runtype='Nvisits_single')
 
-mybud.plot_budget_zlim(dd_budget=dd_budget)
-mybud.plot_budget_visits(fieldName='COSMOS',season=1,dd_budget=dd_budget)
+mybud.plot_budget_zlim(dd_budget=-1)
+mybud.plot_budget_visits(fieldName='COSMOS', season=1, dd_budget=-1)
 
-#mybud.printVisits(dd_budget)
+mybud.plot_budget_zlim(dd_budget=dd_budget)
+mybud.plot_budget_visits(fieldName='COSMOS', season=1, dd_budget=dd_budget)
+
+
+# mybud.printVisits(dd_budget)
 
 plt.show()
