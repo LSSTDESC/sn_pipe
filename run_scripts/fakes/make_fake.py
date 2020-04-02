40bf90a15f32f2b809ed9f6ce4a690a84f71fa9b
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-04-02 11:45:03 +0200

    include some default parameters for the script (instead of hiding them)

--- run_scripts/fakes/make_fake.py
+++ run_scripts/fakes/make_fake.py
@@ -5,9 +5,9 @@ from optparse import OptionParser
 
 parser = OptionParser()
 
-parser.add_option("--config", type="str", default='',
+parser.add_option("--config", type="str", default='input/Fake_cadence/Fake_cadence_seqs.yaml',
                   help="config file name[%default]")
-parser.add_option("--output", type="str", default='',
+parser.add_option("--output", type="str", default='Fake_DESC',
                   help="output file name[%default]")
 parser.add_option("--seqs", type="int", default='0',
                   help="sequences for generations[%default]")
@@ -18,17 +18,12 @@ configName = opts.config
 outputName = opts.output
 seqs = opts.seqs
 
-if configName == '':
-    configName = 'input/Fake_cadence/Fake_cadence_seqs.yaml'
-if outputName == '':
-    outputName='Fake_DESC'
-
 config = yaml.load(open(configName), Loader=yaml.FullLoader)
 
 print(config)
-mygen = GenerateFakeObservations(config,sequences=seqs).Observations
+mygen = GenerateFakeObservations(config, sequences=seqs).Observations
 
 print(mygen)
 print(mygen.dtype)
 
-np.save('{}.npy'.format(outputName),np.copy(mygen))
+np.save('{}.npy'.format(outputName), np.copy(mygen))
