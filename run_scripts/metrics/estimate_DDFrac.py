4cdaa99285a6b09a669c7a98c6da1d3bb278e52c
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-04-03 11:07:58 +0200

    minor modif of the script estimate_DDFrac

--- run_scripts/metrics/estimate_DDFrac.py
+++ run_scripts/metrics/estimate_DDFrac.py
@@ -10,7 +10,7 @@ from sn_tools.sn_utils import MultiProc
 
 
 def func(proc, params):
-    
+
     n_cluster = 5
     dbName = proc['dbName'].decode()
     if 'euclid' in dbName:
@@ -20,9 +20,10 @@ def func(proc, params):
     dbExtens = params['dbExtens']
     fields = DDFields()
 
-    ana = AnaOS(dbDir, dbName, dbExtens, n_cluster,fields).stat
+    ana = AnaOS(dbDir, dbName, dbExtens, n_cluster, fields).stat
 
     return ana
+
 
 def Nvisits_night_test(df, cadence=3.,
                        season_length=6,
@@ -52,7 +53,6 @@ def Nvisits_night_test(df, cadence=3.,
 
     """
 
-   
     df['Nvisits_DD'] = frac_DD*(df['DD']+df['WFD'])
     df['Nvisits_DD'] /= season_length*nseasons*nfields*30./cadence
 
@@ -62,7 +62,7 @@ def Nvisits_night_test(df, cadence=3.,
 def plots(df, fields=['COSMOS']):
     """
     Function performing a set of plots
-    
+
     Parameters
     ----------------
     df: pandas df
@@ -74,12 +74,12 @@ def plots(df, fields=['COSMOS']):
 
     # sort the df by DD fraction
     df = df.sort_values(by=['frac_DD'])
-    
+
     # plot DD fraction
     plotDD(df, 'frac_DD', 'DD frac')
-    
+
     # loop on fields to plot filter allocation
-    
+
     for field in fields:
         for band in 'ugrizy':
             cola = '{}'.format(field)
@@ -88,16 +88,17 @@ def plots(df, fields=['COSMOS']):
             bandfrac = '{}_frac_{}'.format(field, band)
             dfb.loc[:, bandfrac] = dfb[colb]/dfb[cola]
             plotDD(dfb, bandfrac,
-                        '{} - {} band frac'.format(field, band))
-        for val in ['area','width_RA','width_Dec']:
-            plotDD(df,'{}_{}'.format(field, val),
-                        '{} - {}'.format(field, val))
+                   '{} - {} band frac'.format(field, band))
+        for val in ['area', 'width_RA', 'width_Dec']:
+            plotDD(df, '{}_{}'.format(field, val),
+                   '{} - {}'.format(field, val))
+
 
 def plotDD(df, what, leg):
     """
 
     Method to plot(barh)  some variables: cadence vs what
-    
+
     Parameters
     ----------------
     df: pandas df
@@ -114,7 +115,7 @@ def plotDD(df, what, leg):
 
     df = df.sort_values(by=what)
     ax.barh(df['cadence'], df[what])
-    
+
     xmin, xmax = ax.get_xlim()
     ax.set_xlim([0.0, xmax])
     ax.set_xlabel(leg, fontsize=fontsize)
@@ -124,12 +125,13 @@ def plotDD(df, what, leg):
     plt.grid(axis='x')
     plt.tight_layout()
 
+
 parser = OptionParser()
 parser.add_option("--dbList", type="str", default='List.txt',
                   help="db name [%default]")
 parser.add_option("--dbExtens", type="str", default='npy',
                   help="db extension [%default]")
-parser.add_option("--dbDir", type="str", default='',
+parser.add_option("--dbDir", type="str", default='/sps/lsst/cadence/LSST_SN_CADENCE/cadence_db',
                   help="db dir [%default]")
 
 opts, args = parser.parse_args()
@@ -137,9 +139,6 @@ opts, args = parser.parse_args()
 print('Start processing...')
 
 dbDir = opts.dbDir
-if dbDir == '':
-    dbDir = '/sps/lsst/cadence/LSST_SN_CADENCE/cadence_db'
-
 dbList = opts.dbList
 dbExtens = opts.dbExtens
 
@@ -148,19 +147,19 @@ outName = 'Nvisits.npy'
 if not os.path.isfile(outName):
     toprocess = np.genfromtxt(dbList, dtype=None, names=[
         'dbName', 'simuType', 'nside', 'coadd', 'fieldType', 'nproc'])
-    params = {} 
-    params['dbDir'] =dbDir
-    params['dbExtens'] =dbExtens
+    params = {}
+    params['dbDir'] = dbDir
+    params['dbExtens'] = dbExtens
 
-    data = MultiProc(toprocess,params,func,nproc=8).data
-    np.save(outName,data.to_records(index=False))
+    data = MultiProc(toprocess, params, func, nproc=8).data
+    np.save(outName, data.to_records(index=False))
 
 
 tab = pd.DataFrame(np.load(outName))
 
 # this is a check: number of visits per obs night with
 # cadence, season_length, nseasons and nfields chosen
-#Nvisits_night_test(tab)
+# Nvisits_night_test(tab)
 
 
 # Plots
