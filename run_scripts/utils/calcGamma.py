e37db55320eccfe6f30cf0e0aa3d41fba66901b6
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-04-17 08:29:44 +0200

    add a new script to estimate gamma values

---
+++ run_scripts/utils/calcGamma.py
@@ -0,0 +1,13 @@
+from sn_tools.sn_utils import Gamma
+from sn_tools.sn_telescope import Telescope
+import numpy as np
+
+bands = 'grizy'
+telescope = Telescope(airmass=1.2)
+
+outName = 'gamma_test.hdf5'
+mag_range = np.arange(15., 28., 1.)
+exptimes = np.arange(1., 9000., 10.)
+Gamma(bands, telescope, outName,
+      mag_range=mag_range,
+      exptimes=exptimes)
