a4372138ec639aa2e5259292ab0ad514eb60eb55
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-04-17 09:17:35 +0200

    add a script to reshuffle observing strategy data (ie modifying the number of visits per night) for DD

---
+++ run_scripts/utils/reshuffling.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+from optparse import OptionParser
+
+parser = OptionParser()
+
+parser.add_option('--dbName', type='str', default='flexddf_v1.4_10yrs_DD',
+                  help='db name [%default]')
+parser.add_option('--config', type='str', default='scen1',
+                  help='configuration - should correspond to a config.csv file [%default]')
+parser.add_option('--dirConfig', type='str',
+                  default='input/reshuffling',
+                  help="config files location [%default]")
+
+
+opts, args = parser.parse_args()
+dbName = opts.dbName
+config = opts.config
+dirConfig = opts.dirConfig
+
+# load datas
+tab = pd.DataFrame(
+    np.load('{}_with_fields.npy'.format(dbName), allow_pickle=True))
+
+# get the number of visits to be implemented
+nvisits = pd.read_csv('{}/{}.csv'.format(dirConfig, config)
+                      ).to_dict(orient='records')[0]
+
+# correct for the number of exposures and times
+tab['numExposures'] = tab.apply(lambda x: nvisits[x['filter']], axis=1)
+tab['visitExposureTime'] *= tab['numExposures']
+tab['visitTime'] *= tab['numExposures']
+
+# print(tab)
+
+# get medians
+med_night = tab.groupby(['fieldname', 'night',
+                         'filter', 'season']).median().reset_index()
+
+# m5 should also be modified
+med_night['fiveSigmaDepth'] += 1.25*np.log(med_night['numExposures'])
+
+# remove columns associated to DD fields
+todrop = ['fieldname', 'healpixID', 'pixRA',
+          'pixDec', 'ebv', 'RA', 'Dec', 'fieldnum']
+finaldf = med_night.drop(columns=todrop)
+
+print(finaldf.columns)
+
+print(finaldf[['visitExposureTime', 'numExposures',
+               'visitTime', 'fiveSigmaDepth']])
+
+# save in npy file
+
+np.save('{}_{}.npy'.format(dbName, config), finaldf.to_records(index=False))
