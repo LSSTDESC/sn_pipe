c2b462791a6a807457ee68f1589ea559d39b0f55
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-04-20 16:35:43 +0200

    extend mag range in gamma calc

--- run_scripts/utils/calcGamma.py
+++ run_scripts/utils/calcGamma.py
@@ -6,7 +6,7 @@ bands = 'grizy'
 telescope = Telescope(airmass=1.2)
 
 outName = 'gamma_test.hdf5'
-mag_range = np.arange(15., 28., 1.)
+mag_range = np.arange(15., 38., 1.)
 exptimes = np.arange(1., 9000., 10.)
 Gamma(bands, telescope, outName,
       mag_range=mag_range,
