fb38347fed0c9543c5d840c01941c1bde53ae330
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-05-11 11:59:23 +0200

    moved class to make fit to sn_fit_lc pack

--- run_scripts/fit_sn/run_sn_fit.py
+++ run_scripts/fit_sn/run_sn_fit.py
@@ -3,22 +3,40 @@ import argparse
 import time
 import h5py
 from astropy.table import Table, vstack
-from importlib import import_module
-from sn_tools.sn_telescope import Telescope
-import os
 import numpy as np
 import multiprocessing
 from optparse import OptionParser
+from sn_fit.sn_fit import Fitting
 from sn_fit.mbcov import MbCov
 import glob
 
 
-alpha = 0.14
-beta = 3.1
+def makeYaml(input_file, dbDir, prodid, outDir, nproc, covmb=0, display=0):
+    """
+    Function to replace generic parameters of a yaml file
 
+    Parameters
+    --------------
+    input_file: str
+       input yaml file to modify
+    dbDir: str
+       database directory location
+    prodid: str
+       production id
+    outDir: str
+      output directory - where to put the results
+    nproc: int
+      number of procs for multiprocessing
+    covmb: int, opt
+      bool to estimate mb covariance data (default: 0)
+    display: int, opt
+      bool to display LC fit in real-time (default:0)
 
-def makeYaml(input_file, dbDir, prodid, outDir, nproc):
+    Returns
+    -----------
+    dict with modified parameters
 
+    """
     with open(input_file, 'r') as file:
         filedata = file.read()
 
@@ -26,35 +44,40 @@ def makeYaml(input_file, dbDir, prodid, outDir, nproc):
     filedata = filedata.replace('outDir', outDir)
     filedata = filedata.replace('dbDir', dbDir)
     filedata = filedata.replace('nnproc', str(nproc))
+    filedata = filedata.replace('covmbcalc', str(covmb))
+    filedata = filedata.replace('displayval', str(display))
+
     return yaml.load(filedata, Loader=yaml.FullLoader)
 
 
-def dump(outname, names, val, inum):
+def procelem(lc_name, simul, fit, j=0, output_q=None):
+    """
+    Method to process (fit) a set of LC
+    elementary cell of the multiprocessing effort
 
-    # dump the results in a hdf5 file
-    # res = Table(np.rec.fromrecords(val,names = names))
-    Table(rows=val, names=names).write(
-        outname, 'fit_lc_{}'.format(inum), append=True, compression=True)
+    Parameters
+    ---------------
+    lc_name: str
+      name of the file containing LCs
+    simul: 
+    fit: fit_xxx class
+      instance of the class used to fit LCs
+    j:  int,opt
+       internal parameter for multiprocessing (default: -1)
+    output_q: multiprocessing.queue, opt
+       queue for multiprocessing (default: None)
 
+    Returns
+    ----------
+    res: astropy table with fit results
 
-def procelem(lc_name, simul, fit, covmb, j=0, output_q=None):
-
-    res = []
+    """
+    res = Table()
     for simu in simul:
         lc = None
-        # if simu['n_lc_points'] > 0:
         lc = Table.read(lc_name, path='lc_{}'.format(simu['id_hdf5']))
         resfit = fit(lc)
-        idx = resfit['fitstatus'] == 'fitok'
-        if len(resfit[idx]) > 0:
-            if covmb is not None:
-                covDict = mbcovCalc(covmb, resfit)
-                for key in covDict.keys():
-                    resfit[key] = [covDict[key]]
-
-            res = vstack([res, resfit])
-
-    # dump('{}_{}'.format(fit.fit_out,j),names,val,j)
+        res = vstack([res, resfit])
 
     print('done', j)
     if output_q is not None:
@@ -63,176 +86,121 @@ def procelem(lc_name, simul, fit, covmb, j=0, output_q=None):
         return res
 
 
-def mbcovCalc(covmb, vals):
+def multiproc(simu_name, lc_name, fit, covmb=None, nproc=1):
+    """
+    Method to grab and fit LC using multiprocessing
 
-    cov = np.ndarray(shape=(3, 3), dtype=float, order='F')
-    cov[0, 0] = vals['Cov_x0x0'].data
-    cov[1, 1] = vals['Cov_x1x1'].data
-    cov[2, 2] = vals['Cov_colorcolor'].data
-    cov[0, 1] = vals['Cov_x0x1'].data
-    cov[0, 2] = vals['Cov_x0color'].data
-    cov[1, 2] = vals['Cov_x1color'].data
-    cov[2, 1] = cov[1, 2]
-    cov[1, 0] = cov[0, 1]
-    cov[2, 0] = cov[0, 2]
+    Parameters
+    ---------------
+    simu_name: str
+      name of the file with SN parameters (astropy table)
+    lc_name: str
+       name of the file containing the light curves to fit
+    fit: Fitting instance
+       where the fit is performed
+    covmb: MbCov class, opt
+      MbCov class used to estimate Mb covariance data (default: None)
+    nproc: int, opt
+      number of procs for multiprocessing (default: 1)
 
-    params = dict(zip(['x0', 'x1', 'c'], [vals['x0_fit'].data,
-                                          vals['x1_fit'].data, vals['color_fit'].data]))
+    Returns
+    -----------
+    astropy table with fitted parameters
 
-    resu = covmb.mbCovar(params, cov, ['x0', 'x1', 'c'])
-    sigmu_sq = resu['Cov_mbmb']
-    sigmu_sq += alpha**2 * vals['Cov_x1x1'].data + \
-        beta**2 * vals['Cov_colorcolor'].data
-    sigmu_sq += 2.*alpha*resu['Cov_x1mb']
-    sigmu_sq += -2.*alpha*beta*vals['Cov_x1color'].data
-    sigmu_sq += -2.*beta*resu['Cov_colormb']
-    sigmu = 0.
-    if sigmu_sq >= 0.:
-        sigmu = np.sqrt(sigmu_sq)
+    """
 
-    resu['sigma_mu'] = sigmu.item()
-    return resu
-
-
-def multiproc(simu_name='', lc_name='', fit=None, covmb=None, nproc=1):
-
+    # getting the simu file
     f = h5py.File(simu_name, 'r')
     print(f.keys())
+    # reading the simu file
     for i, key in enumerate(f.keys()):
         simu = Table.read(simu_name, path=key)
 
-    nref = 2000
+    # multiprocessing parameters
     nlc = len(simu)
-    print('total number of LC', nlc)
-    delta = nlc
-    if nproc > 1:
-        delta = int(delta/(nproc))
+    t = np.linspace(0, nlc, nproc+1, dtype='int')
+    print(t)
 
-    batch = range(0, nlc, delta)
-    if nlc not in batch:
-        batch = np.append(batch, nlc)
-    print(batch)
     names = None
     val = []
     inum = -1
     result_queue = multiprocessing.Queue()
 
-    for i in range(len(batch)-1):
-        ida = batch[i]
-        idb = batch[i+1]
-        if ida > 0 and (ida % nref) == 0:
-            print('Running', ida)
-        p = multiprocessing.Process(name='Subprocess-'+str(i), target=procelem, args=(
-            lc_name, simu[ida:idb], fit, covmb, i, result_queue))
+    procs = [multiprocessing.Process(name='Subprocess-'+str(i), target=procelem, args=(
+        lc_name, simu[t[i]:t[i+1]], fit, i, result_queue)) for i in range(nproc)]
+
+    for p in procs:
         p.start()
-        print('start', i)
 
     resultdict = {}
-    for j in range(len(batch)-1):
+    for j in range(nproc):
         resultdict.update(result_queue.get())
 
     for p in multiprocessing.active_children():
         p.join()
 
     val = Table()
-    print('dumping results')
-    for j in range(len(batch)-1):
+    for j in range(nproc):
         val = vstack([val, resultdict[j]])
 
-    print('finishing')
+    print('dumping results')
     if len(val) > 0:
         inum += 1
         fit.dump(val, inum)
 
 
-class Fit_All:
-    def __init__(self, telescope, output_config, display_lc, fitter_config, prodid):
+def process(config, covmb=None):
+    """
+    Method to process(fit) a set of LCs
 
-        module = import_module(fitter_config['name'])
-        self.fitter = module.Fit_LC(
-            model=fitter_config['model'], version=fitter_config['version'], telescope=telescope, display=display_lc)
+    Parameters
+    --------------
+    config: dict
+      parameters used for the processing (fit)
+    covmb: MbCov class, opt
+      MbCov call used to estimated mb covariance data (default: None)
 
-        if output_config['save']:
-            self.prepareSave(output_config['directory'], prodid)
-
-        self.val = []
-
-    def __call__(self, lc, j=-1, output_q=None):
-
-        resfit = self.fitter(lc)
-
-        if output_q is not None:
-            output_q.put({j: resfit})
-        else:
-            return resfit
-
-    def prepareSave(self, outdir, prodid):
-
-        if not os.path.exists(outdir):
-            print('Creating output directory', outdir)
-            os.makedirs(outdir)
-
-        self.fit_out = outdir+'/Fit_'+prodid+'.hdf5'
-        if os.path.exists(self.fit_out):
-            os.remove(self.fit_out)
-
-    def dump(self, tab, inum):
-
-        # dump the results in a hdf5 file
-        # res = Table(np.rec.fromrecords(val,names = names))
-        # tab = Table(rows=val, names=names)
-        tab['fitstatus'] = tab['fitstatus'].astype(
-            h5py.special_dtype(vlen=str))
-        tab.write(self.fit_out, 'fit_lc_{}'.format(
-            inum), append=True, compression=True)
-
-
-parser = argparse.ArgumentParser(
-    description='Run a LC fitter from a configuration file')
-parser.add_argument('config_filename',
-                    help='Configuration file in YAML format.')
-
-
-def run(dirFiles, prodid, outDir, nproc, covmb=None):
-    # YAML input file.
-    # config = yaml.load(open(config_filename))
-    config = makeYaml('input/fit_sn/param_fit_gen.yaml',
-                      dirFiles, prodid, outDir, nproc)
-    print(config)
-
-    # load telescope
-    tel_par = config['Instrument']
+    """
 
     # this is for output
     save_status = config['Output']['save']
     outdir = config['Output']['directory']
+
+    # prodid parameter
     prodid = config['ProductionID']
+
+    # input files:
+    # simu_file: astropy table with SN parameters
+    # lc file: astropy tables with light curves
 
     simu_name = config['Simulations']['dirname']+'/Simu_'+prodid+'.hdf5'
     lc_name = config['Simulations']['dirname']+'/LC_'+prodid+'.hdf5'
 
-    telescope = Telescope(atmos=True, airmass=1.2)
+    # Fitting instance
+    fit = Fitting(config, covmb=covmb)
 
-    fit = Fit_All(telescope, config['Output'],
-                  config['Display'], config['Fitter'], prodid)
-
-    # Loop on the simu_file to grab simulated LCs
+    # Loop on the simu_file to grab and fit simulated LCs
     multiproc(simu_name=simu_name, lc_name=lc_name, fit=fit,
               covmb=covmb, nproc=config['Multiprocessing']['nproc'])
 
 
+parser = argparse.ArgumentParser(
+    description='Run a LC fitter on a set of LC curves.')
+
 parser = OptionParser()
 
-parser.add_option("--dirFiles", type="str", default='',
+parser.add_option("--dirFiles", type="str", default='/sps/lsst/users/gris/Output_Simu_pipeline_0',
                   help="location dir of the files[%default]")
 parser.add_option("--prodid", type="str", default='Test',
                   help="db name [%default]")
-parser.add_option("--outDir", type="str", default='',
+parser.add_option("--outDir", type="str", default='/sps/lsst/users/gris/Output_Fit_0',
                   help="output dir [%default]")
 parser.add_option("--nproc", type="int", default=1,
                   help="number of proc [%default]")
 parser.add_option("--mbcov", type="int", default=0,
                   help="mbcol calc [%default]")
+parser.add_option("--display", type="int", default=0,
+                  help="to display fit in real-time[%default]")
 parser.add_option("--prefix", type=str, default='sncosmo_DD',
                   help="prefix for input file[%default]")
 
@@ -240,20 +208,16 @@ parser.add_option("--prefix", type=str, default='sncosmo_DD',
 opts, args = parser.parse_args()
 
 dirFiles = opts.dirFiles
-if dirFiles == '':
-    dirFiles = '/sps/lsst/users/gris/Output_Simu_pipeline_0'
 outDir = opts.outDir
-if outDir == '':
-    outDir = '/sps/lsst/users/gris/Output_Fit_0'
-
 prodid = opts.prodid
 nproc = opts.nproc
 mbCalc = opts.mbcov
+display = opts.display
 
-salt2Dir = 'SALT2_Files'
 
 covmb = None
 if mbCalc:
+    salt2Dir = 'SALT2_Files'
     covmb = MbCov(salt2Dir, paramNames=dict(
         zip(['x0', 'x1', 'color'], ['x0', 'x1', 'c'])))
 
@@ -263,5 +227,9 @@ files = glob.glob('{}/Simu_{}_{}*.hdf5'.format(dirFiles, opts.prefix, prodid))
 for fi in files:
     prodid = '{}_{}'.format(opts.prefix, fi.split(
         '{}_'.format(opts.prefix))[-1].split('.hdf5')[0])
-    print('hhh', prodid)
-    run(dirFiles, prodid, outDir, nproc, covmb=covmb)
+    # make and load config file
+    config = makeYaml('input/fit_sn/param_fit_gen.yaml',
+                      dirFiles, prodid, outDir, nproc, mbCalc, display)
+    print(config)
+    # now run
+    process(config, covmb=covmb)
