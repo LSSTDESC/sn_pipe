8761ecc1e35972ed80ef2e4d54727ba762c6d313
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-06-05 11:46:02 +0200

    code improvement: add a class to analyze and plot simulation results

--- plot_scripts/simulation/plot_simu.py
+++ plot_scripts/simulation/plot_simu.py
@@ -6,6 +6,86 @@ import pprint
 from optparse import OptionParser
 
 
+class SimuPlot:
+    """
+    class to analyze and plot simulation output (parameters and LC)
+
+    Parameters
+    ---------------
+    dbDir: str
+      location dir of the files
+    dbName: str
+       procID of the data to process
+
+    """
+
+    def __init__(self, dbDir, dbName):
+
+        # load simulation parameters
+        parName = 'Simu_{}.hdf5'.format(opts.dbName)
+
+        self.simuPars = self.load_params('{}/{}'.format(opts.dbDir, parName))
+
+    def load_params(self, paramFile):
+        """
+        Function to load simulation parameters
+
+        Parameters
+        ---------------
+        paramFile: str
+          name of the parameter file
+
+        Returns
+        -----------
+        params: astropy table
+        with simulation parameters
+
+        """
+
+        f = h5py.File(paramFile, 'r')
+        print(f.keys(), len(f.keys()))
+        params = Table()
+        for i, key in enumerate(f.keys()):
+            pars = Table.read(paramFile, path=key)
+            params = vstack([params, pars])
+
+        return params
+
+    def plotParameters(self, season=1, toplot=['x1', 'color', 'daymax', 'z']):
+        """ Plot simulation parameters
+        parameters ('X1', 'Color', 'DayMax', 'z')
+
+        Parameters
+        ---------------
+        fieldname: (DD or WFD)
+        fieldid: (as given by OpSim)
+        tab: recarray of parameters
+        season: season
+
+        """
+
+        idx = self.simuPars['season'] == season
+        sel = self.simuPars[idx]
+        thesize = 15
+        ncols = 2
+        nrows = int(len(toplot)/2)
+        print(nrows, ncols)
+        fig, ax = plt.subplots(ncols=ncols, nrows=nrows, figsize=(10, 9))
+        #title = '{} - fieldid {} - season {}'.format(fieldname, fieldid, season)
+        title = 'season {}'.format(season)
+        fig.suptitle(title, fontsize=thesize)
+
+        for i, var in enumerate(toplot):
+            ix = int(i/2)
+            iy = i % 2
+            axis = ax[ix][iy]
+            axis.hist(sel[var], histtype='step')  # bins=len(sel[var]))
+            axis.set_xlabel(var, fontsize=20)
+            axis.set_ylabel('Number of entries', fontsize=thesize)
+            axis.tick_params(axis='x', labelsize=thesize)
+            axis.tick_params(axis='y', labelsize=thesize)
+
+
 def plotParameters(fieldname, fieldid, tab, season):
     """ Plot simulation parameters
     parameters ('X1', 'Color', 'DayMax', 'z')
@@ -113,6 +193,23 @@ parser.add_option("--dbDir", type="str", default='Output_Simu',
 opts, args = parser.parse_args()
 
 print('Start processing...', opts)
+
+splot = SimuPlot(opts.dbDir, opts.dbName)
+
+# get the simulation parameters
+simupars = splot.simuPars
+
+print('NSN', len(simupars))
+# get columns
+
+cols = simupars.columns
+
+print(cols)
+
+splot.plotParameters()
+
+plt.show()
+
 
 parName = 'Simu_{}.hdf5'.format(opts.dbName)
 
