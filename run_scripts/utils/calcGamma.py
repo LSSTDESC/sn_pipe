f285d3c458349455a256d5822c898fa02e074eb4
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-06-10 17:12:48 +0200

    new way of estimating gammas

--- run_scripts/utils/calcGamma.py
+++ run_scripts/utils/calcGamma.py
@@ -7,9 +7,11 @@ telescope = Telescope(airmass=1.2)
 
 outName = 'gamma_test.hdf5'
 mag_range = np.arange(15., 38., 1.)
-exptimes = np.arange(0., 9000., 10.)
-exptimes[0] = 1.
+#exptimes = np.arange(0., 9000., 10.)
+#exptimes[0] = 1.
+nexps = range(1, 500, 1)
+single_exposure_time = 30.
 
 Gamma(bands, telescope, outName,
       mag_range=mag_range,
-      exptimes=exptimes)
+      single_exposure_time=single_exposure_time, nexps=nexps)
