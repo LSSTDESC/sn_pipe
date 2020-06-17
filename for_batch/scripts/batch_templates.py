ecbe3f19d02adb070e34d90d1da837ec71d8da8e
pgris <gris@clermont.in2p3.fr>
2020-06-17 15:28:47 +0200

    updating batch_templates script to account for latest code dev

--- for_batch/scripts/batch_templates.py
+++ for_batch/scripts/batch_templates.py
@@ -1,8 +1,13 @@
 import os
 import numpy as np
 
+def addoption(cmd, name, val):
+    cmd += ' --{} {}'.format(name, val)
+    return cmd
 
-def batch(x1, color, nproc=8, zmax=1.2):
+
+
+def batch(x1, color, nproc=8, zmin=0.01,zmax=1.2,zstep=0.01,outDirLC='',outDirTemplates='',what='simu'):
 
     cwd = os.getcwd()
     dirScript = cwd + "/scripts"
@@ -29,22 +34,32 @@ def batch(x1, color, nproc=8, zmax=1.2):
     script.write("#!/bin/env bash\n")
     script.write(" cd " + cwd + "\n")
     script.write(" echo 'sourcing setups' \n")
-    script.write(" source setup_release.sh CCIN2P3\n")
-    script.write(" source export.sh \n")
-    script.write("echo 'sourcing done' \n")
-    """
-    script.write("export PYTHONPATH=sn_tools:$PYTHONPATH \n")
-    script.write("export PYTHONPATH=sn_metrics:$PYTHONPATH \n")
-    script.write("export PYTHONPATH=sn_stackers:$PYTHONPATH \n")
-    """
-    script.write("echo $PYTHONPATH \n")
+    script.write(" source setup_release.sh Linux\n")
 
-    cmd = 'python run_scripts/templates/run_simulation_template.py --simul 1 --lcdiff 0 --x1 {} --color {} --nproc {} --zmax {}'.format(
-        x1, color, nproc, zmax)
-    script.write(cmd+" \n")
-    cmd = 'python run_scripts/templates/run_simulation_template.py --simul 0 --lcdiff 1 --x1 {} --color {}'.format(
-        x1, color)
-    script.write(cmd+" \n")
+
+    cmd = 'python run_scripts/templates/run_template_LC.py'
+    cmd = addoption(cmd, 'x1', x1)
+    cmd = addoption(cmd, 'color', color)
+    cmd = addoption(cmd, 'zmin', zmin)
+    cmd = addoption(cmd, 'zmax', zmax)
+    cmd = addoption(cmd, 'zstep', zstep)
+    cmd = addoption(cmd, 'nproc', nproc)
+    cmd = addoption(cmd, 'outDir', outDirLC)
+    if what == 'simu':
+        print(cmd)
+        script.write(cmd+"\n")
+
+
+
+    # stack produced LCs
+    cmd = 'python run_scripts/templates/run_template_vstack.py'
+    cmd = addoption(cmd, 'x1', x1)
+    cmd = addoption(cmd, 'color', color)
+    cmd = addoption(cmd, 'lcDir', '{}/fake_simu_data'.format(outDirLC))
+    cmd = addoption(cmd, 'outDir', outDirTemplates)
+    if what == 'vstack':
+        print(cmd)
+        script.write(cmd+" \n")
     script.write("EOF" + "\n")
     script.close()
     os.system("sh "+scriptName)
@@ -58,5 +73,11 @@ zmax = [1.1, 1.1, 0.8, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2]
 
 zmax_dict = dict(zip(x1_colors, zmax))
 
+outDirLC = '/sps/lsst/users/gris/fakes_for_templates'
+outDirTemplates = '/sps/lsst/users/gris/Template_LC'
+
+what='simu'
 for (x1, color) in x1_colors:
-    batch(x1, color, zmax=zmax_dict[(x1, color)])
+    batch(x1, color, zmax=zmax_dict[(x1, color)],
+          outDirLC=outDirLC,outDirTemplates=outDirTemplates,what=what)
+    break
