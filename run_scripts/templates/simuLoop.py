1ae6f663f8a5a7c6cebd479f31187f0153fc8fe5
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-06-23 08:58:07 +0200

    add bluecutoff, redcutoff and ebvofMW as parameters for template production

--- run_scripts/templates/simuLoop.py
+++ run_scripts/templates/simuLoop.py
@@ -10,9 +10,14 @@ x1_color = [(-2.0, 0.2)]
 zmin = 0.01
 zmax = 0.05
 zstep = 0.01
-nproc = 4
-outDirLC = 'fakes_for_templates'
-outDirTemplates = 'Template_LC'
+nproc = 1
+bluecutoff = 380
+redcutoff = 800
+ebvofMW = 0
+outDirLC = 'fakes_for_templates_{}_{}_ebvofMW_{}'.format(
+    bluecutoff, redcutoff, ebvofMW)
+outDirTemplates = 'Template_LC_{}_{}_ebvofMW_{}'.format(
+    bluecutoff, redcutoff, ebvofMW)
 
 for (x1, color) in x1_color:
     # generate LCs
@@ -24,6 +29,10 @@ for (x1, color) in x1_color:
     cmd = addoption(cmd, 'zstep', zstep)
     cmd = addoption(cmd, 'nproc', nproc)
     cmd = addoption(cmd, 'outDir', outDirLC)
+    cmd = addoption(cmd, 'bluecutoff', bluecutoff)
+    cmd = addoption(cmd, 'redcutoff', redcutoff)
+    cmd = addoption(cmd, 'ebvofMW', ebvofMW)
+
     print(cmd)
     os.system(cmd)
 
@@ -31,6 +40,9 @@ for (x1, color) in x1_color:
     cmd = 'python run_scripts/templates/run_template_vstack.py'
     cmd = addoption(cmd, 'x1', x1)
     cmd = addoption(cmd, 'color', color)
+    cmd = addoption(cmd, 'bluecutoff', bluecutoff)
+    cmd = addoption(cmd, 'redcutoff', redcutoff)
+    cmd = addoption(cmd, 'ebvofMW', ebvofMW)
     cmd = addoption(cmd, 'lcDir', '{}/fake_simu_data'.format(outDirLC))
     cmd = addoption(cmd, 'outDir', outDirTemplates)
     print(cmd)
