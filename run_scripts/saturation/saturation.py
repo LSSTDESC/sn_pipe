56c09d981235e483b3858e98d538a3ba47148c14
Philippe Gris <gris@clermont.in2p3.fr>
2020-08-03 18:39:44 +0200

    start implementation of saturation effects on sn

--- run_scripts/saturation/saturation.py
+++ run_scripts/saturation/saturation.py
@@ -1,10 +1,12 @@
 from sn_saturation.psf_pixels import PSF_pixels,PlotPixel,PlotMaxFrac
 from sn_saturation.mag_saturation import MagToFlux,MagSaturation,PlotMagSat
-from sn_saturation.observations import Observations
+from sn_saturation.observations import Observations,prepareYaml
+from sn_saturation.sn_sat_effects import SaturationTime
 import numpy as np
 import time
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 class PixelPSFSeeing:
     """
@@ -92,7 +94,7 @@ class PixelPSFSeeing:
     
     
 """
-seeing = 0.5
+seeing = 0.2355/3.
 #for psf_type in ['single_gauss','double_gauss']:
 for psf_type in ['single_gauss']:
     test = PSF_pixels(seeing,psf_type)
@@ -103,9 +105,9 @@ for psf_type in ['single_gauss']:
     print(resa.dtype)
     print(psf_type,resa[idxa])
     print(psf_type,resb[idxb])
-
-
 """
+
+
 
 """
 time_ref = time.time()
@@ -151,8 +153,43 @@ PlotMagSat('gri',res_sat)
 
 #################Fake Observations
 
-obs = Observations('../DB_Files','descddf_v1.4_10yrs')
+# this is to generate observations : list of (nexp, exptime)
+nexp_expt = [(1,5),(1,15),(1,30)]
+#obs = Observations('../DB_Files','descddf_v1.4_10yrs')
 
+# plot the seeing
 #obs.plotSeeings()
-obs.make_all_obs()
+
+# make observations
+#obs.make_all_obs()
+
+#perform simulation on Observations_* files
+nexp_expt = [(1,15)]
+
+x1_color = [(0.0,0.0)]
+seasons = [2]
+nproc = 1
+input_yaml = 'input/saturation/param_simulation_gen.yaml'
+"""
+for (x1,color) in x1_color:
+    for season in seasons:
+        for (nexp,expt) in nexp_expt:
+            output_yaml = 'param_simulation_{}_{}_{}_{}.yaml'.format(x1,color,nexp,expt,season)
+            prepareYaml(input_yaml,nexp,expt,x1,color,season,nproc,output_yaml)
+            cmd = 'python run_scripts/simulation/run_simulation_from_yaml.py --config_yaml={} --npixels 1'.format(output_yaml)
+            cmd += ' --radius 0.01'
+            cmd += ' --RAmin 0.0'
+            cmd += ' --RAmax 0.1'
+            os.system(cmd)
+            break
+"""
+dirFile = 'Output_Simu'
+prodid = 'Saturation_1_15_0.0_0.0_2_0'
+full_well = 120000
+for (x1,color) in x1_color:
+    for season in seasons:
+        for (nexp,expt) in nexp_expt:
+            sat = SaturationTime(dirFile,x1,color,nexp,expt,season)
+            sat.time_of_saturation(full_well)
+
 plt.show()
