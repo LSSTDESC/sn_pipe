44f02a9b95a6a84984f73631703ad8c22fdb4daa
Philippe Gris <gris@clermont.in2p3.fr>
2020-08-04 17:14:06 +0200

    continue implementation of scripts for sat studies

--- run_scripts/saturation/saturation.py
+++ run_scripts/saturation/saturation.py
@@ -1,7 +1,7 @@
 from sn_saturation.psf_pixels import PSF_pixels,PlotPixel,PlotMaxFrac
 from sn_saturation.mag_saturation import MagToFlux,MagSaturation,PlotMagSat
 from sn_saturation.observations import Observations,prepareYaml
-from sn_saturation.sn_sat_effects import SaturationTime
+from sn_saturation.sn_sat_effects import SaturationTime,plotTimeSaturation
 import numpy as np
 import time
 import matplotlib.pyplot as plt
@@ -91,8 +91,73 @@ class PixelPSFSeeing:
 
         return resfi
 
+def Simulations(nexp_expt=[(1,15),(1,30)],x1_color=[(0.0,0.0)],seasons = [2],cadence=3):
+    """
+    Function to perform a set of simulations
+
+    Parameters
+    ---------------
+    nexp_expt: list(tuples)
+      list of (nexp, exptime) combis
+    x1_color: list(tuple)
+      list of (x1,color) combi
+    season: list(int)
+      list of the seasons to gen
+    cadence: int
+      cadence of observations
+
+    """
     
+    nproc = 1
+    input_yaml = 'input/saturation/param_simulation_gen.yaml'
+
+    for (x1,color) in x1_color:
+        for season in seasons:
+            for (nexp,expt) in nexp_expt:
+                output_yaml = 'param_simulation_{}_{}_{}_{}_{}.yaml'.format(x1,color,nexp,expt,season,cadence)
+                prepareYaml(input_yaml,nexp,expt,x1,color,season,nproc,output_yaml,cadence)
+                cmd = 'python run_scripts/simulation/run_simulation_from_yaml.py --config_yaml={} --npixels 1'.format(output_yaml)
+                cmd += ' --radius 0.01'
+                cmd += ' --RAmin 0.0'
+                cmd += ' --RAmax 0.1'
+                os.system(cmd)              
+
+
+def estimateSaturationTime(dirFile,x1_color,seasons,nexp_expt,cadence_obs,nproc):
+    """
+    Function to estimate saturation time vs z
+
+    Parameters
+    --------------
+    dirFile: str
+      location dir of the simulation
+    x1_color: list(tuple)
+      list of (x1,color) combi   
+    season: list(int)
+      list of the seasons to gen
+    nexp_expt: list(tuples)
+      list of (nexp, exptime) combis
+    cadence: int
+      cadence of observations
+    nproc: int
+     number of proc for multiprocessing
     
+    """
+    full_wells = [90000,120000]
+
+    timesat = pd.DataFrame()
+    
+    for (x1,color) in x1_color:
+        for season in seasons:
+            for (nexp,expt) in nexp_expt:
+                sat = SaturationTime(dirFile,x1,color,nexp,expt,season,cadence_obs)
+                for full_well in full_wells:
+                    res = sat.multi_time(full_well,npp=nproc)
+                    timesat = pd.concat((timesat,res))
+                    print(res)
+
+
+    np.save('TimeSat_{}.npy'.format(cadence_obs),timesat.to_records(index=False))
 """
 seeing = 0.2355/3.
 #for psf_type in ['single_gauss','double_gauss']:
@@ -155,41 +220,30 @@ PlotMagSat('gri',res_sat)
 
 # this is to generate observations : list of (nexp, exptime)
 nexp_expt = [(1,5),(1,15),(1,30)]
-#obs = Observations('../DB_Files','descddf_v1.4_10yrs')
-
+obs = Observations('../DB_Files','descddf_v1.4_10yrs')
+cadence_obs = 3
 # plot the seeing
 #obs.plotSeeings()
 
 # make observations
-#obs.make_all_obs()
+#obs.make_all_obs(cadence=cadence_obs)
 
 #perform simulation on Observations_* files
-nexp_expt = [(1,15)]
-
+nexp_expt = [(1,15),(1,30)]
 x1_color = [(0.0,0.0)]
 seasons = [2]
-nproc = 1
-input_yaml = 'input/saturation/param_simulation_gen.yaml'
+
+#Simulations(nexp_expt=nexp_expt ,x1_color=x1_color,seasons=seasons,cadence=cadence_obs)
+
+#estimate the saturation time here
+estimateSaturationTime('Output_Simu',x1_color,seasons,nexp_expt,cadence_obs,nproc=1)
+
+
+
 """
-for (x1,color) in x1_color:
-    for season in seasons:
-        for (nexp,expt) in nexp_expt:
-            output_yaml = 'param_simulation_{}_{}_{}_{}.yaml'.format(x1,color,nexp,expt,season)
-            prepareYaml(input_yaml,nexp,expt,x1,color,season,nproc,output_yaml)
-            cmd = 'python run_scripts/simulation/run_simulation_from_yaml.py --config_yaml={} --npixels 1'.format(output_yaml)
-            cmd += ' --radius 0.01'
-            cmd += ' --RAmin 0.0'
-            cmd += ' --RAmax 0.1'
-            os.system(cmd)
-            break
+df = pd.DataFrame(np.load('TimeSat_{}.npy'.format(cadence_obs),allow_pickle=True))
+
+plotTimeSaturation(0.0,0.0,df)
 """
-dirFile = 'Output_Simu'
-prodid = 'Saturation_1_15_0.0_0.0_2_0'
-full_well = 120000
-for (x1,color) in x1_color:
-    for season in seasons:
-        for (nexp,expt) in nexp_expt:
-            sat = SaturationTime(dirFile,x1,color,nexp,expt,season)
-            sat.time_of_saturation(full_well)
 
 plt.show()
