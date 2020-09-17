7538d5f9e8d3e146cc9dfbe7037a6b27594a0a99
Philippe Gris <gris@clermont.in2p3.fr>
2020-09-17 15:39:08 +0200

    code cleaning plus new options added

--- run_scripts/fakes/loop_full_fast.py
+++ run_scripts/fakes/loop_full_fast.py
@@ -1,33 +1,44 @@
 import numpy as np
 import os
 
-#simus = ['sn_fast', 'sn_cosmo']
-simus = ['sn_cosmo']
-ebvs = [0.0]
-redcutoff = 800.0
-blues = [360.0, 370.0, 380.0]
-blues = [360.]
-fake_config = 'input/Fake_cadence/Fake_cadence.yaml'
-x1 = -2.0
-color = 0.2
-error_model = 0
 
-for bluecutoff in blues:
-    bluecutoff = np.round(bluecutoff, 1)
-    for ebv in ebvs:
-        cmd_comm = 'python run_scripts/fakes/full_simulation_fit.py  --ebvofMW {} --outDir_simu Output_Simu_{}_{}_ebvofMW_{} --outDir_fit Output_Fit_{}_{}_ebvofMW_{} --bluecutoff {} --redcutoff {} --fake_config {} --x1 {} --color {} --error_model {}'.format(
-            ebv, bluecutoff, redcutoff, ebv, bluecutoff, redcutoff, ebv, bluecutoff, redcutoff, fake_config, x1, color,error_model)
+def run(x1,color,simus,ebv,bluecutoff,redcutoff,error_model,fake_config,snrmin,nbef,naft):
 
-        # fast simulator
-        # for simu in
-        for simu in simus:
+    cutoff = '{}_{}'.format(bluecutoff,redcutoff)
+    if error_model:
+        cutoff = 'error_model'
+    
+    outDir_simu = 'Output_Simu_{}_ebvofMW_{}'.format(cutoff,ebv)
+    outDir_fit = 'Output_Fit_{}_ebvofMW_{}_snrmin_{}'.format(cutoff,ebv,int(snrmin))
+    
+    cmd_comm = 'python run_scripts/fakes/full_simulation_fit.py  --ebvofMW {}'.format(ebv)
+    cmd_comm += ' --outDir_simu {} --outDir_fit {}'.format(outDir_simu,outDir_fit)
+    cmd_comm += ' --bluecutoff {} --redcutoff {}'.format(bluecutoff,redcutoff) 
+    cmd_comm += ' --fake_config {} --x1 {} --color {} --error_model {}'.format(fake_config, x1, color,error_model)
+    cmd_comm += ' --snrmin {} --nbef {} --naft {}'.format(snrmin,nbef,naft)
+
+    for simu in simus:
             cmd = cmd_comm
             cmd += ' --simulator {}'.format(simu)
             os.system(cmd)
+    
 
-        """
-        # sncosmo simulator
-        cmd = cmd_comm
-        cmd += ' --simulator sn_cosmo'
-        os.system(cmd)
-        """
+simus = ['sn_fast', 'sn_cosmo']
+#simus = ['sn_cosmo']
+ebvs = [0.0]
+redcutoff = 800.0
+blues = [360.0, 370.0, 380.0]
+blues = [380.]
+fake_config = 'input/Fake_cadence/Fake_cadence.yaml'
+x1 = -2.0
+color = 0.2
+error_model = 1
+
+snrmin = 5.
+nbef = 4
+naft = 5
+
+#run(x1,color,simus,0.0,380.,800.,1,fake_config,snrmin,nbef,naft)
+
+for bluecutoff in blues:
+    run(x1,color,simus,0.0,bluecutoff,800.,0,fake_config,snrmin,nbef,naft)
