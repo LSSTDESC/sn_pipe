ec8c2f74c15896f4c942a847934394fce6b88bc5
Philippe Gris <gris@clermont.in2p3.fr>
2020-09-21 18:23:52 +0200

    add two new parameters in the script for MoviePixels

--- run_scripts/visu_cadence/run_visu_cadence.py
+++ run_scripts/visu_cadence/run_visu_cadence.py
@@ -9,6 +9,8 @@ parser.add_option("--dbName", type="str", default='alt_sched',
                   help="db name [%default]")
 parser.add_option("--dbDir", type="str", default='/sps/lsst/cadence/LSST_SN_CADENCE/cadence_db', help="db dir [%default]")
 parser.add_option("--dbDir_pixels", type="str", default='../ObsPixelized_circular_new/', help="db dir for pixel data[%default]")
+parser.add_option("--figDir", type="str", default='../OS_Figures', help="dir for figures [%default]")
+parser.add_option("--movieDir", type="str", default='../OS_Movies', help="dir for movies [%default]")
 parser.add_option("--nights", type="str", default='1,2',
                   help="list of nights to display  [%default]")
 parser.add_option("--saveMovie", type="int", default=0,
@@ -25,6 +27,8 @@ opts, args = parser.parse_args()
 
 dbDir = opts.dbDir
 dbDir_pixels = opts.dbDir_pixels
+figDir = opts.figDir
+movieDir = opts.movieDir
 dbName = opts.dbName
 saveMovie = opts.saveMovie
 realTime = opts.realTime
@@ -49,6 +53,6 @@ if dispType == 'snapshot':
 if dispType =='moviepixels':
     nightmin = np.min(nights)
     nightmax = np.max(nights)
-    MoviePixels(dbDir=dbDir, dbDir_pixels=dbDir_pixels,dbName=dbName, saveMovie=saveMovie, realTime=realTime,
+    MoviePixels(dbDir=dbDir, dbDir_pixels=dbDir_pixels,figDir=figDir,movieDir=movieDir,dbName=dbName, saveMovie=saveMovie, realTime=realTime,
                 saveFig=saveFig,nightmin=nightmin,nightmax=nightmax)
 
