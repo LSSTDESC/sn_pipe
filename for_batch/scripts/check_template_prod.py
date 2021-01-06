d3808a2a0fa89f8d155e733e953fc906d5cc8b84
pgris <gris@clermont.in2p3.fr>
2021-01-06 16:48:27 +0100

    improved script by adding config parameters

--- for_batch/scripts/check_template_prod.py
+++ for_batch/scripts/check_template_prod.py
@@ -1,17 +1,43 @@
 import glob
 import numpy as np
+from optparse import OptionParser
 
+parser = OptionParser()
 
-x1 = 0.0
-color = 0.0
+parser.add_option("--x1", type=float, default=-2.0,
+                  help="SN x1[%default]")
+parser.add_option("--color", type=float, default=0.2,
+                  help="SN color[%default]")
+parser.add_option("--zmax", type=float, default=1.01,
+                  help="SN max redshift[%default]")
+parser.add_option("--zmin", type=float, default=0.01,
+                  help="SN min redshift[%default]")
+parser.add_option("--zstep", type=float, default=0.01,
+                  help="SN min redshift[%default]")
+parser.add_option("--error_model", type=int, default=1,
+                  help="error model for SN LC estimation[%default]")
+parser.add_option("--bluecutoff", type=float, default=380.0,
+                  help="blue cutoff[%default]")
+parser.add_option("--redcutoff", type=float, default=800.0,
+                  help="red cutoff[%default]")
 
-nvals = 119
-ebvs = np.arange(0.0,0.40,0.01)
-prefix = '/sps/lsst/users/gris/fakes_for_templates_380.0_800.0_ebvofMW_'
+opts, args = parser.parse_args()
 
-listref = list(np.arange(0.0,1.2,0.01))
-listref[0] = 0.01
+x1 = opts.x1
+color = opts.color
+
+nvals = int((opts.zmax-opts.zmin)/opts.zstep)
+ebvs = np.arange(0.0,0.30,0.01)
+
+cutoff = '{}_{}'.format(opts.bluecutoff, opts.redcutoff)
+if opts.error_model>0:
+    cutoff = 'error_model'
+
+prefix = '/sps/lsst/users/gris/fakes_for_templates_{}_ebvofMW_'.format(cutoff)
+
+listref = list(np.arange(opts.zmin,opts.zmax,opts.zstep))
 listref = np.round(listref,2)
+
 for ebv in ebvs:
     ebv = np.round(ebv,2)
     fis = glob.glob('{}{}/fake_simu_data/{}_{}/LC_Fake_{}_{}*.hdf5'.format(prefix,ebv,x1,color,x1,color))
