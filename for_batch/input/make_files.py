96bd9023340f80ec7e4a5f80e225b729dfad8e48
pgris <gris@clermont.in2p3.fr>
2021-01-26 14:39:49 +0100

    code update

--- for_batch/input/make_files.py
+++ for_batch/input/make_files.py
@@ -2,7 +2,7 @@ import glob
 import numpy as np
 from optparse import OptionParser
 
-def genFile(files,fName,postfix):
+def genFile(files,fName,postfix,dirFiles,dbExtens):
     """
     Method to generate a file of OS names and associated infos
     This file is used as an input file to metrics, simulations in batch
@@ -21,18 +21,20 @@ def genFile(files,fName,postfix):
 
     script = open(fName,"w")
     #firstline='# dbName			simuType	  nside  coadd fieldtype nproc\n'
-    firstline='dbName,simuType,nside,coadd,fieldtype,nproc\n'
+    #firstline='dbName,simuType,nside,coadd,fieldtype,nproc\n'
+    firstline='dbDir,dbExtens,dbName,simuType,nside,coadd,fieldType,nproc\n'
     script.write(firstline)
     r = []
     for fi in files:
-        dbName = fi.split('/')[-1].split('.db')[0]
+        dbName = fi.split('/')[-1].split('.{}'.format(dbExtens))[0]
         r.append(len(dbName))
         #ljust = np.max(r)
 
     for fi in files:
-        dbName = fi.split('/')[-1].split('.db')[0]
+        line = '{}/{},{}'.format(dirFiles,dbExtens,dbExtens)
+        dbName = fi.split('/')[-1].split('.{}'.format(dbExtens))[0]
         #dbName=dbName.ljust(ljust)
-        line='{},{}\n'.format(dbName,postfix)
+        line += ',{},{}\n'.format(dbName,postfix)
         print(line)
         script.write(line)
 
@@ -43,16 +45,19 @@ parser = OptionParser()
 
 parser.add_option("--simuVersion", type=str, default='fbs_14',
                   help="simulation version[%default]")
-
-parser.add_option("--dirFiles", type=str, default='/sps/lsst/cadence/LSST_SN_PhG/cadence_db/fbs_1.4/db',
+parser.add_option("--dirFiles", type=str, default='/sps/lsst/cadence/LSST_SN_PhG/cadence_db/fbs_1.4',
                   help="dir where the files are [%default]")
+parser.add_option("--dbExtens", type=str, default='db',
+                  help="db extens (db or npy)  [%default]")
+
 
 opts, args = parser.parse_args()
 
 simuVersion = opts.simuVersion
 dirFiles = opts.dirFiles
+dbExtens = opts.dbExtens
 
-files = glob.glob('{}/*.db'.format(dirFiles))
+files = glob.glob('{}/{}/*.{}'.format(dirFiles,dbExtens,dbExtens))
 
 print(files)
 
@@ -63,5 +68,5 @@ DD_postfix = '1,128,1,DD,6'
 WFD_postfix = '0,64,1,WFD,8'
 
 print(DD_postfix)
-genFile(files,'DD_{}.csv'.format(simuVersion),DD_postfix)
-genFile(files,'WFD_{}.csv'.format(simuVersion),WFD_postfix)
+genFile(files,'DD_{}.csv'.format(simuVersion),DD_postfix,dirFiles,dbExtens)
+genFile(files,'WFD_{}.csv'.format(simuVersion),WFD_postfix,dirFiles,dbExtens)
