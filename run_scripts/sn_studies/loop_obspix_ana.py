01d95b61d37d15d51e8b034ab2a364b4b62ad560
pgris <gris@clermont.in2p3.fr>
2021-03-15 14:36:19 +0100

    add output directory for the results

--- run_scripts/sn_studies/loop_obspix_ana.py
+++ run_scripts/sn_studies/loop_obspix_ana.py
@@ -1,12 +1,39 @@
 import pandas as pd
 import os
+from optparse import OptionParser
 
-ll = pd.read_csv('for_batch/input/List_Db_DD.csv',comment='#')
+parser = OptionParser()
 
+parser.add_option('--OSlist', type=str, default='for_batch/input/List_Db_DD.csv',
+                  help='OS list to process [%default]')
+parser.add_option('--pixel_ana_dir', type=str, default='/sps/lsst/users/gris/pixels_analysis',
+                  help='pixel analysis file directory [%default]')
+parser.add_option('--action', type=str, default='pixels_analysis,OS_downtime',
+                  help='pixel analysis file directory [%default]')
+parser.add_option('--pixelDir', type=str, default='/sps/lsst/users/gris/ObsPixelized_128',
+                  help='obs pixels directory [%default]')
+parser.add_option('--outputDir', type=str, default='/sps/lsst/users/gris/OS_downtime',
+                  help='output directory [%default]')
+
+opts, args = parser.parse_args()
+
+actions = opts.action.split(',')
+
+ll = pd.read_csv(opts.OSlist,comment='#')
+
+pixel_ana_dir = opts.pixel_ana_dir
 for i, row in ll.iterrows():
     cmd = 'python sn_studies/sn_DD_nsn/obspixel_analysis_DD.py'
-    cmd += ' --fileDir /sps/lsst/users/gris/ObsPixelized_128' 
-    cmd += ' --outputDir /sps/lsst/users/gris/pixels_analysis'
+    cmd += ' --fileDir {}'.format(opts.pixelDir) 
+    cmd += ' --outputDir {}'.format(pixel_ana_dir)
     cmd += ' --dbName {}'.format(row['dbName'])
-    os.system(cmd)
-
+    if 'pixels_analysis' in actions:
+        os.system(cmd)
+    cmd = 'python sn_studies/sn_DD_nsn/OS_downtime.py' 
+    cmd += ' --dbDir {}'.format(row['dbDir'])
+    cmd += ' --dbName {}'.format(row['dbName']) 
+    cmd += ' --fieldNames COSMOS,CDFS,XMM-LSS,ELAIS,ADFS1,ADFS2' 
+    cmd += ' --fieldDir {}'.format(pixel_ana_dir)
+    cmd += ' --outputDir {}'.format(opts.outputDir)
+    if 'OS_downtime' in actions:
+        os.system(cmd)
