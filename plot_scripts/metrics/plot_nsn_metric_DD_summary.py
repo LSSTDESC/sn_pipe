21f0cd91f897145e89a0c0c2da8dcc19e833738c
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-03-26 09:27:35 +0100

    code cleaning

--- plot_scripts/metrics/plot_nsn_metric_DD_summary.py
+++ plot_scripts/metrics/plot_nsn_metric_DD_summary.py
@@ -306,8 +306,8 @@ metricTot = Summary(dirFile, 'NSN',
 
 print(metricTot.dtype)
 print('oo', np.unique(metricTot[['cadence', 'fieldname']]), type(metricTot))
-fieldNames = ['COSMOS', 'CDFS', 'XMM-LSS', 'ELAIS', 'ADFS1', 'ADFS2']
-fieldNames = ['COSMOS']
+#fieldNames = ['COSMOS', 'CDFS', 'XMM-LSS', 'ELAIS', 'ADFS1', 'ADFS2']
+#fieldNames = ['COSMOS']
 #nsn_plot.plot_DDArea(metricTot, forPlot, sntype='faint')
 
 nsn_plot.plot_DDSummary(metricTot, forPlot, sntype=snType,
