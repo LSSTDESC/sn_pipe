914d955a1eb11dd17988ccffd7fe2014b58726cb
Philippe Gris <gris@clermont.in2p3.fr>
2021-06-21 17:36:44 +0200

    code update

--- run_scripts/utils/calcGamma.py
+++ run_scripts/utils/calcGamma.py
@@ -5,12 +5,14 @@ import numpy as np
 bands = 'grizy'
 telescope = Telescope(airmass=1.2)
 
-outName = 'gamma_test.hdf5'
+outName = 'gamma_nexp_1.hdf5'
 mag_range = np.arange(13., 38., 0.05)
 #exptimes = np.arange(0., 9000., 10.)
 #exptimes[0] = 1.
-nexps = range(1, 500, 1)
-single_exposure_time = [15., 30.]
+#nexps = range(1, 500, 1)
+nexps = [1]
+#single_exposure_time = [15., 30.]
+single_exposure_time = range(1,61,1)
 
 Gamma(bands, telescope, outName,
       mag_range=mag_range,
