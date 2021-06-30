3c14eee14080ef7a4931d6ccd2b7405ccba50f2a
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-06-30 17:47:53 +0200

    extend estimation to 1s exptime

--- run_scripts/utils/calcGamma.py
+++ run_scripts/utils/calcGamma.py
@@ -5,14 +5,14 @@ import numpy as np
 bands = 'grizy'
 telescope = Telescope(airmass=1.2)
 
-outName = 'gamma_nexp_1.hdf5'
+outName = 'gamma_extended.hdf5'
 mag_range = np.arange(13., 38., 0.05)
 #exptimes = np.arange(0., 9000., 10.)
 #exptimes[0] = 1.
 #nexps = range(1, 500, 1)
-nexps = [1]
+nexps = [1, 2]
 #single_exposure_time = [15., 30.]
-single_exposure_time = range(1,61,1)
+single_exposure_time = range(1, 30, 2)
 
 Gamma(bands, telescope, outName,
       mag_range=mag_range,
