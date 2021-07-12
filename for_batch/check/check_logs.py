7cdd5e74cbbc934a6e10955e4f239aa64b6edaa5
pgris <gris@clermont.in2p3.fr>
2021-07-12 09:44:42 +0200

    add a script to check log files for metric production

---
+++ for_batch/check/check_logs.py
@@ -0,0 +1,43 @@
+import glob
+from optparse import OptionParser
+
+def transform(fi):
+
+    fa = fi.replace('logs','scripts')
+    fa = fa.replace('.log','.sh')
+
+    return fa
+
+parser = OptionParser()
+
+parser.add_option("--logDir", type="str", default='logs',
+                  help="log directory [%default]")
+parser.add_option("--relnum", type="str", default='v1.7',
+                  help="release number [%default]")
+
+opts, args = parser.parse_args()
+
+dirLog = opts.logDir
+relnum = opts.relnum
+
+search_path = '{}/*{}_10yrs*.log'.format(dirLog,relnum)
+fis = glob.glob(search_path)
+
+print(fis)
+
+what = 'end of processing'
+outFile = None
+for fi in fis:
+    ff = open(fi)
+    data = ff.read()
+    n_what = data.count(what)
+    if n_what != 8:
+        print('to reprocess:',fi,transform(fi))
+        if not outFile:
+            outFile = open('toreprocess_{}.sh'.format(relnum),'w')
+            outFile.write("#!/bin/bash \n")
+        else:
+            outFile.write('sh {} \n'.format(transform(fi)))
+
+if outFile:
+    outFile.close()
