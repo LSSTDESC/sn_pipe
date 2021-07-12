65742ceed57bf8236438493c06b9ec235e25bb29
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-07-12 12:11:16 +0200

    add script parameters

--- run_scripts/utils/make_movie_from_png.py
+++ run_scripts/utils/make_movie_from_png.py
@@ -1,11 +1,22 @@
 import os
+from optparse import OptionParser
 
+parser = OptionParser()
 
-figDir = 'figures_nsn'
-movDir = 'movies'
-prefix = 'healpix1'
+parser.add_option("--figDir", type="str", default='figures',
+                  help="fig directory [%default]")
+parser.add_option("--movieDir", type="str", default='movies',
+                  help="directory where movies will be placed[%default]")
+parser.add_option("--prefix", type="str", default='healpix36503',
+                  help="prefix for the figs to assemble as movie [%default]")
 
-cmd = 'ffmpeg -r 1 -s 1920x1080 -f image2 -i {}/{}_%000d.jpg -vcodec libx264 -crf 25  -pix_fmt yuv420p {}/{}.mp4 -y'.format(
+opts, args = parser.parse_args()
+
+figDir = opts.figDir
+movDir = opts.movieDir
+prefix = opts.prefix
+
+cmd = 'ffmpeg -v verbose -r 2 -s 1920x1080 -f image2 -i {}/{}_%00d.jpg -vcodec libx264 -crf 25  -pix_fmt yuv420p {}/{}.mp4 -y'.format(
     figDir, prefix, movDir, prefix)
 
 print(cmd)
