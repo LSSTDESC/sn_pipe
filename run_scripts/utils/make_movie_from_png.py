e0389cdeeefb5d726130270c66562ce7c501bb51
Philippe Gris <gris@clermont.in2p3.fr>
2021-07-15 16:55:48 +0200

    add the rate as a script parameter

--- run_scripts/utils/make_movie_from_png.py
+++ run_scripts/utils/make_movie_from_png.py
@@ -11,18 +11,20 @@ parser.add_option("--prefix", type="str", default='healpix36503',
                   help="prefix for the figs to assemble as movie [%default]")
 parser.add_option("--extens", type="str", default='jpg',
                   help="extension of the figures to assemble as a movie [%default]")
+parser.add_option("--rate", type=int, default=10,
+                  help="movie rate [%default]")
 opts, args = parser.parse_args()
 
 figDir = opts.figDir
 movDir = opts.movieDir
 prefix = opts.prefix
 extens = opts.extens
+rate = opts.rate
 
 if not os.path.exists(movDir):
     os.mkdir(movDir)
 
-cmd = 'ffmpeg -v verbose -r 4 -s 1920x1080 -f image2 -i {}/{}_%00d.{} -vcodec libx264 -crf 25  -pix_fmt yuv420p {}/{}.mp4 -y'.format(
-    figDir, prefix, extens, movDir, prefix)
+cmd = 'ffmpeg -v verbose -r {} -s 1920x1080 -f image2 -i {}/{}_%00d.{} -vcodec libx264 -crf 25  -pix_fmt yuv420p {}/{}.mp4 -y'.format(rate,figDir, prefix, extens, movDir, prefix)
 
 print(cmd)
 
