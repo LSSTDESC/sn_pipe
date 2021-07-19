7ca4cb4904f644728904cb782465039af23913df
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-07-19 18:12:20 +0200

    code update: make these plots using independent functions

--- plot_scripts/metrics/plot_nsn_metric_OS.py
+++ plot_scripts/metrics/plot_nsn_metric_OS.py
@@ -1,8 +1,80 @@
 import numpy as np
 import sn_plotter_metrics.nsnPlot as nsn_plot
+from sn_tools.sn_io import loopStack
 from optparse import OptionParser
 import pandas as pd
 import matplotlib.pyplot as plt
+import glob
+import healpy as hp
+
+def loadData(dirFile, dbName, metricName,fieldType,nside):
+
+    search_path = '{}/{}/{}/*NSNMetric_{}*_nside_{}_*.hdf5'.format(dirFile, dbName, metricName, fieldType, nside)
+    print('looking for', search_path)
+    fileNames = glob.glob(search_path)
+
+    metricValues = np.array(loopStack(fileNames, 'astropyTable'))
+
+    return metricValues
+    
+def plotMollview(data, varName, leg, op, xmin, xmax,nside):
+        """
+        Method to display results as a Mollweid map
+
+        Parameters
+        ---------------
+        data: pandas df
+          data to consider
+        varName: str
+          name of the variable to display
+        leg: str
+          legend of the plot
+        op: operator
+          operator to apply to the pixelize data(median, sum, ...)
+        xmin: float
+          min value for the display
+        xmax: float
+         max value for the display
+
+        """
+        npix = hp.nside2npix(nside)
+
+        hpxmap = np.zeros(npix, dtype=np.float)
+        hpxmap = np.full(hpxmap.shape, 0.)
+        hpxmap[data['healpixID'].astype(
+            int)] += data[varName]
+
+        norm = plt.cm.colors.Normalize(xmin, xmax)
+        cmap = plt.cm.jet
+        cmap.set_under('w')
+        resleg = op(data[varName])
+        if 'nsn' in varName:
+            resleg = int(resleg)
+        else:
+            resleg = np.round(resleg, 2)
+        title = '{}: {}'.format(leg, resleg)
+
+        hp.mollview(hpxmap, min=xmin, max=xmax, cmap=cmap,
+                    title=title, nest=True, norm=norm)
+        hp.graticule()
+
+        # save plot here
+        name = leg.replace(' - ', '_')
+        name = name.replace(' ', '_')
+        
+def jackknife(x, func):
+    """Jackknife estimate of the estimator func"""
+    n = len(x)
+    idx = np.arange(n)
+    return np.sum(func(x[idx!=i]) for i in range(n))/float(n)
+
+def jackknife_var(x, func):
+    """Jackknife estiamte of the variance of the estimator func."""
+    n = len(x)
+    idx = np.arange(n)
+    j_est = jackknife(x, func)
+    return (n-1)/(n + 0.0) * np.sum((func(x[idx!=i]) - j_est)**2.0
+                                    for i in range(n))
 
 parser = OptionParser(
     description='Display NSN metric results for WFD fields')
@@ -27,15 +99,52 @@ metricName = 'NSN'
 dbName = opts.dbName
 
 
-dbInfo = pd.DataFrame([[dbName, 'test', 'test', 'test', 'r', '*']],
-                      columns=['dbName', 'newName', 'group', 'plotName', 'color', 'marker'])
+metricValues = loadData(dirFile,dbName,metricName,fieldType,nside)
 
-metricdata = nsn_plot.NSNAnalysis(dirFile, dbInfo.loc[0], metricName, fieldType,
+print(metricValues.dtype)
+sntype = 'faint'
+idx = metricValues['status_{}'.format(sntype)] == 1
+idx &= metricValues['zlim_{}'.format(sntype)] > 0.
+idx &= metricValues['nsn_zlim_{}'.format(sntype)] > 0.
+data = pd.DataFrame(metricValues[idx])
+
+seasons = data['season'].unique()
+io = seasons <= 10
+seasons = seasons[io]
+fig, ax = plt.subplots()
+
+"""
+means = data.groupby(['season']).max().reset_index()
+
+ax.plot(means['season'],means['zlim_faint'],'ko')
+plt.show()
+"""
+
+for season in seasons:
+    idx = data['season'] == season
+    sel = data[idx]
+    leg = 'zlim - season {}'.format(int(season))
+    #plotMollview(sel,'zlim_faint',leg,np.median,0.1,0.4,nside)
+    #ax.hist(sel['zlim_faint'],histtype='step',bins=20)
+    ax.plot(sel['zlim_faint'],np.cumsum(sel['zlim_faint']))
+    #print(jackknife(sel['zlim_faint'],np.mean))
+    
+    
+plt.show()
+
+
+print(test)
+
+dbInfo = pd.DataFrame([[dirFile,dbName, 'test', 'test', 'test', 'r', '*']],
+                      columns=['dirFile','dbName', 'newName', 'group', 'plotName', 'color', 'marker'])
+dbInfo = pd.DataFrame([[dirFile,dbName, 'test', 'test', 'test', 'r', '*','fbs','1.7.1','rolling']],
+                      columns=['dirFile','dbName', 'newName', 'group', 'plotName', 'color', 'marker','simuType','simuNum','family'])
+metricdata = nsn_plot.NSNAnalysis(dbInfo.loc[0], metricName, fieldType,
                                   nside)
 
 # Mollweid plots
 dbName = dbName.split('_10yrs')[0]
-metricdata.Mollview_median('zlim_faint', '{} - zlim_faint'.format(dbName))
+metricdata.Mollview_median('zlim', '{} - zlim'.format(dbName))
 #metricdata.Mollview_median('ebvofMW', 'E(B-V)')
 metricdata.Mollview_median('cadence', '{} -  cadence'.format(dbName))
 metricdata.Mollview_median('season_length', '{} -  season length'.format(dbName))
