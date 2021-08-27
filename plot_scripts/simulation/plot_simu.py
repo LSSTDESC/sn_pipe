5dde6c09459ecd7532d5a1c2874f5fc39f3c8e2a
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-08-27 10:14:03 +0200

    code updated and cleaned

--- plot_scripts/simulation/plot_simu.py
+++ plot_scripts/simulation/plot_simu.py
@@ -1,9 +1,6 @@
-import h5py
-import numpy as np
-from astropy.table import Table, vstack
-import pprint
 from optparse import OptionParser
 from sn_plotter_simu.simuPlot import SimuPlot
+from sn_plotter_simu import plt
 
 parser = OptionParser()
 
@@ -11,17 +8,21 @@ parser.add_option("--dbName", type="str", default='alt_sched',
                   help="OS name [%default]")
 parser.add_option("--fileDir", type="str", default='Output_Simu',
                   help="dir location of the results [%default]")
+parser.add_option("--tagName", type="str", default='SNIa',
+                  help="tag name for production [%default]")
+
 
 opts, args = parser.parse_args()
 
 print('Start processing...', opts)
 
-splot = SimuPlot(opts.fileDir, opts.dbName)
-
+plt.show()
+splot = SimuPlot(opts.fileDir, opts.dbName, opts.tagName)
+# plt.show()
 # get the simulation parameters
 simupars = splot.simuPars
 
-#plt.show()
+# plt.show()
 
 print('Number of simulated supernovae', len(simupars))
 # get columns
@@ -29,16 +30,15 @@ print('Number of simulated supernovae', len(simupars))
 cols = simupars.columns
 
 print(cols)
-import matplotlib.pyplot as plt
-plt.plot(simupars['pixRA'],simupars['pixDec'],'ko')
-plt.show()
+#plt.plot(simupars['pixRA'], simupars['pixDec'], 'ko')
 print(splot.simuPars)
+# plt.show()
 
 # plotting SN parameters
 splot.plotParameters()
-
+plt.show()
 # display LC loop
-splot.plotLoopLC()
+splot.plotLoopLC(pause_time=10)
 
 
 # splot.checkLC()
