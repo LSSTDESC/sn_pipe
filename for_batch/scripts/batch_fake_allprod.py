f19a37a62bef0c4e28cae0d3d2c4b61cc514e66f
pgris <gris@clermont.in2p3.fr>
2021-09-02 15:13:40 +0200

    add script and config file to ease the production of fake SN, including obs generation, simulation (LC generation), and fit (LC fit)

---
+++ for_batch/scripts/batch_fake_allprod.py
@@ -0,0 +1,69 @@
+from optparse import OptionParser
+import pandas as pd
+import os
+
+def go(config, script, lvar):
+    """
+    Method to make and launch the script to generate fake observations
+
+    Parameters
+    ----------
+    config: pandas df
+      parameters for fake obs.
+    script: str
+      script to use for processing
+    lvar: list(str)
+      list of parameters of the script to use
+
+    """
+
+    cmd_scr = 'python {}'.format(script)
+
+    for index, row in config.iterrows():
+        cmd_ = cmd_scr
+        for var in lvar:
+            cmd_ += ' --{} {}'.format(var,row[var])
+        print(cmd_)
+        os.system(cmd_)
+
+
+
+parser = OptionParser(description='Fake generation, simulation and fit')
+
+parser.add_option("--configFile", type="str",default='for_batch/input/config_fakes.csv', help="parameters for the production [%default]")
+parser.add_option("--action", type="str",default='generation', help="action to perform (generation, simulation, fit) [%default]")
+parser.add_option("--genDir", type="str",default='/sps/lsst/users/gris/Fake_Observations', help="output dir of observations[%default]")
+parser.add_option("--simuDir", type="str",default='/sps/lsst/users/gris/Fakes/Simu', help="output dir of simulations [%default]")
+parser.add_option("--fitDir", type="str",default='/sps/lsst/users/gris/Fakes/Fit', help="output dir of fits [%default]")
+
+opts, args = parser.parse_args()
+
+configFile = opts.configFile
+action = opts.action
+genDir = opts.genDir
+simuDir = opts.simuDir
+fitDir = opts.fitDir
+
+
+#read config parameters
+
+params = pd.read_csv(configFile)
+
+print(params)
+
+if action == 'generation':
+    params['outDir'] = genDir
+    params['outName'] = params['dbName']
+    go(params,'run_scripts/fakes/gen_fake_obs.py',
+                  ['outName','Nvisits_g','Nvisits_r','Nvisits_i','Nvisits_z','Nvisits_y','bands','outDir'])
+
+if action == 'simulation':
+    params['outDir'] = simuDir
+    params['dbDir'] = genDir
+    go(params, 'for_batch/scripts/batch_fake_simu.py', ['dbName','outDir','dbDir'])
+
+if action == 'fit':
+    params['outDir'] = fitDir
+    params['simuDir'] = simuDir
+    go(params,'for_batch/scripts/batch_fake_fit.py', ['dbName','outDir','simuDir'])
+
