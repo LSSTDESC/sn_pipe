c1ae054d4c3859540cbb85f1821c1cc261a147c2
pgris <gris@clermont.in2p3.fr>
2022-01-07 15:23:47 +0100

    changed some default params

--- for_batch/scripts/launch_cosmofits.py
+++ for_batch/scripts/launch_cosmofits.py
@@ -4,14 +4,14 @@ from optparse import OptionParser
 
 
 parser = OptionParser()
-parser.add_option("--Ny", type=int, default=20,help="y-band max visits at z=0.9 [%default]")
+parser.add_option("--Ny", type=int, default=40,help="y-band max visits at z=0.9 [%default]")
 parser.add_option("--fit_parameters", type=str, default='Om,w0',
                   help="parameters to fit [%default]")
 parser.add_option("--sigma_mu_photoz", type=str, default='None',
                   help="mu error from photoz [%default]")
 parser.add_option("--sigma_mu_bias_x1_color", type=str, default='sigma_mu_bias_x1_color_1_sigma',
                   help="mu error bias from x1 and color n-sigma variation [%default]")
-parser.add_option("--outDir", type=str, default='/sps/lsst/users/gris/fake/Fit_bias',
+parser.add_option("--outDir", type=str, default='/sps/lsst/users/gris/cosmofit/Fit_bias',
                   help="output directory [%default]")
 
 opts, args = parser.parse_args()
@@ -23,7 +23,7 @@ sigma_mu_bias_x1_color = opts.sigma_mu_bias_x1_color
 outDir = opts.outDir
 
 configs = ['config_cosmoSN_universal_10.csv',
-           'config_cosmoSN_deep_rolling_0.90_0.90_2_2.csv',
+           #'config_cosmoSN_deep_rolling_0.90_0.90_2_2.csv',
            'config_cosmoSN_deep_rolling_0.80_0.80_2_2.csv',
            'config_cosmoSN_deep_rolling_2_2_mini.csv']
            #'config_cosmoSN_deep_rolling_0.80_2.csv']
@@ -31,18 +31,13 @@ configs = ['config_cosmoSN_universal_10.csv',
            #'config_cosmoSN_deep_rolling_0.80_0.80_3_3.csv']
 #configs = ['config_cosmoSN_deep_rolling_2_2_mini.csv']
 
-configs = ['config_cosmoSN_deep_rolling_0.80_0.80_yearly.csv',
+configs += ['config_cosmoSN_deep_rolling_0.80_0.80_yearly.csv',
            'config_cosmoSN_deep_rolling_2_2_mini_yearly.csv',
-           'config_cosmoSN_universal_yearly.csv']
-
-configs = ['config_cosmoSN_deep_rolling_2_2_mini_yearly.csv']
-
-configs = ['config_cosmoSN_deep_rolling_2_2_mini_0.65.csv',
-           'config_cosmoSN_deep_rolling_2_2_mini_0.60.csv',
+           'config_cosmoSN_universal_yearly.csv',
            'config_cosmoSN_deep_rolling_2_2_mini_0.65_yearly.csv',
            'config_cosmoSN_deep_rolling_2_2_mini_0.60_yearly.csv']
 
-configs = ['config_cosmoSN_deep_rolling_2_2_mini_0.65_yearly.csv']
+#configs = ['config_cosmoSN_deep_rolling_2_2_mini_0.65_yearly.csv']
 
 scr = 'python for_batch/scripts/loop_cosmo_scen.py --fileName'
 
