ffc1a522f172bd500863993b8929a8744624c31f
pgris <gris@clermont.in2p3.fr>
2022-06-28 15:38:43 +0200

    bug correction

--- for_batch/check/check_prod_metric.py
+++ for_batch/check/check_prod_metric.py
@@ -18,12 +18,18 @@ for ll in llist:
     df = pd.read_csv(ll)
     list_processed = glob.glob('{}/*'.format(opts.theDir))
     df_processed = pd.DataFrame(list_processed, columns=['dbName'])
-    df_processed['dbName'] = df.dbName.str.split('/',-1)
-    df_processed['dbName'] = df['dbName'].apply(lambda x : x[:])
-    print(df_processed)
+    df_processed = df_processed.sort_values(by=['dbName'])
+    df_processed['dbName'] = df_processed.dbName.str.split('/').str[-1]
+    #df_processed['dbName'] = df['dbName'].apply(lambda x : x[:])
     #df['processed?'] = np.where(df['dbName'] == df_processed['dbName'], 'True', 'False')
     
     df = df.merge(df_processed, left_on='dbName',right_on='dbName',how='outer')
+    df = df.dropna()
     idx = np.isin(df['dbName'],df_processed['dbName'].to_list())
     print('rrr',len(df),len(df[idx]))
-    print(df[~idx]['dbName'])
+    to_reprocess = df[~idx]
+    if len(to_reprocess) > 0:
+        vv = ['nside','coadd','nproc','simuType']
+        to_reprocess[vv] = to_reprocess[vv].astype(int)
+        to_reprocess.to_csv('torepro_{}'.format(ll),index=False)
+    
