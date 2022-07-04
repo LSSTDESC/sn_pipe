f32814031282708d41f092fd36abe0314c41facc
pgris <gris@clermont.in2p3.fr>
2022-07-04 10:53:09 +0200

    use the new batch class

--- for_batch/scripts/batch_obsTopixels.py
+++ for_batch/scripts/batch_obsTopixels.py
@@ -2,8 +2,9 @@ import os
 import numpy as np
 from optparse import OptionParser
 import pandas as pd
+from sn_tools.sn_batchutils import BatchIt
 
-
+"""
 def process(dbDir, dbName, dbExtens, scriptref, outDir, nproc,
           saveData, fieldType, simuType, nside, fieldNames, nclusters,
             RAmin,RAmax,nRA,Decmin,Decmax,nDec,radius,mode='batch'):
@@ -103,6 +104,33 @@ def cmdb(dbDir, dbName, dbExtens, scriptref, outDir, nproc,
         cmd += ' --nclusters {}'.format(nclusters)
 
     return cmd
+"""
+
+def bbatch(scriptref,params,vvars,val,bid):
+    
+    processName =  'obsTopixels_{}'.format(bid)
+    mybatch = BatchIt(processName=processName)
+    for vv in vvars:
+        params[vv] = val[vv]
+    mybatch.add_batch(scriptref,params)
+    mybatch.go_batch()
+
+def make_scr(scriptref,params,scrName):
+    script = open(scrName, "w")
+
+    script.write('#!/bin/bash \n')
+    cmd = 'python {}'.format(scriptref)
+    
+    for key,vals in params.items():
+        cmd += ' --{} {}'.format(key,vals)
+
+    script.write(cmd+'\n')
+
+
+    cmd = "srun -p htc_interactive --ntasks-per-core 8 {}".format(scrName)
+
+    print('executing',cmd)
+    os.system(cmd)
 
 
 parser = OptionParser()
@@ -123,9 +151,9 @@ parser.add_option("--RAmax", type=float, default=360.,
                   help="RA max for obs area - [%default]")
 parser.add_option("--nRA", type=int, default=10,
                   help="number of RA patches - [%default]")
-parser.add_option("--Decmin", type=float, default=-80.,
+parser.add_option("--Decmin", type=float, default=-1.0,
                   help="Dec min for obs area - [%default]")
-parser.add_option("--Decmax", type=float, default=20.,
+parser.add_option("--Decmax", type=float, default=-1.0,
                   help="Dec max for obs area - [%default]")
 parser.add_option("--nDec", type=int, default=1,
                   help="number of Dec patches - [%default]")
@@ -150,6 +178,8 @@ nRA = opts.nRA
 nDec = opts.nDec
 radius = opts.radius
 mode = opts.mode
+nclusters = opts.nclusters
+
 """
 toprocess = np.genfromtxt(dbList, dtype=None, names=[
                           'dbName', 'simuType', 'nside', 'coadd', 'fieldType', 'nproc'])
@@ -162,10 +192,43 @@ fieldNames = []
 #fieldDD = ['COSMOS', 'CDFS', 'ELAIS', 'XMM-LSS', 'ADFS1', 'ADFS2']
 fieldDD = opts.DDFs.split(',')
 
+params = {}
+params['outDir'] = outDir
+params['RAmin'] = RAmin
+params['RAmax'] = RAmax
+params['nRA'] = nRA
+params['Decmin'] = Decmin
+params['Decmax'] = Decmax
+params['nDec'] = nDec
+params['radius'] = radius
+params['saveData'] = 1
+
+vvars = ['dbDir', 'dbName', 'dbExtens','nproc','fieldType','simuType','nside']
+
 for io, val in toprocess.iterrows():
-    if val['fieldType'] == 'DD':
-        fieldNames = fieldDD
+    bid = '{}_{}_{}'.format(val['dbName'], val['nside'], val['fieldType'])
+    if mode == 'batch':
+        if val['fieldType'] == 'DD':
+            fieldNames = fieldDD
+            params['nclusters'] = nclusters
+            for fieldName in fieldNames:
+                bbid = '{}_{}'.format(bid,fneldName)
+                bbatch(scriptref,params,vvars+['fieldName'],val,bbid)
+        else:
+            bbatch(scriptref,params,vvars,val,bid)
+    else:
+        scrName = 'test_{}.sh'.format(bid)
+        for vv in vvars:
+             params[vv] = val[vv]
+        make_scr(scriptref,params,scrName)
+        os.system('chmod +x {}'.format(scrName))
+
+
+    """
     process(val['dbDir'], val['dbName'], val['dbExtens'], scriptref, outDir, val['nproc'],
               1, val['fieldType'], val['simuType'], val['nside'], fieldNames, opts.nclusters,
               RAmin,RAmax,nRA,Decmin,Decmax,nDec,radius,mode)
+    """ 
+        
+    
     
