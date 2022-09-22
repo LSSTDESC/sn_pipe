4a8417bc84a4ef8c61b507320436b541455613fd
Philippe Gris <gris@clermont.in2p3.fr>
2022-09-22 18:01:29 +0200

    add a script in metric to get stat of DDFs

---
+++ run_scripts/metrics/stat_DD.py
@@ -0,0 +1,29 @@
+import numpy as np
+from optparse import OptionParser
+import pandas as pd
+from sn_tools.sn_cadence_tools import Stat_DD
+import astropy
+import h5py
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+parser = OptionParser()
+parser.add_option("--dbList", type="str", default='List.csv',
+                  help="db name [%default]")
+
+opts, args = parser.parse_args()
+
+dbList = opts.dbList
+
+toprocess = pd.read_csv(dbList, comment='#')
+
+print('toprocess', toprocess)
+outName = 'Summary_DD.hdf5'
+file_data = h5py.File('{}'.format(outName), 'w')
+
+for i, vv in toprocess.iterrows():
+    restab = Stat_DD(vv['dbDir'], vv['dbName'], vv['dbExtens']).summary
+    thepath = 'summary_{}'.format(i)
+    print(restab)
+    astropy.io.misc.hdf5.write_table_hdf5(
+        restab, file_data, path=thepath, overwrite=True, serialize_meta=True)
