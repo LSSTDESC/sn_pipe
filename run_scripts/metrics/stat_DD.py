13f3a9689ff3ac5a22c72ca6a1fb08806ae6334a
Philippe Gris <gris@clermont.in2p3.fr>
2022-09-26 10:43:57 +0200

    a summary file is now produced

--- run_scripts/metrics/stat_DD.py
+++ run_scripts/metrics/stat_DD.py
@@ -1,7 +1,7 @@
 import numpy as np
 from optparse import OptionParser
 import pandas as pd
-from sn_tools.sn_cadence_tools import Stat_DD
+from sn_tools.sn_cadence_tools import Stat_DD_night, Stat_DD_season
 import astropy
 import h5py
 import warnings
@@ -18,12 +18,19 @@ dbList = opts.dbList
 toprocess = pd.read_csv(dbList, comment='#')
 
 print('toprocess', toprocess)
-outName = 'Summary_DD.hdf5'
-file_data = h5py.File('{}'.format(outName), 'w')
+"""
+outName = 'Summary_DD_night.hdf5'
+file_data_night = h5py.File('{}'.format(outName), 'w')
+"""
 
+restot = pd.DataFrame()
 for i, vv in toprocess.iterrows():
-    restab = Stat_DD(vv['dbDir'], vv['dbName'], vv['dbExtens']).summary
+    restab = Stat_DD_night(vv['dbDir'], vv['dbName'], vv['dbExtens']).summary
     thepath = 'summary_{}'.format(i)
     print(restab)
-    astropy.io.misc.hdf5.write_table_hdf5(
-        restab, file_data, path=thepath, overwrite=True, serialize_meta=True)
+    res = Stat_DD_season(restab)
+    # astropy.io.misc.hdf5.write_table_hdf5(
+    #    restab, file_data, path=thepath, overwrite=True, serialize_meta=True)
+    restot = pd.concat((restot, res))
+
+restot.to_hdf('Summary_DD.hdf5', key='summary')
