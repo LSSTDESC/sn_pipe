5613e7ebc282ae0eaf2913db8db829f3d9cf6e3e
Philippe Gris <gris@clermont.in2p3.fr>
2022-10-14 18:16:59 +0200

    code tuning

--- plot_scripts/metrics/plot_DD_stat_night.py
+++ plot_scripts/metrics/plot_DD_stat_night.py
@@ -2,15 +2,68 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from optparse import OptionParser
+import operator as op
 
 
-def plot(data, varx='night', vary='config', legx='night', legy='', figtit=''):
+def get_moon(data):
+
+    # get moon obs
+    idx = data['u'] > 0
+    selmoon = data[idx]
+
+    # remove the season column
+    selmoon = selmoon.drop(columns=['season'])
+
+    # restimate seasons
+    from sn_tools.sn_obs import season
+    selmoon = season(selmoon.to_records(index=False),
+                     season_gap=3, mjdCol='night')
+    moondata = pd.DataFrame(np.copy(selmoon))
+
+    moonseason = moondata.groupby(['season']).apply(lambda x: season_length(x))
+
+    res = 0.
+    if len(moonseason) > 0:
+        res = moonseason['moon_length'].median()
+
+    return res
+
+
+def season_length(grp):
+
+    nmin = grp['night'].min()
+    nmax = grp['night'].max()
+
+    return pd.DataFrame({'moon_length': [nmax-nmin+1],
+                         'night_min': nmin,
+                         'night_max': nmax})
+
+
+def select_data(data, config, ope):
+
+    idx = True
+    for key, vv in config.items():
+        print('hello', key, vv)
+        idx &= ope(data[key], vv)
+
+    return data[idx]
+
+
+def plot(data, varx='night', vary='config', legx='night', legy='', figtit='', line=[], highlight={}):
 
     fig, ax = plt.subplots(figsize=(17, 8))
     fig.suptitle(figtit)
 
-    ax.plot(data[varx], data[vary], 'ko')
+    ax.plot(data[varx], data[vary], 'ko', mfc='None')
+    if line:
+        xmin, xmax = ax.get_xlim()
+        ax.plot([xmin, xmax], line, color='r')
+    if highlight:
+        for key, vals in highlight.items():
+            seldata = select_data(data, vals['config'], vals['op'])
+            ax.plot(seldata[varx], seldata[vary], '{}*'.format(vals['color']))
     ax.set_xlabel(legx)
+    ax.set_ylabel(legy)
     ax.grid()
 
 
@@ -43,19 +96,49 @@ sel = sel.sort_values(by=['night'])
 figtitb = figtit
 cad = np.mean(np.diff(sel['night']))
 figtit += '\n mean cad: {} days'.format(np.round(cad, 1))
-plot(sel, figtit=figtit)
-dd = sel.groupby(['config']).size().to_frame('nnights').reset_index()
+print('hello', sel.columns)
+sel['nvisits'] = sel['u']+sel['g']+sel['r']+sel['i']+sel['z']+sel['y']
 
+highlight = {}
+
+hh = {}
+#hh['config'] = dict(zip('ugrizy', [0, 2, 9, 37, 52, 21]))
+hh['config'] = dict(zip('ugrizy', [0, 2, 9, 1, 1, 1]))
+hh['op'] = op.eq
+hh['color'] = 'r'
+highlight[1] = hh
+hhb = {}
+hhb['config'] = dict(zip('u', [1]))
+hhb['op'] = op.ge
+hhb['color'] = 'b'
+highlight[2] = hhb
+
+moon_dur = get_moon(sel)
+
+print('Moon', moon_dur)
+plot(sel, figtit=figtit, highlight=highlight)
+plot(sel, varx='night', vary='nvisits',
+     legx='night', legy='Nvisits', figtit=figtit, line=[], highlight=highlight)
+plot(sel, varx='moonPhase', vary='config',
+     legx='lunar phase', legy='', figtit=figtit, line=[], highlight=highlight)
+plot(sel, varx='night', vary='moonPhase',
+     legx='night', legy='lunar phase', figtit=figtit, line=[40, 40], highlight=highlight)
+dd = sel.groupby(['config', 'u', 'g', 'r', 'i', 'z', 'y']
+                 ).size().to_frame('nnights').reset_index()
+print('hhhh', dd)
 nn = 'N$_{nights}$'
 figtitb += '\n {}={}'.format(nn, len(sel))
-plot(dd, varx='nnights', legx='N$_{nights}$', figtit=figtitb)
+plot(dd, varx='nnights', legx='N$_{nights}$',
+     figtit=figtitb, highlight=highlight)
 io = dd['nnights'] <= 10
-print(len(dd[io]), len(dd[io])/len(sel), dd[~io][['nnights', 'config']])
 
-iu = sel['config'].isin(dd[~io]['config'].to_list())
-iub = sel['config'].isin(['0u-2g-9r-37i-52z-21y'])
+if len(sel) > 0:
+    print(len(dd[io]), len(dd[io])/len(sel), dd[~io][['nnights', 'config']])
 
-print('new cad', np.mean(np.diff(sel[iu]['night'])), np.mean(
-    np.diff(sel[iub]['night'])))
+    iu = sel['config'].isin(dd[~io]['config'].to_list())
+    iub = sel['config'].isin(['0u-2g-9r-37i-52z-21y'])
+
+    print('new cad', np.mean(np.diff(sel[iu]['night'])), np.mean(
+        np.diff(sel[iub]['night'])))
 
 plt.show()
--- run_scripts/metrics/make_new_OS.py
+++ run_scripts/metrics/make_new_OS.py
@@ -5,7 +5,7 @@ import numpy.lib.recfunctions as rf
 
 
 class newOS:
-    def __init__(self, sum_night, data_OS, outName):
+    def __init__(self, sum_night, data_OS, outName, lunar_phase=40, median_ref=dict(zip('grizy', [24.49, 24.04, 23.6, 22.98, 22.14]))):
         """
         class to make a "new" OS for an original one (data_OS)
 
@@ -15,26 +15,42 @@ class newOS:
            OS summary per night
         data_OS: numpy array
            original OS data
-
+        outName: str
+          file outputName
+        lunar_phase: float, opt
+          lunar phase threshold for u-band swapping
         """
         # grab original data types
         dtypes = data_OS.dtype
 
-        # rename DDF if necessary
-        self.ref_config = ['0u-2g-9r-37i-52z-21y',
-                           '1u-2g-9r-37i-0z-21y', '2u-2g-9r-37i-0z-21y']
-        new_config = '0u-2g-9r-37i-52z-21y'
-        # grap the number of visits required per band
-        self.Nvisits = self.grab_nvisits(new_config)
+        self.lunar_phase = lunar_phase
+        self.median_ref = median_ref
 
-        #self.data = data_OS
+        self.ref_config = {}
+        self.ref_config['DD:COSMOS'] = {}
+        self.ref_config['DD:COSMOS']['config'] = '0u-2g-9r-37i-52z-21y'
+        self.ref_config['DD:COSMOS']['lunar_config'] = '1u-2g-9r-37i-0z-21y'
+
+        self.ref_config['DD:XMM_LSS'] = {}
+        self.ref_config['DD:XMM_LSS']['config'] = '0u-2g-9r-37i-52z-21y'
+        self.ref_config['DD:XMM_LSS']['lunar_config'] = '1u-2g-9r-37i-0z-21y'
+
+        self.ref_config['DD:EDFS_a'] = {}
+        self.ref_config['DD:EDFS_a']['config'] = '0u-2g-9r-1i-1z-1y'
+        self.ref_config['DD:EDFS_a']['lunar_config'] = '1u-2g-9r-1i-0z-1y'
+
+        self.ref_config['DD:EDFS_b'] = {}
+        self.ref_config['DD:EDFS_b']['config'] = '0u-2g-9r-1i-1z-1y'
+        self.ref_config['DD:EDFS_b']['lunar_config'] = '1u-2g-9r-1i-0z-1y'
+
+        # self.data = data_OS
         # extract non ddf data
-        #idx = data_OS['note'].isin(sum_night['field'])
+        # idx = data_OS['note'].isin(sum_night['field'])
         idx = np.in1d(data_OS['note'], sum_night['field'].to_list())
         self.data_DD = pd.DataFrame(data_OS[idx])
         data_non_DD = data_OS[~idx]
 
-        #idx = sum_night['field'] == 'DD:XMM_LSS'
+        # idx = sum_night['field'] == 'DD:XMM_LSS'
 
         res = sum_night.groupby('field').apply(
             lambda x: self.make_new_OS(x)).reset_index()
@@ -53,20 +69,27 @@ class newOS:
         cols.remove('index')
 
         trans = np.copy(res[cols].to_records(index=False))
+
         """
         trans = rf.append_fields(
             trans, 'note', res['note'].to_list(), dtypes=['<U15'])
         trans = rf.append_fields(trans, 'band', res['band'].to_list())
         """
         # add non ddf data
-        #print(trans.dtype, trans['note'])
-        #print(data_non_DD.dtype, data_non_DD['note'])
+        # print(trans.dtype, trans['note'])
+        # print(data_non_DD.dtype, data_non_DD['note'])
+        print(trans.dtype)
+        print(data_non_DD[cols].dtype)
+
         trans = np.concatenate((trans, data_non_DD[cols]))
         trans = rf.append_fields(
             trans, 'note', res['note'].to_list()+data_non_DD['note'].tolist())
         trans = rf.append_fields(
             trans, 'band', res['band'].to_list()+data_non_DD['band'].tolist())
-
+        """
+        trans = rf.append_fields(trans, 'note', res['note'].to_list())
+        trans = rf.append_fields(trans, 'band', res['band'].to_list())
+        """
         np.save(outName, np.copy(trans))
 
     def grab_nvisits(self, config):
@@ -87,17 +110,20 @@ class newOS:
         idx = self.data_DD['note'] == DDF
         data_DDF = self.data_DD[idx]
 
+        # select configuration for cadence
+        config_cadence = self.ref_config[DDF]
+
         print(len(np.unique(sum_night['night'])),
               len(np.unique(data_DDF['night'])))
 
         res = sum_night.groupby('season').apply(
-            lambda x: self.make_new_OS_season(x, data_DDF)).reset_index(drop=True)
+            lambda x: self.make_new_OS_season(x, data_DDF, config_cadence)).reset_index(drop=True)
 
         res['note'] = DDF
 
         return res
 
-    def make_new_OS_season(self, sum_night_season, data_DDF):
+    def make_new_OS_season(self, sum_night_season, data_DDF, config_cadence):
 
         season = sum_night_season.name
 
@@ -107,24 +133,59 @@ class newOS:
 
         # grab median values per seasons
         colint = sel_DDF.select_dtypes(include=['int64']).columns
-        meds = sel_DDF.groupby('band').median().reset_index()
-        for col in colint:
-            meds[col] = meds[col].astype(int)
+        # get the list of nights depending on the moon phase
+        idx = sel_DDF['moonPhase'] < self.lunar_phase
 
-        meds = meds.drop(columns=['night'])
+        what = ['fiveSigmaDepth', 'band']
+        meds_moon = sel_DDF[idx][what].groupby('band').median().reset_index()
+        meds_nomoon = sel_DDF[~idx][what].groupby(
+            'band').median().reset_index()
 
+        for b in 'grizy':
+            idxx = meds_moon['band'] == b
+            if len(meds_moon[idxx]) == 0:
+                dd = pd.DataFrame([[b, self.median_ref[b]]], columns=[
+                                  'band', 'fiveSigmaDepth'])
+                meds_moon = pd.concat((meds_moon, dd))
+            idxx = meds_nomoon['band'] == b
+            if len(meds_nomoon[idxx]) == 0:
+                dd = pd.DataFrame([[b, self.median_ref[b]]], columns=[
+                                  'band', 'fiveSigmaDepth'])
+                meds_nomoon = pd.concat((meds_nomoon, dd))
+        """
         # get the list of nights with the "good" and "bad" filter combination
         idf = sum_night_season['config'].isin(self.ref_config)
         good_nights = sum_night_season[idf]['night']
         bad_nights = sum_night_season[~idf]['night']
+        """
 
+        data_moon = sel_DDF[idx]
+        data_nomoon = sel_DDF[~idx]
+        list1 = np.unique(data_moon['night'])
+        list2 = np.unique(data_nomoon['night'])
+        intersect = list(set(list1).intersection(list2))
+        idx = data_nomoon['night'].isin(intersect)
+        data_nomoon = data_nomoon[~idx]
+
+        new_data = pd.DataFrame()
+        if len(data_moon) > 0:
+            print('processing data moon')
+            res = data_moon.groupby('night').apply(
+                lambda x: self.make_new_OS_night(x, meds_moon, config_cadence['lunar_config'])).reset_index(level=0)
+            new_data = pd.concat((new_data, res))
+        if len(data_nomoon) > 0:
+            print('processing data no moon')
+            res = data_nomoon.groupby('night').apply(
+                lambda x: self.make_new_OS_night(x, meds_nomoon, config_cadence['config'])).reset_index(level=0)
+            new_data = pd.concat((new_data, res))
+
+        """
         # good nights: leave data as it is
         idx_good = sel_DDF['night'].isin(good_nights)
         new_data = sel_DDF[idx_good]
 
         # bad nights: make new OS from ref
         bad_data = sel_DDF[~idx_good]
-
         if len(bad_data) > 0:
             res = bad_data.groupby('night').apply(
                 lambda x: self.make_new_OS_night(x, meds)).reset_index(level=0)
@@ -136,25 +197,47 @@ class newOS:
                 print('probleme here', nanc)
                 print(res['night'])
             new_data = pd.concat((new_data, res))
-
+        """
         return new_data
 
-    def make_new_OS_night(self, grp, meds):
+    def make_new_OS_night(self, grp, meds_season, config):
 
         night = grp.name
 
         # now build OS
 
         totdf = pd.DataFrame()
-        for key, nv in self.Nvisits.items():
-            idx = meds['band'] == key
-            df = meds[idx]
-            if not df.empty:
-                newdf = pd.DataFrame()
 
-                if nv > 0:
-                    newdf = pd.concat([df]*nv, ignore_index=True)
-                    totdf = pd.concat((totdf, newdf))
+        meds = grp.groupby('band').median().reset_index()
+        selmed = grp.drop(columns=['band', 'note', 'night'])
+        meds_all = pd.DataFrame(
+            [selmed.median().to_list()], columns=selmed.columns)
+        colint = grp.select_dtypes(include=['int64']).columns
+
+        for col in colint:
+            meds[col] = meds[col].astype(int)
+            if col in meds_all.columns:
+                meds_all[col] = meds_all[col].astype(int)
+
+        meds = meds.drop(columns=['night'])
+
+        visits = self.grab_nvisits(config)
+
+        for key, nv in visits.items():
+            if nv > 0:
+                idx = meds['band'] == key
+                df = meds[idx]
+                if df.empty:
+                    df = meds_all
+                    df['band'] = key
+                    df['note'] = grp['note'].unique()
+                    io = meds_season['band'] == key
+                    selmeds = meds_season[io]
+                    if len(selmeds) > 0:
+                        df['fiveSigmaDepth'] = selmeds['fiveSigmaDepth']
+                df['observationId'] = 0
+                newdf = pd.concat([df]*nv, ignore_index=True)
+                totdf = pd.concat((totdf, newdf))
 
         return totdf
 
@@ -190,4 +273,5 @@ data_OS = np.load('{}/{}.npy'.format(dbDir, dbName), allow_pickle=True)
 
 print(np.unique(data_OS['note']))
 
+idd = sum_night['field'] == 'DD:EDFS_a'
 nOS = newOS(sum_night, data_OS, outName)
