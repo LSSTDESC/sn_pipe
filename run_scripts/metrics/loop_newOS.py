6a21d1600f2ca44e7b501a71ee873b1322e76d11
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2022-10-25 18:11:34 +0200

    add configDir in the script

--- run_scripts/metrics/loop_newOS.py
+++ run_scripts/metrics/loop_newOS.py
@@ -1,7 +1,7 @@
 import os
 
 cmd_scr = 'python run_scripts/metrics/make_new_OS.py'
-
+configDir = 'input/metrics'
 configs = ['config_newOS.yaml', 'config_newOS_double.yaml']
 
 for b in 'riy':
@@ -11,5 +11,6 @@ for b in 'riy':
 
 for lp in [0.10, 0.20, 0.30, 0.40]:
     for conf in configs:
-        cmd = '{} --config {} --lunar_phase {}'.format(cmd_scr, conf, lp)
+        cmd = '{} --config {}/{} --lunar_phase {}'.format(
+            cmd_scr, configDir, conf, lp)
         os.system(cmd)
