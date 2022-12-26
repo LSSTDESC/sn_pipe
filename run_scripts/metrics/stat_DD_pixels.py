9fe1890aa7e2dfe4690992d96204474bfafc425a
Gris <philippe.gris@clermont.in2p3.fr>
2022-12-26 11:26:47 +0100

    code update - only the db is needed now!

--- run_scripts/metrics/stat_DD_pixels.py
+++ run_scripts/metrics/stat_DD_pixels.py
@@ -6,6 +6,55 @@ from sn_tools.sn_cadence_tools import stat_DD_night_pixel, stat_DD_season_pixel
 from optparse import OptionParser
 from sn_tools.sn_io import make_dict_from_config,add_parser
 from sn_tools.sn_process import FP2pixels
+from sn_tools.sn_obs import ProcessPixels
+
+def transform(pixels,obs):
+    """
+    Function to transform the pixels df
+
+    Parameters
+    ----------
+    pixels: pandas df
+      data to transform
+    
+
+    """
+
+    # link pixels <-> observations
+    res = pixels.groupby(['healpixID','pixRA','pixDec','fieldName']).apply(lambda x: get_obsid(x)).reset_index()
+
+    # get mean values for observations
+    obsCol = ['observationStartMJD', 'fieldRA', 'fieldDec',
+                       'visitExposureTime', 'fiveSigmaDepth', 'numExposures', 'seeingFwhmEff']
+
+    obsm = pd.DataFrame(obs)
+    obsm = obsm.groupby(['observationId','filter','night'])[obsCol].mean().reset_index()
+
+    res = res.merge(obsm, left_on=['observationId'],right_on=['observationId'])
+    
+    return res
+
+def get_obsid(grp):
+    """
+    Function to get obsids and make a panda df
+
+    Parameters
+    ----------
+    grp: pandas df
+      data to process
+
+    Returns
+    -------
+    pandas df with observationId
+    """
+
+    obsids = ProcessPixels.getList(grp)
+    
+        
+    tt = pd.DataFrame(obsids,columns=['observationId'])
+
+    return tt
+    
 
 confDict_gen = make_dict_from_config('input/obs_pixelize', 'config_obs_pixelize.txt')
 parser = OptionParser()
@@ -38,9 +87,17 @@ for fieldName in fieldNames:
     dict_process['fieldName']=fieldName
     obs2pixels = FP2pixels(**dict_process)
 
+    observations = obs2pixels.obs
     pixels = obs2pixels()
     pixels['fieldName'] = fieldName
-    print(type(pixels),pixels.columns)
+    pixels = transform(pixels,observations)
+    resdf = stat_DD_night_pixel(pixels,opts.dbName)
+    restot = pd.concat((restot, resdf))
+
+
+restot.to_hdf(opts.outName, key='summary')
+
+    
 """
 for i, row in dfdb.iterrows():
     # processing obspixelfiles
