30bbedce26461b1e8a9bc706c5c6b16a10a5df04
Philippe Gris <gris@clermont.in2p3.fr>
2023-01-04 10:06:52 +0100

    code update: parameters set from a script

--- run_scripts/fakes/make_fake.py
+++ run_scripts/fakes/make_fake.py
@@ -1,9 +1,41 @@
-import yaml
 import numpy as np
-from sn_tools.sn_cadence_tools import GenerateFakeObservations
 from optparse import OptionParser
-import numpy.lib.recfunctions as rf
+from sn_tools.sn_io import make_dict_from_config
+from sn_tools.sn_fake_utils import FakeObservations, add_option, config
+import sn_script_input
 
+
+# this is to load option for fake cadence
+path = sn_script_input.__path__
+confDict_fake = make_dict_from_config(path[0], 'config_cadence.txt')
+
+
+parser = OptionParser(description='Script to generate fake observations.')
+add_option(parser, confDict_fake)
+
+opts, args = parser.parse_args()
+
+config_fake = config(confDict_fake, opts)
+
+# generate fake obs
+fakeData = FakeObservations(config_fake).obs
+print('fake Data', len(fakeData))
+
+if opts.saveData:
+    #save script parameters
+    outDir = opts.outDir
+    outName = opts.outName
+    from sn_tools.sn_io import checkDir
+    checkDir(outDir)
+    import yaml
+    outputyaml = '{}/{}.yaml'.format(outDir, outName)
+    with open(outputyaml, 'w') as file:
+        documents = yaml.dump(config_fake, file)
+    # save fake obs
+    path = '{}/{}.npy'.format(outDir,outName)
+    np.save(path,np.copy(fakeData))
+
+"""
 parser = OptionParser()
 
 parser.add_option("--config", type="str", default='input/Fake_cadence/Fake_cadence_seqs.yaml',
@@ -22,7 +54,7 @@ seqs = opts.seqs
 
 config = yaml.load(open(configName), Loader=yaml.FullLoader)
 
-# print(config)
+print(config)
 mygen = GenerateFakeObservations(config, sequences=seqs).Observations
 
 # print(mygen)
@@ -39,3 +71,4 @@ mygen = rf.append_fields(mygen, 'Dec', mygen['fieldRA'])
 # print(mygen.dtype)
 
 np.save('{}.npy'.format(outputName), np.copy(mygen))
+"""
