d8f56b8d94184162db7c99ee52d3fe1ac1921669
pgris <gris@clermont.in2p3.fr>
2023-02-10 18:16:44 +0100

    code cleaning

--- plot_scripts/sn/sn_analyzer.py
+++ plot_scripts/sn/sn_analyzer.py
@@ -38,6 +38,14 @@ def loadData(theDir, dbName):
     return res.to_pandas()
 
 
+def load_complete(dbDir, dbName, alpha=0.4, beta=3):
+
+    res = loadData(dbDir, dbName)
+    res = complete_df(res, alpha, beta)
+
+    return res
+
+
 def complete_df(res, alpha=0.4, beta=3):
     """
     Function to complete df infos
@@ -232,21 +240,103 @@ def plotSN_effi(data, xvar='n_epochs_aft', bins=range(1, 20, 1),
     ax.plot(bins[:-1], effi, 'ko')
 
 
-dbDir = 'Output_SN'
-dbName = 'draft_connected_v2.99_10yrs'
-res = loadData(dbDir, dbName)
-res = complete_df(res, alpha=0.4, beta=3)
+def effi(resa, resb, xvar='z', bins=np.arange(0.01, 1.1, 0.02)):
 
-histSN_params(res)
-plotSN_2D(res)
+    groupa = resa.groupby(pd.cut(resa[xvar], bins))
+    groupb = resb.groupby(pd.cut(resb[xvar], bins))
+
+    effi = groupb.size()/groupa.size()
+
+    bin_centers = (bins[: -1] + bins[1:])/2
+
+    return bin_centers, effi
+
+
+def plot_effi(resa, resb, xvar='z', leg='',
+              bins=np.arange(0.01, 1.1, 0.02), fig=None, ax=None):
+
+    if fig is None:
+        fig, ax = plt.subplots(figsize=(10, 8))
+
+    x, y = effi(resa, resb, xvar=xvar, bins=bins)
+
+    ax.plot(x, y, label=leg)
+
+
+def select(res, list_sel):
+    """
+    Function to select a pandas df
+
+    Parameters
+    ----------
+    res : pandas df
+        data to select.
+
+    Returns
+    -------
+    pandas df
+        selected df.
+
+    """
+    idx = True
+    for vals in list_sel:
+        idx &= vals[1](res[vals[0]], vals[2])
+
+    return res[idx]
+
+
+dbName = 'draft_connected_v2.99_10yrs'
+res = load_complete('Output_SN', dbName)
+res_fast = load_complete('Output_SN_fast', dbName)
+
+histSN_params(res_fast)
+plotSN_2D(res_fast)
 print(res.columns)
 
-idx = res['n_epochs_phase_minus_10'] >= 1
-idx &= res['n_epochs_phase_plus_20'] >= 1
-idx &= res['n_epochs_bef'] >= 4
-idx &= res['n_epochs_aft'] >= 10
-sel = res[idx]
+dict_sel = {}
 
+dict_sel['G10'] = [('n_epochs_m10_p35', operator.ge, 4),
+                   ('n_epochs_m10_p5', operator.ge, 1),
+                   ('n_epochs_p5_p20', operator.ge, 1),
+                   ('n_bands_m8_p10', operator.ge, 2),
+                   ('sigmaC', operator.le, 0.04)]
+
+sel = select(res, dict_sel['G10'])
+fig, ax = plt.subplots()
+ax.hist(sel['n_epochs_aft'], histtype='step')
+
+fig, ax = plt.subplots()
+ax.hist(sel['n_epochs_bef'], histtype='step')
+
+plt.show()
+combi = []
+for nbef in range(3, 5):
+    for naft in range(3, 11):
+        combi.append((nbef, naft))
+
+combi = [(3, 8), (4, 10), (3, 6)]
+for (nbef, naft) in combi:
+    print(nbef, naft)
+    seltype = 'metric_{}_{}'.format(nbef, naft)
+    dict_sel[seltype] = [('n_epochs_phase_minus_10', operator.ge, 1),
+                         ('n_epochs_phase_plus_20', operator.ge, 1),
+                         ('n_epochs_bef', operator.ge, nbef),
+                         ('n_epochs_aft', operator.ge, naft),
+                         ('sigmaC', operator.le, 0.04)]
+
+fig, ax = plt.subplots(figsize=(10, 8))
+for key, vals in dict_sel.items():
+    sel = select(res, vals)
+    selfast = select(res_fast, vals)
+    print(key, len(sel), len(selfast))
+    plot_effi(res, sel, leg=key, fig=fig, ax=ax)
+    plot_effi(res, selfast, leg=key, fig=fig, ax=ax)
+
+ax.legend()
+
+# plt.show()
+
+"""
 plotSN_2D(sel, vary='sigmaC', legy='$\sigma_C$')
 plotSN_2D(sel, vary='sigmat0', legy='$\sigma_{T_0}$')
 plotSN_2D_binned(sel, bins=np.arange(0.1, 0.7, 0.01),
@@ -254,9 +344,15 @@ plotSN_2D_binned(sel, bins=np.arange(0.1, 0.7, 0.01),
 plotSN_2D(sel, varx='n_epochs_aft',
           legx='N$_{epochs}^{aft}$', vary='sigmaC', legy='$\sigma_C$')
 
+"""
+
+
+"""
 plotSN_2D(sel, varx='n_epochs_bef',
           legx='N$_{epochs}^{bef}$', vary='sigmaC', legy='$\sigma_C$')
-
-
+"""
+plt.show()
+"""
 plotSN_effi(sel)
 plt.show()
+"""
