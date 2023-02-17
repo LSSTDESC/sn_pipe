efedb4792ca4055b4eca4da3a2c9fbfb52006203
pgris <gris@clermont.in2p3.fr>
2023-02-17 18:25:24 +0100

    add estimation of the main telescope parameters

--- run_scripts/utils/telescope_params.py
+++ run_scripts/utils/telescope_params.py
@@ -1,9 +1,22 @@
 from sn_telmodel.sn_telescope import Telescope
-
+import pandas as pd
 telescope = Telescope(airmass=1.2)
 
 bands = 'ugrizy'
-zp = dict(zip(bands, [telescope.zp(b) for b in bands]))
 
-print('zero points', zp)
-print('mean wavelength', telescope.mean_wavelength)
+exptime = 600
+df = pd.DataFrame(['u', 'g', 'r', 'i', 'z', 'y'], columns=['band'])
+zp = dict(zip(bands, [telescope.zp(b) for b in bands]))
+mag_sky = dict(zip(bands, [telescope.mag_sky(b) for b in bands]))
+flux_sky = dict(zip(bands, [telescope.flux_sky(b, exptime) for b in bands]))
+m5 = dict(zip(bands, [telescope.m5(b, exptime) for b in bands]))
+
+df['zp (AB)'] = [telescope.zp(b) for b in bands]
+df['msky'] = [telescope.mag_sky(b) for b in bands]
+df['flux_sky'] = [telescope.flux_sky(b, exptime) for b in bands]
+df['FWHMeff'] = [telescope.FWHMeff(b) for b in bands]
+df['m5'] = [telescope.m5(b, exptime) for b in bands]
+
+
+df = df.round(2)
+print(df.to_string(index=False))
