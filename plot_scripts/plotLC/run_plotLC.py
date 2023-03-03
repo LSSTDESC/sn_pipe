36227f37b95dab05d7df3a7e72319d8f2cdb8cf2
pgris <gris@clermont.in2p3.fr>
2023-03-03 13:40:25 +0100

    possible to link LC to SN

--- plot_scripts/plotLC/run_plotLC.py
+++ plot_scripts/plotLC/run_plotLC.py
@@ -7,15 +7,22 @@ parser.add_option('--metaFileInput', type=str, default='Meta_fit.hdf5',
                   help='meta file name to process [%default]')
 parser.add_option('--metaDirInput', type=str, default='dataLC',
                   help='meta dir [%default]')
+parser.add_option('--SNFileInput', type=str, default='None',
+                  help='SN file[%default]')
+parser.add_option('--SNDirInput', type=str, default='None',
+                  help='SN dir [%default]')
 
 opts, args = parser.parse_args()
 
-metaFileInput = opts.metaFileInput
-metaDirInput = opts.metaDirInput
+opts_dict = vars(opts)
 
-visu = VisuLC(metaFileInput, metaDirInput)
+visu = VisuLC(**opts_dict)
 
 while 1:
     answer = input('SN to plot? ')
 
-    visu.plot(answer)
+    if answer != 'exit':
+        visu.plot(answer)
+
+    else:
+        break
