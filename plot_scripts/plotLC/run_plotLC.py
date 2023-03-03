84738f7e49431ee7e5ce190892094617d86be6fe
pgris <gris@clermont.in2p3.fr>
2023-03-03 17:42:08 +0100

    plot improvement

--- plot_scripts/plotLC/run_plotLC.py
+++ plot_scripts/plotLC/run_plotLC.py
@@ -3,13 +3,13 @@ from optparse import OptionParser
 
 parser = OptionParser()
 
-parser.add_option('--metaFileInput', type=str, default='Meta_fit.hdf5',
+parser.add_option('--metaFile', type=str, default='Meta_fit.hdf5',
                   help='meta file name to process [%default]')
-parser.add_option('--metaDirInput', type=str, default='dataLC',
+parser.add_option('--metaDir', type=str, default='dataLC',
                   help='meta dir [%default]')
-parser.add_option('--SNFileInput', type=str, default='None',
+parser.add_option('--SNFile', type=str, default='None',
                   help='SN file[%default]')
-parser.add_option('--SNDirInput', type=str, default='None',
+parser.add_option('--SNDir', type=str, default='None',
                   help='SN dir [%default]')
 
 opts, args = parser.parse_args()
--- plot_scripts/sn/sn_to_lc.py
+++ plot_scripts/sn/sn_to_lc.py
@@ -1,14 +1,17 @@
+from sn_plotter_simu.visuLC import SNToLC
 from optparse import OptionParser
 
 parser = OptionParser()
 
-parser.add_option('--LCDirInput', type=str, default='dataLC',
+parser.add_option('--metaDir', type=str, default='dataLC',
                   help='meta dir [%default]')
-parser.add_option('--SNFileInput', type=str, default='None',
+parser.add_option('--SNFile', type=str, default='None',
                   help='SN file[%default]')
-parser.add_option('--SNDirInput', type=str, default='None',
+parser.add_option('--SNDir', type=str, default='None',
                   help='SN dir [%default]')
 
 opts, args = parser.parse_args()
 
 opts_dict = vars(opts)
+
+SNToLC(**opts_dict)
