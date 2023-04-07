f0c0ef2f7786f101c37dfa65810370bc8e32fecc
pgris <gris@clermont.in2p3.fr>
2023-04-07 16:44:06 +0200

    add possibility to process multiple files

--- plot_scripts/sn/sn_analyzer_fakes.py
+++ plot_scripts/sn/sn_analyzer_fakes.py
@@ -10,6 +10,46 @@ import matplotlib.pyplot as plt
 import operator
 import numpy as np
 from sn_analysis.sn_calc_plot import Calc_zlim, histSN_params, select, plot_effi, effi
+import glob
+import pandas as pd
+
+
+def gime_zlim(df):
+
+    df['sigmaC'] = np.sqrt(df['Cov_colorcolor'])
+    # simulation parameters
+    # histSN_params(df)
+
+    dict_sel = {}
+
+    dict_sel['G10'] = [('n_epochs_m10_p35', operator.ge, 4),
+                       ('n_epochs_m10_p5', operator.ge, 1),
+                       ('n_epochs_p5_p20', operator.ge, 1),
+                       ('n_bands_m8_p10', operator.ge, 2),
+                       ('sigmaC', operator.le, 0.04),
+                       ]
+
+    dict_sel['metric'] = [('n_epochs_bef', operator.ge, 4),
+                          ('n_epochs_aft', operator.ge, 10),
+                          ('n_epochs_phase_minus_10', operator.ge, 1),
+                          ('n_epochs_phase_plus_20', operator.ge, 1),
+                          ('sigmaC', operator.le, 0.04),
+                          ]
+
+    sel = select(df, dict_sel['metric'])
+
+    """
+    fig, ax = plt.subplots()
+    plot_effi(df, sel, leg='test', fig='tt', ax=ax)
+    plot_2D(df)
+    """
+    effival = effi(df, sel)
+    mycl = Calc_zlim(effival)
+    zlim = mycl.zlim
+
+    # mycl.plot_zlim(zlim)
+
+    return zlim
 
 
 def plot_2D(res, varx='z', legx='$', vary='sigmaC',
@@ -24,40 +64,29 @@ def plot_2D(res, varx='z', legx='$', vary='sigmaC',
 
 
 theDir = 'Output_SN/Fakes'
-theFile = 'SN_conf_z_moon_-1_full_salt3.hdf5'
-df = loadData_fakeSimu(theDir, theFile)
 
-df['sigmaC'] = np.sqrt(df['Cov_colorcolor'])
-# simulation parameters
-histSN_params(df)
+fis = glob.glob('{}/SN_*.hdf5'.format(theDir))
+# theFile = 'SN_conf_z_moon_-1_full_salt3.hdf5'
 
-dict_sel = {}
+r = []
+for fi in fis:
+    ffi = fi.split('/')[-1]
+    hname = ffi.split('_')[4]
+    df = loadData_fakeSimu(theDir, ffi)
+    zlim = gime_zlim(df)
+    print(hname, zlim)
+    r.append((int(hname), zlim))
 
-dict_sel['G10'] = [('n_epochs_m10_p35', operator.ge, 4),
-                   ('n_epochs_m10_p5', operator.ge, 1),
-                   ('n_epochs_p5_p20', operator.ge, 1),
-                   ('n_bands_m8_p10', operator.ge, 2),
-                   ('sigmaC', operator.le, 0.04),
-                   ]
+df = pd.DataFrame.from_records(r, columns=['moon_frac', 'zlim'])
 
-dict_sel['metric'] = [('n_epochs_bef', operator.ge, 4),
-                      ('n_epochs_aft', operator.ge, 10),
-                      ('n_epochs_phase_minus_10', operator.ge, 1),
-                      ('n_epochs_phase_plus_20', operator.ge, 1),
-                      ('sigmaC', operator.le, 0.04),
-                      ]
-
-sel = select(df, dict_sel['metric'])
-
-fig, ax = plt.subplots()
-plot_effi(df, sel, leg='test', fig='tt', ax=ax)
-plot_2D(df)
-
-effival = effi(df, sel)
-mycl = Calc_zlim(effival)
-zlim = mycl.zlim
-mycl.plot_zlim(zlim)
-print('ooo', zlim)
-
-plt.show()
+idx = df['moon_frac'] < 0
+zlim_ref = df[idx]['zlim'].values[0]
+print(zlim_ref)
+df['delta_zlim'] = df['zlim']-zlim_ref
 print(df)
+fig, ax = plt.subplots()
+idx = df['moon_frac'] > 0
+sel = df[idx]
+sel = sel.sort_values(by=['moon_frac'])
+ax.plot(sel['moon_frac'], sel['delta_zlim'])
+plt.show()
