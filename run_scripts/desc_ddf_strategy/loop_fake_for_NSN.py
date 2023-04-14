1f47f00286e9d1fd28f98c072b6185c2d48256b5
pgris <gris@clermont.in2p3.fr>
2023-04-14 17:51:35 +0200

    add a script to produce sn with various fake config

---
+++ run_scripts/desc_ddf_strategy/loop_fake_for_NSN.py
@@ -0,0 +1,19 @@
+import numpy as np
+import os
+scr_m = 'python run_scripts/sim_to_fit/run_test.py --scriptName moon_run_b.sh --show_results 0 --config_obs=input/DESC_cohesive_strategy/combi_obs.csv --config_simu=input/DESC_cohesive_strategy/combi_simufit.csv --SN_NSNfactor 300 --SN_x1_type=random --SN_color_type=random --SN_z_type=random  --SN_daymax_type=random --mooncompensate=1'
+
+dz = 0.1
+zmin = 0.01
+zmax = 1.1
+zvals = np.arange(zmin, zmax, dz)
+
+for io in range(len(zvals)-1):
+    zzmin = np.round(zvals[io], 2)
+    zzmax = np.round(zvals[io+1], 2)
+    add_tag = '{}_{}'.format(np.round(zzmin, 2), np.round(zzmax, 2))
+    scr = scr_m
+    scr += ' --SN_z_min={}'.format(zzmin)
+    scr += ' --SN_z_max={}'.format(zzmax)
+    scr += ' --add_tag={}'.format(add_tag)
+    print(scr)
+    os.system(scr)
