3460e0ae3f1e9eb4aa34b2705534f450f83e999c
pgris <gris@clermont.in2p3.fr>
2023-04-27 15:25:25 +0200

    add a script to estimate zlim vs conf

---
+++ run_scripts/desc_ddf_strategy/loop_fake_for_zlim.py
@@ -0,0 +1,6 @@
+import numpy as np
+import os
+
+scr_m = 'python run_scripts/sim_to_fit/run_test.py --scriptName moon_run_b.sh --show_results 0 --config_obs=input/DESC_cohesive_strategy/combi_obs.csv --config_simu=input/DESC_cohesive_strategy/combi_simufit.csv --SN_x1_type=unique --SN_color_type=unique --SN_z_type=uniform  --SN_z_min=0.01 --SN_z_max=0.9 --SN_daymax_type=unique --mooncompensate=1 --SN_NSNabsolute 1'
+#scr_m = 'python run_scripts/sim_to_fit/run_test.py --show_results=0 --scriptName=moon_run.sh --config_obs=input/DESC_cohesive_strategy/combi_obs.csv --config_simu=input/DESC_cohesive_strategy/combi_simufit.csv'
+os.system(scr_m)
