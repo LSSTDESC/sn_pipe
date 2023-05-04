6f05d450a0c221c7a900df23cd85558494f034ea
Gris <philippe.gris@clermont.in2p3.fr>
2023-05-04 14:56:09 +0200

    add the number of procs as parameter

--- run_scripts/desc_ddf_strategy/loop_fake_for_NSN.py
+++ run_scripts/desc_ddf_strategy/loop_fake_for_NSN.py
@@ -1,6 +1,12 @@
 import numpy as np
 import os
-scr_m = 'python run_scripts/sim_to_fit/run_test.py --scriptName moon_run_b.sh --show_results 0 --config_obs=input/DESC_cohesive_strategy/combi_obs.csv --config_simu=input/DESC_cohesive_strategy/combi_simufit.csv --SN_NSNfactor 300 --SN_x1_type=random --SN_color_type=random --SN_z_type=random  --SN_daymax_type=random --mooncompensate=1'
+scr_m = 'python run_scripts/sim_to_fit/run_test.py \
+        --scriptName moon_run_b.sh --show_results 0 \
+        --config_obs=input/DESC_cohesive_strategy/combi_obs.csv \
+        --config_simu=input/DESC_cohesive_strategy/combi_simufit.csv \
+        --SN_NSNfactor 300 --SN_x1_type=random --SN_color_type=random \
+        --SN_z_type=random --SN_daymax_type=random'
+scr_m += ' --mooncompensate=1 --MultiprocessingFit_nproc 12'
 
 dz = 0.1
 zmin = 0.01
