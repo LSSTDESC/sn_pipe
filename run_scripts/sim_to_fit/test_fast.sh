59170d78aac264402159dad3cffdf0f2652dc8c8
pgris <gris@clermont.in2p3.fr>
2023-05-15 09:07:51 +0200

    add two scripts to test sim_to_fit with fake data; mv run_test.py to run_fake.py

---
+++ run_scripts/sim_to_fit/test_fast.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run_scripts/sim_to_fit/run_fake.py --SN_NSNabsolute=1 --MultiprocessingSimu_nproc=8
---
+++ run_scripts/sim_to_fit/test_slower.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run_scripts/sim_to_fit/run_fake.py --SN_NSNabsolute=1 --obsFromSimu=1 --SN_daymax_type=uniform --MultiprocessingFit_nproc=8
