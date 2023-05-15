a4a174f1cb8f0cfb0137688cdb079626948dceec
pgris <gris@clermont.in2p3.fr>
2023-05-15 11:04:16 +0200

    m5 values are chosen from files in input/m5_OS

--- run_scripts/sim_to_fit/test_fast.py
+++ run_scripts/sim_to_fit/test_fast.py
@@ -1,3 +1,40 @@
 #!/bin/bash
+import os
+import pandas as pd
+from optparse import OptionParser
 
-python run_scripts/sim_to_fit/run_fake.py --SN_NSNabsolute=1 --MultiprocessingSimu_nproc=8
+
+parser = OptionParser()
+
+parser.add_option("--m5_file", type=str,
+                  default='input/m5_OS/m5_field_season_baseline_v3.0_10yrs.csv',
+                  help="file to process [%default]")
+parser.add_option("--ref_field", type=str, default='DD:XMM_LSS',
+                  help="to set prior or not[%default]")
+
+opts, args = parser.parse_args()
+
+
+m5_file = opts.m5_file
+ref_field = opts.ref_field
+
+# get m5 values
+m5 = pd.read_csv(m5_file)
+m5_med = m5.groupby(['note', 'filter'])[
+    'fiveSigmaDepth', 'airmass'].median().reset_index()
+
+idx = m5_med['note'] == ref_field
+m5_field = m5_med[idx]
+
+print(m5_field)
+
+cmd = 'python run_scripts/sim_to_fit/run_fake.py'
+cmd += ' --SN_NSNabsolute=1 --MultiprocessingSimu_nproc=8'
+
+for b in 'ugrizy':
+    io = m5_field['filter'] == b
+    sel = m5_field[io]
+    cmd += ' --m5_{}={}'.format(b, sel['fiveSigmaDepth'].values[0])
+
+print(cmd)
+os.system(cmd)
--- run_scripts/sim_to_fit/test_slower.py
+++ run_scripts/sim_to_fit/test_slower.py
@@ -1,3 +1,25 @@
 #!/bin/bash
+#!/bin/bash
+import os
+import pandas as pd
+from optparse import OptionParser
 
-python run_scripts/sim_to_fit/run_fake.py --SN_NSNabsolute=1 --obsFromSimu=1 --SN_daymax_type=uniform --MultiprocessingFit_nproc=8
+
+parser = OptionParser()
+
+parser.add_option("--m5_file", type=str,
+                  default='input/m5_OS/m5_field_night_baseline_v3.0_10yrs.csv',
+                  help="file to process [%default]")
+
+opts, args = parser.parse_args()
+
+
+m5_file = opts.m5_file
+
+cmd = 'python run_scripts/sim_to_fit/run_fake.py'
+cmd += ' --SN_NSNabsolute=1 --obsFromSimu=1 --SN_daymax_type=uniform'
+cmd += ' --MultiprocessingFit_nproc=8 --MultiprocessingSimu_nproc=8'
+cmd += ' --obsCondFile={}'.format(m5_file)
+
+print(cmd)
+os.system(cmd)
