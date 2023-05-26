0074d63999d6e5c895e08d3ea8173e68853b5a64
Gris <philippe.gris@clermont.in2p3.fr>
2023-05-26 10:30:12 +0200

    modif to have a minimum number of visits around min

--- run_scripts/desc_ddf_strategy/make_simu_Nv_z_tuning.py
+++ run_scripts/desc_ddf_strategy/make_simu_Nv_z_tuning.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from optparse import OptionParser
+import numpy as np
 
 parser = OptionParser(description='Script to generate fake obs, simu and fit')
 
@@ -36,6 +37,27 @@ if zcomp == '0.70':
     Nz = 62
     Ny = 12
 
+if zcomp == '0.65':
+    Ng = 4
+    Nr = 9
+    Ni = 10
+    Nz = 15
+    Ny = 3
+
+if zcomp == '0.60':
+    Ng = 4
+    Nr = 9
+    Ni = 1
+    Nz = 1
+    Ny = 0
+
+if zcomp == '0.55':
+    Ng = 4
+    Nr = 7
+    Ni = 1
+    Nz = 1
+    Ny = 0
+
 cadg = 2
 cadr = 2
 cadi = 2
@@ -47,9 +69,17 @@ moonswapFilter = 'y'
 
 r = []
 io = 0
-for NNi in range(int(pmin*Ni), int(pmax*Ni), 2):
-    for NNz in range(int(pmin*Nz), int(pmax*Nz), 2):
-        for NNy in range(int(pmin*Ny), int(pmax*Ny), 2):
+Ni_min = np.max([int(pmin*Ni), Ni-3])
+Ni_max = np.max([int(pmax*Ni), Ni+3])
+Nz_min = np.max([int(pmin*Nz), Nz-3])
+Nz_max = np.max([int(pmax*Nz), Nz+3])
+Ny_min = np.max([int(pmin*Ny), Ny-3])
+Ny_max = np.max([int(pmax*Ny), Ny+3])
+
+
+for NNi in range(Ni_min, Ni_max, 1):
+    for NNz in range(Nz_min, Nz_max, 1):
+        for NNy in range(Ny_min, Ny_max, 1):
             io += 1
             combi_name = 'combi_{}'.format(io)
             plot_name = 'plot_{}'.format(io)
