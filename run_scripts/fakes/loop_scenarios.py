ed5d6ea622b31ddc52037a8f3d354d282604a9fa
pgris <gris@clermont.in2p3.fr>
2023-06-20 17:59:53 +0200

    implement multiprocessing to speed things up

--- run_scripts/fakes/loop_scenarios.py
+++ run_scripts/fakes/loop_scenarios.py
@@ -1,5 +1,23 @@
 import os
 import glob
+from sn_tools.sn_utils import multiproc
+
+
+def loop_it(listDB, params, j=0, output_q=None):
+
+    dirScen = params['dirScen']
+
+    cmd = 'python run_scripts/fakes/make_scenario.py --configFile {}'.format(
+        dirScen)
+    for scen in listDB:
+        cmd_ = '{}/{}'.format(cmd, scen.split('/')[-1])
+        print(cmd_)
+        os.system(cmd_)
+
+    if output_q is not None:
+        return output_q.put({j: 1})
+    else:
+        return 1
 
 
 dirScen = 'input/DESC_cohesive_strategy'
@@ -10,6 +28,12 @@ list_scen += ['{}/DDF_SCOC_pII.csv'.format(dirScen),
               '{}/DDF_Univ_SN.csv'.format(dirScen),
               '{}/DDF_Univ_WZ.csv'.format(dirScen)]
 
+params = {}
+params['dirScen'] = dirScen
+
+multiproc(list_scen, params, loop_it, 8)
+
+"""
 cmd = 'python run_scripts/fakes/make_scenario.py --configFile {}'.format(
     dirScen)
 
@@ -17,3 +41,4 @@ for scen in list_scen:
     cmd_ = '{}/{}'.format(cmd, scen.split('/')[-1])
     print(cmd_)
     os.system(cmd_)
+"""
