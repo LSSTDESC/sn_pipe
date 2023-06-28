d0cddcf84821ecff527d2d19a729595e33119920
Gris <philippe.gris@clermont.in2p3.fr>
2023-06-28 18:40:25 +0200

    implemented a cosmology with mu, sigma_mu

--- run_scripts/sn_cosmology/cosmology.py
+++ run_scripts/sn_cosmology/cosmology.py
@@ -7,7 +7,7 @@ Created on Tue Jun 27 09:59:56 2023
 """
 import pandas as pd
 from sn_analysis.sn_selection import selection_criteria
-from sn_analysis.sn_calc_plot import select
+from sn_analysis.sn_calc_plot import select, bin_it_mean
 from sn_cosmology.cosmo_fit import CosmoFit, fom
 import numpy as np
 from astropy.cosmology import w0waCDM
@@ -56,13 +56,14 @@ class MyFit(CosmoFit):
                                                               parDict['Om0'],
                                                               parDict['w0'],
                                                               parDict['wa']))
+        print('ppp', parDict)
 
         f = cosmo.distmod(self.z.to_list()).value
-        self.h = np.max(f) * (10**-5)
+        self.h = np.max(f) * (10**-8)
 
         return f
 
-    def xi_square(self, *parameters):
+    def xi_square_old(self, *parameters):
         '''
         Calculate Xi_square for a data set of value x and y and a function.
         Parameters
@@ -117,6 +118,55 @@ class MyFit(CosmoFit):
             """
         return X_mat
 
+    def xi_square(self, *parameters):
+        '''
+        Calculate Xi_square for a data set of value x and y and a function.
+        Parameters
+        ----------
+        *parameters : tuple of differents types of entries.
+            see the definition in function()
+        Returns
+        -------
+        X : numerical value
+            The Xi_square value.
+        '''
+        # Numerical calculations for each entry
+        # X_mat = np.sum(((self.y-self.function(*parameters))**2)/self.sigma**2)
+        # diag_sigma = np.linalg.inv(np.diag(self.sigma**2))
+
+        mu = self.mu
+        mu_th = self.fit_function(*parameters)
+        sigma_mu = self.sigma_mu
+        sigmaInt = 0.0
+        # print(var_mu)
+        f = mu - mu_th
+        # Matrix calculation of Xisquare
+        # X_mat = np.matmul(f * f, sigma_mu**-2)
+        X_mat = np.sum(f**2/sigma_mu**2)
+        # prior to be set here
+
+        if not self.prior.empty:
+            idx = self.prior['varname'].isin(self.fitparNames)
+            for io, row in self.prior[idx].iterrows():
+                ref_val = row['refvalue']
+                sigma_val = row['sigma']
+                i = self.fitparNames.index(row['varname'])
+                if len(parameters) > 0:
+                    X_mat += ((parameters[i] - ref_val)**2)/sigma_val**2
+
+            """
+            for i, name in enumerate(self.fitparNames):
+                idx = self.prior['varname'] == name
+                sel = self.prior[idx]
+                if len(sel) > 0:
+                    ref_val = sel['refvalue']
+                    sigma_val = sel['sigma']
+                    X_mat += ((parameters[i] - ref_val)**2)/sigma_val**2
+            """
+
+        print('running', parameters, X_mat)
+        return X_mat
+
 
 def random_sample(nsn_field_season, sn_data):
 
@@ -167,27 +217,43 @@ def plot_mu(data):
 
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots()
-    ax.plot(data['z'], data['mu'], 'ko', mfc='None', ms=5)
-    cosmology = w0waCDM(H0=0.70,
-                        Om0=0.3,
-                        Ode0=0.7,
-                        w0=-1., wa=0.0)
-    f = cosmology.distmod(data['z'].to_list()).value
-    ax.plot(data['z'], f, 'bs', ms=5)
+    df = bin_it_mean(data, xvar='z', yvar='mu')
+    print(df)
+    ax.errorbar(df['z'], df['mu'], yerr=df['mu_std'], marker='o',
+                color='k', mfc='None', ms=5)
+    cosmo = w0waCDM(H0=70,
+                    Om0=0.3,
+                    Ode0=0.7,
+                    w0=-1., wa=0.0)
 
+    bins = np.arange(df['z'].min(), 1.1, 0.02)
+    f = cosmo.distmod(bins).value
+
+    ax.plot(bins, f, 'bs', ms=5)
+
+    df['diff'] = df['mu']-f
+    figb, axb = plt.subplots()
+
+    axb.plot(df['z'], df['diff'], 'ko')
+
+    idx = df['z'] >= 0.5
+    print('mean diff', np.mean(df[idx]['diff']))
+
+    """
     idx = data['sigmaC'] <= 0.04
     seldata = data[idx]
     ax.plot(seldata['z'], seldata['mu'], 'r*', ms=5)
 
     figb, axb = plt.subplots()
     axb.plot(data['z'], data['sigma_mu'], 'ko', mfc='None', ms=5)
-
+    """
     plt.show()
 
 
 # loading SN data
 dataDir = 'SN_analysis_sigmaInt_0.0_Hounsell'
 dbName = 'DDF_DESC_0.80_SN'
+# dbName = 'DDF_Univ_WZ'
 statName = 'sn_field_season.hdf5'
 selconfig = 'G10_JLA'
 seasons = range(4)
@@ -206,22 +272,31 @@ print(data.columns)
 vardf = ['z', 'x1_fit', 'color_fit', 'mbfit', 'Cov_x1x1', 'Cov_x1color',
          'Cov_colorcolor',
          'Cov_mbmb', 'Cov_x1mb', 'Cov_colormb']
-dataValues = [data[key] for key in vardf]
 dataNames = ['z', 'x1', 'color', 'mb', 'Cov_x1x1', 'Cov_x1color', 'Cov_colorcolor',
              'Cov_mbmb', 'Cov_x1mb', 'Cov_colormb']
 # fitparNames = ['w0', 'wa', 'Om0', 'alpha', 'beta', 'Mb']
 fitparNames = ['w0', 'Om0', 'alpha', 'beta', 'Mb']
+# params = [-1., 0., 0.3, 0.16, 3., -19.6]
+params = [-1, 0.3, 0.13, 3.1, -19.08]
+
+
+vardf = ['z', 'mu', 'sigma_mu']
+dataValues = [data[key] for key in vardf]
+dataNames = ['z', 'mu', 'sigma_mu']
+fitparNames = ['w0', 'Om0']
+params = [-1.0, 0.3]
+
 par_protect_fit = ['Om0']
-prior = pd.DataFrame([('Om0', 0.3, 0.07)],
+# par_protect_fit = []
+prior = pd.DataFrame([('Om0', 0.3, 0.0073)],
                      columns=['varname', 'refvalue', 'sigma'])
-prior = pd.DataFrame()
+# prior = pd.DataFrame()
 
 myfit = MyFit(dataValues, dataNames,
               fitparNames=fitparNames, prior=prior,
-              par_protect_fit=[])
+              par_protect_fit=par_protect_fit)
 
-# params = [-1., 0., 0.3, 0.16, 3., -19.6]
-params = [-1, 0.3, 0.13, 3.1, -19.08]
+
 dict_fit = myfit.minuit_fit(params)
 fitpars = []
 for pp in fitparNames:
@@ -231,8 +306,8 @@ dict_fit['Chi2_fit'] = myfit.xi_square(*fitpars)
 # for pp in fitparNames:
 #     fitpars.append(dict_fit['{}_fit'.format(pp)])
 # dict_fit = {}
-
+print(dict_fit)
 fisher_cov = myfit.covariance_fisher(params)
 dict_fit.update(fisher_cov)
-
+# dict_fit['Chi2_fisher'] = myfit.xi_square(*[])
 print(dict_fit)
