1ce18a3ed4b4b23408f1ae6727b7d2a55dfd6e97
pgris <gris@clermont.in2p3.fr>
2023-07-12 14:24:37 +0200

    add plots of the number of DD, UD, DDF and WFD fields

--- plot_scripts/cosmology/plot_cosmo.py
+++ plot_scripts/cosmology/plot_cosmo.py
@@ -9,7 +9,7 @@ Created on Wed Jul  5 14:08:41 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sn_plotter_cosmology.cosmoplot import Process_OS, cosmo_plot, cosmo_four
+from sn_plotter_cosmology.cosmoplot import Process_OS, plot_allOS
 from optparse import OptionParser
 
 
@@ -41,26 +41,6 @@ def load_data(dbDir, config):
     return df
 
 
-def plot_allOS(resdf, config, prior='prior', vary='MoM', legy='$MoM$'):
-
-    fig, ax = plt.subplots(figsize=(12, 8))
-    fig.subplots_adjust(right=0.8)
-    dd = dict(zip(['noprior', 'prior'], ['no prior', 'with prior']))
-    fig.suptitle(dd[prior])
-
-    for i, row in config.iterrows():
-        idx = resdf['dbName_DD'] == row['dbName']
-        idx &= resdf['prior'] == prior
-        sel = resdf[idx]
-        cosmo_plot(sel, vary=vary, legy=legy, ax=ax, ls=row['ls'],
-                   marker=row['marker'], color=row['color'], leg=row['dbName'])
-
-    ax.grid()
-    ax.legend(loc='upper center',
-              bbox_to_anchor=(1.15, 0.7),
-              ncol=1, fontsize=12, frameon=False)
-
-
 parser = OptionParser(description='Script to analyze SN prod')
 
 parser.add_option('--dbDir', type=str, default='../cosmo_fit',
@@ -68,28 +48,57 @@ parser.add_option('--dbDir', type=str, default='../cosmo_fit',
 parser.add_option('--dbList', type=str,
                   default='input/DESC_cohesive_strategy/config_ana.csv',
                   help='OS name[%default]')
-
+parser.add_option('--budget_DD', type=float,
+                  default=0.07,
+                  help='DD budget[%default]')
 opts, args = parser.parse_args()
 
 dbDir = opts.dbDir
 dbList = opts.dbList
-
+budget = np.round(opts.budget_DD, 2)
 
 config = pd.read_csv(dbList, comment='#')
-
+config['dbName'] += '_{}'.format(budget)
 data = load_data(dbDir, config)
-# data['dbName_DD'] = 'DDF_Univ_WZ'
+print(data.columns)
+# data['dbName_DD'] += '_{}'.format(budget)
+
+
 grpCol = ['season', 'dbName_DD', 'prior']
 # resdf = process_OS(data, grpCol)
 resdf = Process_OS(data, grpCol).res
 
+resdf['UD_mean'] = 0
+resdf['DD_mean'] = 0
+
+for UD in ['COSMOS', 'XMM-LSS']:
+    resdf['UD_mean'] += resdf['{}_mean'.format(UD)]
+
+for DD in ['CDFS', 'COSMOS', 'EDFSa', 'EDFSb', 'ELAISS1']:
+    resdf['DD_mean'] += resdf['{}_mean'.format(DD)]
+
+resdf['DDF_mean'] = resdf['UD_mean']+resdf['DD_mean']
+
+for tt in ['DDF', 'UD', 'DD', 'WFD']:
+    resdf['{}_std'.format(tt)] = 0
+
 print(resdf, config)
+print(resdf.columns)
 
 vvars = ['MoM', 'sigma_w']
 leg = dict(zip(vvars, [r'$MoM$', r'$\sigma_w$[%]']))
 for vary in vvars:
     for prior in ['prior', 'noprior']:
         plot_allOS(resdf, config, vary=vary, legy=leg[vary], prior=prior)
+
+vvarsb = ['DDF', 'UD', 'DD', 'WFD']
+legb = [r'$N^{DDF}$', r'$N^{UD}$', r'$N^{DD}$', r'$N^{WFD}$']
+lleg = dict(zip(vvarsb, legb))
+for vary in vvarsb:
+    plot_allOS(resdf, config, vary=vary,
+               legy=lleg[vary], prior='prior', figtitle=False)
+
+
 # ax.legend()
 # idx = resdf['prior'] == 'noprior'
 # cosmo_plot(resdf[idx])
