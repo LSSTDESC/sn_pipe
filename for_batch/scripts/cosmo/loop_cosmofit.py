88382cba6d6719d334ed51c0bdd6ae546744a9bd
pgris <gris@clermont.in2p3.fr>
2023-07-13 09:59:43 +0200

    add the survey file as input parameter

--- for_batch/scripts/cosmo/loop_cosmofit.py
+++ for_batch/scripts/cosmo/loop_cosmofit.py
@@ -6,13 +6,20 @@ parser = OptionParser()
 parser.add_option("--dbList", type="str", default='DD_fakes.csv',
                   help="db list to process  [%default]")
 parser.add_option("--outDir", type="str",
-                  default='/sps/lsst/users/gris/cosmo_fit', help="output directory [%default]")
+                  default='/sps/lsst/users/gris/cosmo_fit',
+                  help="output directory [%default]")
 parser.add_option("--inputDir_DD", type="str",
-                  default='/sps/lsst/users/gris/Output_SN_sigmaInt_0.0_Hounsell_G10_JLA', help="input directory for DD files[%default]")
+                  default='/sps/lsst/users/gris/Output_SN_sigmaInt_0.0_Hounsell_G10_JLA',
+                  help="input directory for DD files[%default]")
 parser.add_option("--inputDir_WFD", type="str",
-                  default='/sps/lsst/users/gris/Output_SN_WFD_sigmaInt_0.0_Hounsell_G10_JLA', help="input directory for WFD files[%default]")
+                  default='/sps/lsst/users/gris/Output_SN_WFD_sigmaInt_0.0_Hounsell_G10_JLA',
+                  help="input directory for WFD files[%default]")
 parser.add_option("--dbName_WFD", type="str",
                   default='draft_connected_v2.99_10yrs', help="dbName for WFD data[%default]")
+parser.add_option("--survey", type=str,
+                  default='input/DESC_cohesive_strategy/survey_scenario.csv',
+                  help=" survey to use[%default]")
+
 opts, args = parser.parse_args()
 
 dbList = opts.dbList
@@ -20,6 +27,7 @@ outDir = opts.outDir
 inputDir_DD = opts.inputDir_DD
 inputDir_WFD = opts.inputDir_WFD
 dbName_WFD = opts.dbName_WFD
+survey = opts.survey
 
 # load OS files to process
 fis = pd.read_csv(dbList, comment='#')
@@ -37,5 +45,6 @@ for i, row in fis.iterrows():
     params['dataDir_WFD'] = inputDir_WFD
     params['dbName_WFD'] = dbName_WFD
     params['outDir'] = outDir
+    params['survey'] = survey
     mybatch.add_batch(script, params)
     mybatch.go_batch()
