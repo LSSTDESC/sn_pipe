3f8e7744ac0041ba6c24a09f5eddb59e20fb7cbe
pgris <gris@clermont.in2p3.fr>
2023-09-29 13:00:52 +0200

    update following some file format update

--- for_batch/scripts/cosmo/loop_cosmofit.py
+++ for_batch/scripts/cosmo/loop_cosmofit.py
@@ -3,7 +3,8 @@ from optparse import OptionParser
 from sn_tools.sn_batchutils import BatchIt
 parser = OptionParser()
 
-parser.add_option("--dbList", type="str", default='DD_fakes.csv',
+parser.add_option("--dbList", type="str",
+                  default='for_batch/input/cosmofit/cosmoList.csv',
                   help="db list to process  [%default]")
 parser.add_option("--outDir", type="str",
                   default='/sps/lsst/users/gris/cosmo_fit',
@@ -14,8 +15,6 @@ parser.add_option("--inputDir_DD", type="str",
 parser.add_option("--inputDir_WFD", type="str",
                   default='/sps/lsst/users/gris/Output_SN_WFD_sigmaInt_0.0_Hounsell_G10_JLA',
                   help="input directory for WFD files[%default]")
-parser.add_option("--dbName_WFD", type="str",
-                  default='baseline_v3.0_10yrs', help="dbName for WFD data[%default]")
 parser.add_option("--survey", type=str,
                   default='input/DESC_cohesive_strategy/survey_scenario.csv',
                   help=" survey to use[%default]")
@@ -32,7 +31,7 @@ dbList = opts.dbList
 outDir = opts.outDir
 inputDir_DD = opts.inputDir_DD
 inputDir_WFD = opts.inputDir_WFD
-dbName_WFD = opts.dbName_WFD
+# dbName_WFD = opts.dbName_WFD
 survey = opts.survey
 tag = opts.tag
 frac_WFD_low_sigmaC = opts.frac_WFD_low_sigmaC
@@ -44,14 +43,14 @@ fis = pd.read_csv(dbList, comment='#')
 
 script = 'run_scripts/cosmology/cosmology.py'
 for i, row in fis.iterrows():
-    dbName = row['dbName']
+    dbName = row['dbName_DD']
     processName = 'cosmo_{}_{}'.format(dbName, tag)
     mybatch = BatchIt(processName=processName)
     params = {}
     params['dataDir_DD'] = inputDir_DD
     params['dbName_DD'] = dbName
     params['dataDir_WFD'] = inputDir_WFD
-    params['dbName_WFD'] = dbName_WFD
+    params['dbName_WFD'] = row['dbName_WFD']
     params['outDir'] = outDir
     params['survey'] = survey
     params['frac_WFD_low_sigmaC'] = frac_WFD_low_sigmaC
