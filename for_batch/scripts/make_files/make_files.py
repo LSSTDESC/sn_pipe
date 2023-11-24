18406a8f4af1d2c7979dd585e665f6cfcffe9958
pgris <gris@clermont.in2p3.fr>
2023-11-24 13:51:09 +0100

    add the telescope throughputs tag

--- for_batch/scripts/make_files/make_files.py
+++ for_batch/scripts/make_files/make_files.py
@@ -2,7 +2,7 @@ import glob
 import numpy as np
 from optparse import OptionParser
 
-def genFile(files,fName,postfix,dirFiles,dbExtens):
+def genFile(files,fName,postfix,dirFiles,dbExtens,teltag):
     """
     Method to generate a file of OS names and associated infos
     This file is used as an input file to metrics, simulations in batch
@@ -22,7 +22,7 @@ def genFile(files,fName,postfix,dirFiles,dbExtens):
     script = open(fName,"w")
     #firstline='# dbName			simuType	  nside  coadd fieldtype nproc\n'
     #firstline='dbName,simuType,nside,coadd,fieldtype,nproc\n'
-    firstline='dbDir,dbExtens,dbName,simuType,nside,coadd,fieldType,nproc\n'
+    firstline='dbDir,dbExtens,dbName,simuType,nside,coadd,fieldType,nproc,teltag\n'
     script.write(firstline)
     r = []
     for fi in files:
@@ -34,7 +34,7 @@ def genFile(files,fName,postfix,dirFiles,dbExtens):
         line = '{}/{},{}'.format(dirFiles,dbExtens,dbExtens)
         dbName = fi.split('/')[-1].split('.{}'.format(dbExtens))[0]
         #dbName=dbName.ljust(ljust)
-        line += ',{},{}\n'.format(dbName,postfix)
+        line += ',{},{},{}\n'.format(dbName,postfix,teltag)
         print(line)
         script.write(line)
 
@@ -43,22 +43,24 @@ def genFile(files,fName,postfix,dirFiles,dbExtens):
 
 parser = OptionParser()
 
-parser.add_option("--simuVersion", type=str, default='fbs_14',
+parser.add_option("--simuVersion", type=str, default='fbs_3.2',
                   help="simulation version[%default]")
-parser.add_option("--dirFiles", type=str, default='/sps/lsst/groups/cadence/LSST_SN_PhG/cadence_db/fbs_fakes',
+parser.add_option("--dirFiles", type=str, default='/sps/lsst/groups/cadence/LSST_SN_PhG/cadence_db',
                   help="dir where the files are [%default]")
 parser.add_option("--dbExtens", type=str, default='db',
                   help="db extens (db or npy)  [%default]")
-
+parser.add_option("--teltag", type=str, default='1.5',
+                  help="telescope throughputs tag [%default]")
 
 opts, args = parser.parse_args()
 
 simuVersion = opts.simuVersion
 dirFiles = opts.dirFiles
 dbExtens = opts.dbExtens
+teltag = opts.teltag
 
 
-search_path = '{}/{}/*.{}'.format(dirFiles,dbExtens,dbExtens)
+search_path = '{}/{}/{}/*.{}'.format(dirFiles,simuVersion,dbExtens,dbExtens)
 print('search path',search_path)
 
 files = sorted(glob.glob(search_path))
@@ -72,5 +74,5 @@ DD_postfix = '1,128,1,DD,6'
 WFD_postfix = '0,64,1,WFD,8'
 
 print(DD_postfix)
-genFile(files,'DD_{}.csv'.format(simuVersion),DD_postfix,dirFiles,dbExtens)
-genFile(files,'WFD_{}.csv'.format(simuVersion),WFD_postfix,dirFiles,dbExtens)
+genFile(files,'DD_{}.csv'.format(simuVersion),DD_postfix,dirFiles,dbExtens,teltag)
+genFile(files,'WFD_{}.csv'.format(simuVersion),WFD_postfix,dirFiles,dbExtens,teltag)
