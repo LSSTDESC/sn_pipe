37a05afca7d4a119e8209a86440162649b2d1748
pgris <gris@clermont.in2p3.fr>
2023-11-29 13:41:56 +0100

    add estimation of the year using the LSST starting date

--- run_scripts/cosmology/sn_selection.py
+++ run_scripts/cosmology/sn_selection.py
@@ -17,7 +17,7 @@ import numpy as np
 def select_filt(dataDir, dbName, sellist, seasons,
                 runType='DDF_spectroz', nsn_factor=1,
                 listFields='COSMOS', fieldType='DD',
-                outDir='Test', nproc=8):
+                outDir='Test', nproc=8, mjdStart=60796.001):
     """
     Function to select and save selected SN data
     (per season)
@@ -44,7 +44,8 @@ def select_filt(dataDir, dbName, sellist, seasons,
         Main output Dir. The default is 'Test'.
     nproc: int, optional.
       number of procs for multiprocessing. The default is 8.
-
+    mjdStart: float, optional.
+      starting date of the LSST survey. The default is 60796.001.
     Returns
     -------
     None.
@@ -73,6 +74,16 @@ def select_filt(dataDir, dbName, sellist, seasons,
         # save output data in pandas df
         outName = '{}/SN_{}_{}_{}.hdf5'.format(
             outDir_full, fieldType, dbName, seas)
+
+        # add lsststart as column
+        sel_data['LSSTStart'] = mjdStart
+        # add year as column
+        sel_data['year'] = sel_data['daymax']+60*(1.+sel_data['z'])
+        sel_data['year'] -= mjdStart
+        sel_data['year'] /= 365.
+        sel_data['year'] += 1.
+        sel_data['year'] = sel_data['year'].astype(int)
+
         sel_data.to_hdf(outName, key='SN')
 
         # get some stat
@@ -163,6 +174,9 @@ parser.add_option("--nsn_factor", type=int,
                   default=30, help="MC normalisation factor [%default]")
 parser.add_option("--nproc", type=int,
                   default=8, help="nproc for multiprocessing [%default]")
+parser.add_option("--lsst_start", type=str,
+                  default='LSSTStart.csv',
+                  help="List of LSST start survey night. [%default]")
 
 opts, args = parser.parse_args()
 
@@ -174,6 +188,8 @@ runType = opts.runType
 fieldType = opts.fieldType
 listFields = opts.listFields
 nsn_factor = opts.nsn_factor
+lsst_start = opts.lsst_start
+
 outDir = '{}_{}'.format(dataDir, selconfig)
 nproc = opts.nproc
 
@@ -181,7 +197,12 @@ nproc = opts.nproc
 seasons = range(1, 11)
 sellist = selection_criteria()[selconfig]
 
+mjd_start = pd.read_csv(lsst_start, comment='#')
+
+idx = mjd_start['dbName'] == dbName
+mjdStart = mjd_start[idx]['LSSTStart'].median()
+
 select_filt(dataDir, dbName, sellist, seasons=seasons,
             runType=runType, nsn_factor=nsn_factor,
             listFields=listFields, fieldType=fieldType,
-            outDir=outDir, nproc=nproc)
+            outDir=outDir, nproc=nproc, mjdStart=mjdStart)
