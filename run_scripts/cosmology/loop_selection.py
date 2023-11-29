db08e537d4815ec59a193f46514514c4251569a7
pgris <gris@clermont.in2p3.fr>
2023-11-29 17:58:43 +0100

    adapt the code to select SNe Ia by year

--- run_scripts/cosmology/loop_selection.py
+++ run_scripts/cosmology/loop_selection.py
@@ -10,11 +10,14 @@ parser.add_option("--dataDir", type=str,
                   help="data dir[%default]")
 parser.add_option("--dbList", type=str,
                   default='list_OS.csv', help="DB list to process [%default]")
-
+parser.add_option("--timescale", type=str,
+                  default='year',
+                  help="Time scale for NSN estimation. [%default]")
 opts, args = parser.parse_args()
 
 dbList = opts.dbList
 dataDir = opts.dataDir
+timescale = opts.timescale
 
 
 db = pd.read_csv(dbList, comment='#')
@@ -23,4 +26,5 @@ script = 'python run_scripts/cosmology/sn_selection.py'
 
 for i, row in db.iterrows():
     cmd = '{} --dbName {} --dataDir {}'.format(script, row['dbName'], dataDir)
+    cmd += ' --timescale={}'.format(timescale)
     os.system(cmd)
--- run_scripts/cosmology/sn_selection.py
+++ run_scripts/cosmology/sn_selection.py
@@ -17,7 +17,8 @@ import numpy as np
 def select_filt(dataDir, dbName, sellist, seasons,
                 runType='DDF_spectroz', nsn_factor=1,
                 listFields='COSMOS', fieldType='DD',
-                outDir='Test', nproc=8, mjdStart=60796.001):
+                outDir='Test', nproc=8, mjdStart=60796.001,
+                timescale='year'):
     """
     Function to select and save selected SN data
     (per season)
@@ -46,6 +47,9 @@ def select_filt(dataDir, dbName, sellist, seasons,
       number of procs for multiprocessing. The default is 8.
     mjdStart: float, optional.
       starting date of the LSST survey. The default is 60796.001.
+    timescale : str, optional
+        timescale for calculation. The default is 'year'.
+
     Returns
     -------
     None.
@@ -56,6 +60,7 @@ def select_filt(dataDir, dbName, sellist, seasons,
     checkDir(outDir_full)
 
     stat_tot = pd.DataFrame()
+    sel_tot = pd.DataFrame()
 
     for seas in seasons:
         # load DDFs
@@ -64,16 +69,14 @@ def select_filt(dataDir, dbName, sellist, seasons,
             seasons=str(seas), nproc=nproc)
         print(seas, len(data))
 
+        if data.empty:
+            continue
         # apply selection on Data
         sel_data = select(data, sellist)
         sel_data = sel_data[sel_data.columns.drop(
             list(sel_data.filter(regex='mask')))]
         if 'selected' in sel_data.columns:
             sel_data = sel_data.drop(columns=['selected'])
-
-        # save output data in pandas df
-        outName = '{}/SN_{}_{}_{}.hdf5'.format(
-            outDir_full, fieldType, dbName, seas)
 
         # add lsststart as column
         sel_data['LSSTStart'] = mjdStart
@@ -84,23 +87,51 @@ def select_filt(dataDir, dbName, sellist, seasons,
         sel_data['year'] += 1.
         sel_data['year'] = sel_data['year'].astype(int)
 
-        sel_data.to_hdf(outName, key='SN')
+        sel_tot = pd.concat((sel_data, sel_tot))
 
+        # save output data in pandas df
+        """
+        outName = '{}/SN_{}_{}_{}.hdf5'.format(
+            outDir_full, fieldType, dbName, seas)
+
+        sel_data.to_hdf(outName, key='SN')
+        """
+        # sel_tot = pd.concat((sel_data, sel_tot))
         # get some stat
         # stat = sel_data.groupby(['field', 'season']).apply(
         #     lambda x: pd.DataFrame({'nsn': [len(x)/nsn_factor]})).reset_index()
+        """
+        stat = get_stat(sel_data, nsn_factor, timescale=timescale)
+        stat_tot = pd.concat((stat_tot, stat))
+        """
 
-        stat = get_stat(sel_data, nsn_factor)
+    if sel_tot.empty:
+        return -1
+
+    timescales = sel_tot[timescale].unique()
+
+    print('hello timescales', timescale, timescales)
+    for timescalev in timescales:
+        idx = sel_tot[timescale] == timescalev
+        sel_data = sel_tot[idx]
+        # save output data in pandas df
+        outName = '{}/SN_{}_{}_{}.hdf5'.format(
+            outDir_full, fieldType, dbName, timescalev)
+
+        sel_data.to_hdf(outName, key='SN')
+        stat = get_stat(sel_data, nsn_factor, timescale=timescale)
         stat_tot = pd.concat((stat_tot, stat))
 
-    stat_tot['season'] = stat_tot['season'].astype(int)
+    # stat_tot = get_stat(sel_tot, nsn_factor, timescale=timescale)
+
+    stat_tot[timescale] = stat_tot[timescale].astype(int)
     stat_tot['nsn'] = stat_tot['nsn'].astype(int)
     outName_stat = '{}/nsn_{}.hdf5'.format(outDir_full, dbName)
 
     stat_tot.to_hdf(outName_stat, key='SN')
 
 
-def get_stat(sel_data, nsn_factor):
+def get_stat(sel_data, nsn_factor, timescale='year'):
     """
     Function to estimate nsn
 
@@ -110,6 +141,8 @@ def get_stat(sel_data, nsn_factor):
         Data to process.
     nsn_factor : float
         Normalization factor.
+    timescale: str, optional.
+      time scale for nsn estimation. The default is 'year'.
 
     Returns
     -------
@@ -119,7 +152,7 @@ def get_stat(sel_data, nsn_factor):
     """
 
     # get total nsn
-    stat_sn = sel_data.groupby(['field', 'season']).apply(
+    stat_sn = sel_data.groupby(['field', timescale]).apply(
         lambda x: nsn_estimate(x,
                                zmax=1.1,
                                nsn_factor=nsn_factor,
@@ -128,7 +161,7 @@ def get_stat(sel_data, nsn_factor):
 
     for zlim in [0.1, 0.2]:
         nname = 'nsn_z_{}'.format(np.round(zlim, 1))
-        stat_sn_z = sel_data.groupby(['field', 'season']).apply(
+        stat_sn_z = sel_data.groupby(['field', timescale]).apply(
             lambda x: nsn_estimate(x,
                                    zmax=zlim,
                                    nsn_factor=nsn_factor,
@@ -136,8 +169,8 @@ def get_stat(sel_data, nsn_factor):
         stat_sn_z = stat_sn_z.drop(['level_2'], axis=1)
         # merge
         stat_sn = stat_sn.merge(
-            stat_sn_z, left_on=['field', 'season'],
-            right_on=['field', 'season'], suffixes=['', ''])
+            stat_sn_z, left_on=['field', timescale],
+            right_on=['field', timescale], suffixes=['', ''])
 
     return stat_sn
 
@@ -177,6 +210,9 @@ parser.add_option("--nproc", type=int,
 parser.add_option("--lsst_start", type=str,
                   default='LSSTStart.csv',
                   help="List of LSST start survey night. [%default]")
+parser.add_option("--timescale", type=str,
+                  default='year',
+                  help="Time scale for NSN estimation. [%default]")
 
 opts, args = parser.parse_args()
 
@@ -189,12 +225,14 @@ fieldType = opts.fieldType
 listFields = opts.listFields
 nsn_factor = opts.nsn_factor
 lsst_start = opts.lsst_start
+timescale = opts.timescale
 
 outDir = '{}_{}'.format(dataDir, selconfig)
 nproc = opts.nproc
 
 
-seasons = range(1, 11)
+seasons = range(1, 12)
+
 sellist = selection_criteria()[selconfig]
 
 mjd_start = pd.read_csv(lsst_start, comment='#')
@@ -205,4 +243,5 @@ mjdStart = mjd_start[idx]['LSSTStart'].median()
 select_filt(dataDir, dbName, sellist, seasons=seasons,
             runType=runType, nsn_factor=nsn_factor,
             listFields=listFields, fieldType=fieldType,
-            outDir=outDir, nproc=nproc, mjdStart=mjdStart)
+            outDir=outDir, nproc=nproc, mjdStart=mjdStart,
+            timescale=timescale)
