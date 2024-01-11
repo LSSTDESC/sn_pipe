37837aa17a2515123ba4a4f6eab2ee680e5085f2
pgris <gris@clermont.in2p3.fr>
2024-01-11 13:30:44 +0100

    bug correction

--- for_batch/scripts/sn_prod/sn_prod.py
+++ for_batch/scripts/sn_prod/sn_prod.py
@@ -56,7 +56,7 @@ def batch_DDF(theDict, scriptref='run_scripts/sim_to_fit/run_sim_to_fit.py',
         tag_dir = '_photz'
     procDict['OutputSimu_directory'] = '{}/{}/DDF{}'.format(outDir,
                                                             dbName, tag_dir)
-    # checkDir(procDict['OutputSimu_directory'])
+    checkDir(procDict['OutputSimu_directory'])
     procDict['OutputFit_directory'] = procDict['OutputSimu_directory']
     # procDict['SN_NSNfactor'] = 30
     procDict['Pixelisation_nside'] = procDict['nside']
