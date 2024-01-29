2ff2356db5b6200a09bf7b065e062eef3addae63
pgris <gris@clermont.in2p3.fr>
2024-01-29 14:58:20 +0100

    cleaning

--- plot_scripts/cosmology/plot_cosmo.py
+++ plot_scripts/cosmology/plot_cosmo.py
@@ -11,6 +11,7 @@ from sn_plotter_cosmology import plt
 import numpy as np
 from sn_plotter_cosmology.cosmoplot import Process_OS, plot_allOS
 from optparse import OptionParser
+import glob
 
 
 def load_data(dbDir, config):
@@ -34,7 +35,41 @@ def load_data(dbDir, config):
     df = pd.DataFrame()
 
     for i, row in config.iterrows():
-        fi = '{}/cosmo_{}.hdf5'.format(dbDir, row['dbName'])
+
+        search_path = '{}/cosmo_{}_*.hdf5'.format(dbDir, row['dbName'])
+        fis = glob.glob(search_path)
+        for fi in fis:
+            dfa = pd.read_hdf(fi)
+            # print(dfa)
+            dfa = dfa.replace([np.inf, -np.inf, np.nan], 0)
+
+            df = pd.concat((df, dfa))
+
+    return df
+
+
+def load_data_old(dbDir, config):
+    """
+    Method to load data
+
+    Parameters
+    ----------
+    dbDir : str
+        Data dir.
+    config : pandas df
+        config (dbName+OS params).
+
+    Returns
+    -------
+    df : pandas df
+        output data.
+
+    """
+
+    df = pd.DataFrame()
+
+    for i, row in config.iterrows():
+        fi = '{}/cosmo_{}_*.hdf5'.format(dbDir, row['dbName'])
         dfa = pd.read_hdf(fi)
         # print(dfa)
         dfa = dfa.replace([np.inf, -np.inf, np.nan], 0)
@@ -42,6 +77,78 @@ def load_data(dbDir, config):
         df = pd.concat((df, dfa))
 
     return df
+
+
+def plot_cosmo_summary(data, udfs, dfs):
+
+    grpCol = [timescale, 'dbName_DD', 'prior']
+    # resdf = process_OS(data, grpCol)
+    resdf = Process_OS(data, grpCol, udfs, dfs).res
+
+    resdf['UD_mean'] = 0
+    resdf['DD_mean'] = 0
+
+    for UD in udfs:
+        resdf['UD_mean'] += resdf['{}_mean'.format(UD)]
+
+    for DD in dfs:
+        resdf['DD_mean'] += resdf['{}_mean'.format(DD)]
+
+    resdf['DDF_mean'] = resdf['UD_mean']+resdf['DD_mean']
+
+    for tt in ['DDF', 'UD', 'DD', 'WFD']:
+        resdf['{}_std'.format(tt)] = 0
+
+    print(resdf, config)
+    print(resdf.columns)
+
+    vvars = ['MoM', 'sigma_w0', 'sigma_wa']
+    leg = dict(zip(vvars, [r'$SMoM$',
+               r'$\sigma_{w_0}$ [%]', r'$\sigma_{w_a}$ [%]']))
+    """
+    priors = ['prior', 'noprior']
+
+    dd = dict(zip(priors, ['with prior', 'no prior']))
+
+    vvars = ['MoM']
+    leg = dict(zip(vvars, [r'$SMoM$']))
+    """
+    priors = ['prior']
+
+    dd = dict(zip(priors, ['']))
+
+    for vary in vvars:
+        for prior in priors:
+            plot_allOS(resdf, config, varx=timescale, legx=timescale, vary=vary,
+                       legy=leg[vary], prior=prior, figtitle=dd[prior], dbNorm='')
+
+    """
+    vvarsb = ['DDF', 'UD', 'DD', 'WFD']
+    # DDFs = ['COSMOS', 'XMM-LSS', 'CDFS', 'EDFSa', 'EDFSb', 'ELAISS1']
+    DDFs = ['COSMOS', 'XMM-LSS', 'CDFS', 'EDFS', 'ELAISS1']
+    vvarsb += DDFs
+    legb = [r'$N^{DDF}_{SN}$', r'$N^{UD}_{SN}$',
+            r'$N^{DD}_{SN}$', r'$N^{WFD}_{SN}$']
+    legbb = []
+    for ddf in DDFs:
+        bb = r'$N^{'+ddf+'}_{SN}$'
+        legb.append(bb)
+
+    legb += legbb
+
+    lleg = dict(zip(vvarsb, legb))
+
+    for vary in vvarsb:
+        plot_allOS(resdf, config, varx=timescale, legx=timescale, vary=vary,
+                   legy=lleg[vary], prior='prior', figtitle='', dbNorm='')
+
+    """
+    # ax.legend()
+    # idx = resdf['prior'] == 'noprior'
+    # cosmo_plot(resdf[idx])
+
+    # cosmo_four(resdf)
+    plt.show()
 
 
 parser = OptionParser(description='Script to analyze SN prod')
@@ -75,76 +182,4 @@ config = pd.read_csv(dbList, comment='#')
 data = load_data(dbDir, config)
 print(data.columns)
 # data['dbName_DD'] += '_{}'.format(budget)
-
-
-grpCol = [timescale, 'dbName_DD', 'prior']
-# resdf = process_OS(data, grpCol)
-resdf = Process_OS(data, grpCol, udfs, dfs).res
-
-
-resdf['UD_mean'] = 0
-resdf['DD_mean'] = 0
-
-for UD in udfs:
-    resdf['UD_mean'] += resdf['{}_mean'.format(UD)]
-
-for DD in dfs:
-    resdf['DD_mean'] += resdf['{}_mean'.format(DD)]
-
-resdf['DDF_mean'] = resdf['UD_mean']+resdf['DD_mean']
-
-for tt in ['DDF', 'UD', 'DD', 'WFD']:
-    resdf['{}_std'.format(tt)] = 0
-
-print(resdf, config)
-print(resdf.columns)
-
-"""
-vvars = ['MoM', 'sigma_w', 'sigma_w0', 'sigma_wa']
-leg = dict(zip(vvars, [r'$SMoM$', r'$\sigma_w$[%]',
-           r'$\sigma_{w_0}$ [%]', r'$\sigma_{w_a}$ [%]']))
-
-priors = ['prior', 'noprior']
-
-dd = dict(zip(priors, ['with prior', 'no prior']))
-"""
-vvars = ['MoM']
-leg = dict(zip(vvars, [r'$SMoM$']))
-
-priors = ['prior']
-
-dd = dict(zip(priors, ['']))
-
-
-for vary in vvars:
-    for prior in priors:
-        plot_allOS(resdf, config, varx=timescale, legx=timescale, vary=vary,
-                   legy=leg[vary], prior=prior, figtitle=dd[prior], dbNorm='')
-
-"""
-vvarsb = ['DDF', 'UD', 'DD', 'WFD']
-# DDFs = ['COSMOS', 'XMM-LSS', 'CDFS', 'EDFSa', 'EDFSb', 'ELAISS1']
-DDFs = ['COSMOS', 'XMM-LSS', 'CDFS', 'EDFS', 'ELAISS1']
-vvarsb += DDFs
-legb = [r'$N^{DDF}_{SN}$', r'$N^{UD}_{SN}$',
-        r'$N^{DD}_{SN}$', r'$N^{WFD}_{SN}$']
-legbb = []
-for ddf in DDFs:
-    bb = r'$N^{'+ddf+'}_{SN}$'
-    legb.append(bb)
-
-legb += legbb
-
-lleg = dict(zip(vvarsb, legb))
-
-for vary in vvarsb:
-    plot_allOS(resdf, config, varx=timescale, legx=timescale, vary=vary,
-               legy=lleg[vary], prior='prior', figtitle='', dbNorm='')
-
-"""
-# ax.legend()
-# idx = resdf['prior'] == 'noprior'
-# cosmo_plot(resdf[idx])
-
-# cosmo_four(resdf)
-plt.show()
+plot_cosmo_summary(data, udfs, dfs)
