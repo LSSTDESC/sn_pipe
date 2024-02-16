12e7e054cd643d0228ae35f312e4ba4aff885e64
pgris <gris@clermont.in2p3.fr>
2024-02-16 16:39:07 +0100

    code improvement plus doc

--- run_scripts/sn_selection/reduce_timescale.py
+++ run_scripts/sn_selection/reduce_timescale.py
@@ -12,7 +12,58 @@ import pandas as pd
 import os
 
 
+def process(dataDir, timescale):
+    """
+    Function to process data
+
+    Parameters
+    ----------
+    dbDir : str
+        Data dir.
+    timescale : str
+        Timescale (year/season).
+
+    Returns
+    -------
+    None.
+
+    """
+    for season in range(1, 15, 1):
+        fis = glob.glob('{}/*{}_{}.hdf5'.format(dataDir, timescale, season))
+        print(season, len(fis))
+        if len(fis) > 0:
+            r = []
+            for bb in fis:
+                r.append(bb)
+            common_substring = os.path.commonprefix(
+                [fis[0], fis[1], fis[2], fis[4]])
+
+            outName = '_'.join(common_substring.split('/')[-1].split('_')[:-1])
+            outName = '{}/{}_{}_{}.hdf5'.format(dataDir,
+                                                outName, timescale, season)
+            df = pd.DataFrame()
+            for fi in fis:
+                dd = pd.read_hdf(fi)
+                df = pd.concat((df, dd))
+            df.to_hdf(outName, key='sn')
+
+    clean_dir(fis)
+
+
 def clean_dir(fis):
+    """
+    Function to remove files
+
+    Parameters
+    ----------
+    fis : list(str)
+        List of files to remove.
+
+    Returns
+    -------
+    None.
+
+    """
 
     for fi in fis:
         cmd_ = 'rm {}'.format(fi)
@@ -21,35 +72,31 @@ def clean_dir(fis):
 
 parser = OptionParser()
 
-parser.add_option("--dbDir", type="str", default='../Output_SN_WFD_sigmaInt_0.0_Hounsell_z_smflux_test_G10_JLA/baseline_v3.0_10yrs/WFD_spectroz',
+parser.add_option("--dbDir", type="str",
+                  default='../Output_SN_WFD_sigmaInt_0.0_Hounsell_z_smflux_test_G10_JLA',
                   help="data dir [%default]")
+parser.add_option("--dbList", type=str,
+                  default='list_OS.csv', help="DB list to process [%default]")
 parser.add_option("--timescale", type=str,
                   default='year',
                   help="Time scale for NSN estimation. [%default]")
+parser.add_option("--zType", type=str,
+                  default='spectroz', help="z type (spectroz/photz)  [%default]")
 
 opts, args = parser.parse_args()
 
 dbDir = opts.dbDir
+dbList = opts.dbList
 timescale = opts.timescale
+zType = opts.zType
 
 
-for season in range(1, 15, 1):
-    fis = glob.glob('{}/*{}_{}.hdf5'.format(dbDir, timescale, season))
-    print(season, len(fis))
-    if len(fis) > 0:
-        r = []
-        for bb in fis:
-            r.append(bb)
-        common_substring = os.path.commonprefix(
-            [fis[0], fis[1], fis[2], fis[4]])
+# load dbs
+dbNames = pd.read_csv(dbList, comment='#')
 
-        outName = '_'.join(common_substring.split('/')[-1].split('_')[:-1])
-        print(common_substring)
-        outName = '{}/{}_{}_{}.hdf5'.format(dbDir, outName, timescale, season)
-        df = pd.DataFrame()
-        for fi in fis:
-            dd = pd.read_hdf(fi)
-            df = pd.concat((df, dd))
-        df.to_hdf(outName, key='sn')
 
-    clean_dir(fis)
+# loop on dbs and process
+
+for dbName in dbNames:
+    dataDir = '{}/{}/{}'.format(dbDir, dbName, zType)
+    process(dataDir, timescale)
