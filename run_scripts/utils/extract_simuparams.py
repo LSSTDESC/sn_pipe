40bf2dba680914894722b079845283f94a72b2b4
pgris <gris@clermont.in2p3.fr>
2024-05-03 13:33:39 +0200

    implement a script for WFD

--- run_scripts/utils/extract_simuparams.py
+++ run_scripts/utils/extract_simuparams.py
@@ -8,13 +8,15 @@ Created on Mon Jan  11 09:36:32 2024
 from sn_analysis.sn_tools import load_complete_dbSimu
 from sn_tools.sn_io import checkDir
 from optparse import OptionParser
+import glob
+import pandas as pd
 
 
-def get_simu_params(dataDir, dbName, seasons,
-                    runType='DDF_spectroz',
-                    listFields='COSMOS', fieldType='DD',
-                    outDir='Test', nproc=8,
-                    dataType='pandasDataFrame', suffix=''):
+def get_simu_params_DDF(dataDir, dbName, seasons,
+                        runType='DDF_spectroz',
+                        listFields='COSMOS', fieldType='DD',
+                        outDir='Test', nproc=8,
+                        dataType='pandasDataFrame', suffix=''):
     """
 
     Function to extract simulation parameters
@@ -76,6 +78,104 @@ def get_simu_params(dataDir, dbName, seasons,
         data[ccols].to_hdf(outName, key='simu', index=False)
 
 
+def get_simu_params_WFD(dataDir, dbName, seasons,
+                        runType='DDF_spectroz',
+                        listFields='COSMOS', fieldType='DD',
+                        outDir='Test', nproc=8,
+                        dataType='pandasDataFrame', suffix=''):
+    """
+
+    Function to extract simulation parameters
+
+    Parameters
+    ----------
+    dataDir : str
+        Location dir of the data.
+    dbName : str
+        OS to process.
+    seasons : list(int)
+        List of seasons to process
+    runType : str, optional
+        Runtype. The default is 'DDF_spectroz'.
+    listFields : list(str), optional
+        List of fields to process. The default is 'COSMOS'.
+    fieldType : str, optional
+        Field type to process. The default is 'DD'.
+    outDir : str, optional
+        output directory. The default is 'Test'.
+    nproc : int, optional
+        number of procs to process. The default is 8.
+    dataType : str, optional
+        Type of data. The default is 'pandasDataFrame'.
+    suffix : str, optional
+        Suffix for data to process. The default is ''.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    outDir_full = '{}/{}/{}'.format(outDir, dbName, runType)
+    checkDir(outDir_full)
+
+    print('output directory', outDir_full)
+
+    timescale = 'season'
+    ccols = ['healpixID', 'z', 'x1', 'color', 'daymax', 'x0', 'season',
+             'epsilon_x0', 'epsilon_x1', 'epsilon_color', 'epsilon_daymax',
+             'SNID']
+
+    ddataDir = '{}/{}/{}'.format(dataDir, dbName, runType)
+    fis = glob.glob('{}/*.hdf5'.format(ddataDir))
+
+    ddict = {}
+    for seas in seasons:
+        ddict[seas] = pd.DataFrame()
+
+    print('ooo', fis, outDir_full)
+    for fi in fis:
+        df = pd.read_hdf(fi)
+
+        for seas in seasons:
+            idx = df['season'] == seas
+            dfs = df[idx]
+            ddict[seas] = pd.concat((ddict[seas], dfs))
+
+    data_tot = pd.DataFrame()
+    for seas, data in ddict.items():
+
+        outName = '{}/SN_simu_params_{}_{}_{}_{}{}.hdf5'.format(
+            outDir_full, fieldType, dbName, timescale, seas, suffix)
+
+        data[ccols].to_hdf(outName, key='simu', index=False)
+        data_tot = pd.concat((data_tot, data[ccols]))
+
+    outName = '{}/SN_simu_params_{}_{}_{}{}.hdf5'.format(
+        outDir_full, fieldType, dbName, timescale, suffix)
+
+    data_tot.to_hdf(outName, key='simu', index=False)
+
+    """
+    for seas in seasons:
+        # load DDFs
+
+        data = load_complete_dbSimu(
+            dataDir, dbName, runType, listDDF=listFields,
+            seasons=str(seas), nproc=nproc, dataType=dataType, suffix=suffix)
+        print(seas, len(data))
+
+        if data.empty:
+            continue
+
+        # extract simulation parameters from data
+        outName = '{}/SN_simu_params_{}_{}_{}_{}{}.hdf5'.format(
+            outDir_full, fieldType, dbName, timescale, seas, suffix)
+
+        data[ccols].to_hdf(outName, key='simu', index=False)
+    """
+
+
 parser = OptionParser()
 
 parser.add_option("--dataDir", type=str,
@@ -93,13 +193,8 @@ parser.add_option("--listFields", type=str,
 parser.add_option("--fieldType", type=str,
                   default='DDF',
                   help=" [%default]")
-parser.add_option("--nsn_factor", type=int,
-                  default=30, help="MC normalisation factor [%default]")
 parser.add_option("--nproc", type=int,
                   default=8, help="nproc for multiprocessing [%default]")
-parser.add_option("--timescale", type=str,
-                  default='year',
-                  help="Time scale for NSN estimation. [%default]")
 parser.add_option("--dataType", type=str,
                   default='pandasDataFrame',
                   help="Data type to process (pandasDataFrame/astropyTable). [%default]")
@@ -115,8 +210,6 @@ selconfig = opts.selconfig
 runType = opts.runType
 fieldType = opts.fieldType
 listFields = opts.listFields
-nsn_factor = opts.nsn_factor
-timescale = opts.timescale
 dataType = opts.dataType
 suffix = opts.suffix
 
@@ -124,8 +217,15 @@ outDir = '{}_simuparams'.format(dataDir)
 nproc = opts.nproc
 seasons = range(1, 13)
 
-get_simu_params(dataDir, dbName, seasons=seasons,
-                runType=runType,
-                listFields=listFields, fieldType=fieldType,
-                outDir=outDir, nproc=nproc,
-                dataType=dataType, suffix=suffix)
+if fieldType == 'WFD':
+    get_simu_params_WFD(dataDir, dbName, seasons=seasons,
+                        runType=runType,
+                        listFields=listFields, fieldType=fieldType,
+                        outDir=outDir, nproc=nproc,
+                        dataType=dataType, suffix=suffix)
+else:
+    get_simu_params_DDF(dataDir, dbName, seasons=seasons,
+                        runType=runType,
+                        listFields=listFields, fieldType=fieldType,
+                        outDir=outDir, nproc=nproc,
+                        dataType=dataType, suffix=suffix)
