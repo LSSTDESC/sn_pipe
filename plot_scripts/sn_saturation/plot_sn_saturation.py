c2f26f564a33f1754b233619a3bca657fdc35147
Gris <philippe.gris@clermont.in2p3.fr>
2024-05-16 17:50:41 +0200

    add plots

--- plot_scripts/sn_saturation/plot_sn_saturation.py
+++ plot_scripts/sn_saturation/plot_sn_saturation.py
@@ -11,9 +11,45 @@ import pandas as pd
 import glob
 from sn_tools.sn_utils import n_z
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def mean_vs(data, varx='z', vary='sigma_mu', bins=np.arange(0.005, 0.11, 0.01)):
+
+    group = data.groupby(pd.cut(data[varx], bins))
+
+    _centers = (bins[:-1] + bins[1:])/2
+    print('kkk', list(group[vary]))
+    _values = group[vary].mean()
+
+    df = pd.DataFrame(_centers, columns=[varx])
+
+    df[vary] = list(_values)
+
+    return df
 
 
 def load_data(dataDir, dbName, zType, fieldType):
+    """
+    Function to load data
+
+    Parameters
+    ----------
+    dataDir : str
+        Data directory.
+    dbName : str
+        dbName to process.
+    zType : str
+        ztype (spectroz, photz, ...).
+    fieldType : str
+        Field type to process.
+
+    Returns
+    -------
+    df : pandas df
+        output data.
+
+    """
 
     fullPath = '{}/{}/{}_{}'.format(dataDir, dbName, fieldType, zType)
 
@@ -26,6 +62,36 @@ def load_data(dataDir, dbName, zType, fieldType):
         df = pd.concat((df, dd))
 
     return df
+
+
+def plot_nsn_z(vals, fig=None, ax=None):
+
+    if fig is None:
+        fig, ax = plt.subplots()
+
+    nsn_tot = len(vals)
+    idx = vals['remove_sat'] == 0
+    sel = vals[idx]
+    print(key, len(sel), nsn_tot-len(sel), len(sel)/norm_factor)
+    nsn_z = n_z(sel, norm_factor=norm_factor)
+    ax.plot(nsn_z['z'], np.cumsum(nsn_z['nsn']))
+    nsn_zb = n_z(vals[~idx], norm_factor=norm_factor)
+    ax.plot(nsn_zb['z'], np.cumsum(nsn_zb['nsn']), linestyle='dashed')
+
+
+def plot_sigma_mu_z(vals, varp='sigma_mu', fig=None, ax=None):
+
+    if fig is None:
+        fig, ax = plt.subplots()
+
+    nsn_tot = len(vals)
+    idx = vals['remove_sat'] == 0
+    sel = vals[idx]
+    print(key, len(sel), nsn_tot-len(sel), len(sel)/norm_factor)
+    nsn_z = mean_vs(sel, varx='z', vary=varp)
+    ax.plot(nsn_z['z'], nsn_z[varp])
+    nsn_zb = mean_vs(vals[~idx], varx='z', vary=varp)
+    ax.plot(nsn_zb['z'], nsn_zb[varp], linestyle='dashed')
 
 
 parser = OptionParser()
@@ -58,15 +124,14 @@ for i, ll in thelist.iterrows():
     data_dict[nn] = load_data(dataDir, dbName, zType, fieldType)
 
 fig, ax = plt.subplots()
+figb, axb = plt.subplots()
 
 for key, vals in data_dict.items():
     print('processing', key)
-    nsn_tot = len(vals)
-    idx = vals['remove_sat'] == 0
-    sel = vals[idx]
-    print(key, len(sel), nsn_tot-len(sel))
-    nsn_z = n_z(sel, norm_factor=norm_factor)
-    ax.plot(nsn_z['z'], nsn_z['nsn'])
-    nsn_zb = n_z(vals[~idx], norm_factor=norm_factor)
-    ax.plot(nsn_zb['z'], nsn_zb['nsn'], linestyle='dotted')
+    idx = vals['z'] <= 0.11
+    vals = vals[idx]
+    # plot_nsn_z(vals, fig=fig, ax=ax)
+    # plot_sigma_mu_z(vals, fig=figb, ax=axb)
+    ax.plot(vals['z'], vals['sigma_mu'], 'ko')
+
 plt.show()
