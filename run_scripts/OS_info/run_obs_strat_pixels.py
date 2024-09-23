57b2c37abbb109a9011b65e70fc512e7a512345b
pgris <gris@clermont.in2p3.fr>
2024-09-23 14:07:29 +0200

    add input params to the metric

--- run_scripts/OS_info/run_obs_strat_pixels.py
+++ run_scripts/OS_info/run_obs_strat_pixels.py
@@ -6,6 +6,7 @@ Created on Fri Sep 20 16:50:42 2024
 @author: philippe.gris@clermont.in2p3.fr
 """
 import sn_script_input
+import sn_metrics_input
 from sn_tools.sn_io import make_dict_from_config, make_dict_from_optparse
 from sn_tools.sn_io import add_parser
 from optparse import OptionParser
@@ -14,22 +15,30 @@ from sn_metrics.sn_obs_strat_pixels import SNObsStratPixel
 
 # get all possible simulation parameters and put in a dict
 path_process_input = sn_script_input.__path__
+path_obstrat_input = sn_metrics_input.__path__
 
 confDict_gen = make_dict_from_config(
     path_process_input[0], 'config_process.txt')
+confDict_obstrat = make_dict_from_config(
+    path_obstrat_input[0], 'config_SN_ObsStratPixels.txt')
 
 parser = OptionParser()
 
 # parser for simulation parameters : 'dynamical' generation
 add_parser(parser, confDict_gen)
+add_parser(parser, confDict_obstrat)
 
 opts, args = parser.parse_args()
 
 procDict = {}
 for key, vals in confDict_gen.items():
     procDict[key] = eval('opts.{}'.format(key))
+metDict = {}
+for key, vals in confDict_obstrat.items():
+    metDict[key] = eval('opts.{}'.format(key))
 
-metricList = [SNObsStratPixel()]
+
+metricList = [SNObsStratPixel(**metDict)]
 """
 fieldType = yaml_params['Observations']['fieldtype']
 fieldName = yaml_params['Observations']['fieldname']
