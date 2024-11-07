d8dca08263a7ad99542acb3cefd733c338f9b90b
pgris <gris@clermont.in2p3.fr>
2024-11-07 11:14:05 +0100

    plot improvement

--- plot_scripts/desc_desi/display_effi_z.py
+++ plot_scripts/desc_desi/display_effi_z.py
@@ -7,16 +7,37 @@ Created on Thu Nov  7 10:47:09 2024
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+import glob
+
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['axes.titlesize'] = 12
+plt.rcParams['axes.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 12
+plt.rcParams['legend.facecolor'] = 'w'
+plt.rcParams['figure.figsize'] = (11, 6)
+plt.rcParams['figure.titlesize'] = 12
+plt.rcParams['axes.labelweight'] = 'bold'
+plt.rcParams['axes.titleweight'] = 'bold'
+plt.rcParams['figure.titleweight'] = 'bold'
+plt.rcParams['font.size'] = 20
+plt.rcParams['font.weight'] = 'bold'
 
 inputDir = 'input/cosmology/host_effi'
 
-files = ['host_effi_desi2_21_v3.csv']
+search_path = '{}/*v3*.csv'.format(inputDir)
+
+fis = glob.glob(search_path)
+fis += ['{}/host_effi_TiDES.csv'.format(inputDir)]
 
 fig, ax = plt.subplots()
-for fi in files:
-    path = '{}/{}'.format(inputDir, fi)
-    df = pd.read_csv(path, comment='#')
-    ax.plot(df['z'], df['effi'])
+for fi in fis:
+    df = pd.read_csv(fi, comment='#')
+    fName = fi.split('/')[-1].split('.csv')[0].split('host_effi_')[-1]
+    ax.plot(df['z'], df['effi'], label=fName)
 
 ax.grid(visible='True')
+ax.legend()
+ax.set_xlabel(r'Redshift')
+ax.set_ylabel(r'Efficiency')
 plt.show()
