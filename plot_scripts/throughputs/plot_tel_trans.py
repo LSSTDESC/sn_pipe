f73afbd44c12abce4b0c2ec8fd67c96e176ff489
pgris <gris@clermont.in2p3.fr>
2024-11-15 14:11:08 +0100

    add a script to display telescope transmission

---
+++ plot_scripts/throughputs/plot_tel_trans.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 10:41:08 2024
+
+@author: philippe.gris@clermont.in2p3;fr
+"""
+
+from sn_telmodel.sn_telescope_new import Telescope
+import matplotlib.pyplot as plt
+
+
+telescope = Telescope()
+telescope.plot_components()
+
+plt.show()
