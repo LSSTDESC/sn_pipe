13ee60ed11a891887248dc9235f30efa9aff6020
Gris <philippe.gris@clermont.in2p3.fr>
2024-11-20 18:47:04 +0100

    inclue plot legend

--- plot_scripts/throughputs/plot_atmos_trans.py
+++ plot_scripts/throughputs/plot_atmos_trans.py
@@ -9,10 +9,41 @@ Created on Wed Nov 20 11:15:04 2024
 from sn_telmodel.sn_atmosphere import Atmos_Transmission
 import matplotlib.pyplot as plt
 
-atmos_trans = Atmos_Transmission()
+# from file
+atmos_trans_file = Atmos_Transmission(atmos_type='from_file')
+atmos_trans_file.load_atmosphere()
 
-atmos_trans.load_atmosphere()
+# from getObsAtmo
+atmos_trans_obsatmo = Atmos_Transmission(atmos_type='obsatmo')
+atmos_trans_obsatmo.load_atmosphere(aerosol=0.04)
 
-atmos_trans.plot_atmospheric_transmission()
+
+params = {}
+par_names = ['airmass', 'aerosol', 'pwv', 'oz', 'beta', 'pressure']
+par_plotnames = ['am', 'aer', 'pwv', 'oz', 'beta', 'P']
+pars = dict(zip(par_names, par_plotnames))
+for key, vals in pars.items():
+    sstr = 'params[\'{}\'] = atmos_trans_obsatmo.{}'.format(vals, key)
+    exec(sstr)
+
+ra = []
+rb = []
+for key, vals in params.items():
+    ra.append(key)
+    rb.append(vals)
+
+ran = ','.join(ra)
+rb = list(map(str, rb))
+rbn = ','.join(rb)
+
+
+labela = 'from file airmass({})+aerosol'.format(atmos_trans_file.airmass)
+labelb = '({})=({})'.format(ran, rbn)
+
+fig, ax = plt.subplots(figsize=(12, 8))
+atmos_trans_file.plot_atmospheric_transmission(
+    plt, fig=fig, ax=ax, label=labela)
+atmos_trans_obsatmo.plot_atmospheric_transmission(
+    plt, fig=fig, ax=ax, label=labelb, color='k', linestyle='dashed')
 
 plt.show()
