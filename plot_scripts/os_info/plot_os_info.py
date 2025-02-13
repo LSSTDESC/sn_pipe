01acf97d3fa36008aa442b79c09801ef65d7f30f
pgris <gris@clermont.in2p3.fr>
2025-02-13 16:49:49 +0100

    add a script to plot os info at the pixel level

---
+++ plot_scripts/os_info/plot_os_info.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 16:42:18 2025
+
+@author: philippe.gris@clermont.in2p3.fr
+"""
+from optparse import OptionParser
+import pandas as pd
+
+parser = OptionParser(description='Script to plot pixel level OS infos')
+
+parser.add_option('--dbName', type=str, default='test_newb',
+                  help='dbName to process [%default]')
+parser.add_option('--dbDir', type=str, default='../test_metric',
+                  help='dbDir of the OS to process [%default]')
+
+opts, args = parser.parse_args()
+
+dbDir = opts.dbDir
+dbName = opts.dbName
+
+fName = '{}/{}.hdf5'.format(dbDir, dbName)
+
+df = pd.read_hdf(fName)
+
+print(df)
