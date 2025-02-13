18d20a9f1eb708ebd2fcf6ea865a1edfde320004
pgris <gris@clermont.in2p3.fr>
2025-02-13 16:31:13 +0100

    add a script to superimpose LSST filters on atmos transmission

---
+++ plot_scripts/throughputs/plot_atmos_trans_filters.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 11:15:04 2024
+
+@author: philippe.gris@clermont.in2p3.fr
+"""
+
+from sn_telmodel.sn_atmosphere import Atmos_Transmission
+from sn_telmodel.sn_telescope import Telescope
+import matplotlib.pyplot as plt
+
+# from file
+atmos_trans_file = Atmos_Transmission(atmos_type='from_file')
+atmos_trans_file.load_atmosphere()
+
+# from getObsAtmo
+atmos_trans_obsatmo = Atmos_Transmission(atmos_type='obsatmo')
+atmos_trans_obsatmo.load_atmosphere(aerosol=0.04)
+
+
+params = {}
+par_names = ['airmass', 'aerosol', 'pwv', 'oz', 'beta', 'pressure']
+par_plotnames = ['am', 'aer', 'pwv', 'oz', 'beta', 'P']
+pars = dict(zip(par_names, par_plotnames))
+for key, vals in pars.items():
+    sstr = 'params[\'{}\'] = atmos_trans_obsatmo.{}'.format(vals, key)
+    exec(sstr)
+
+ra = []
+rb = []
+for key, vals in params.items():
+    ra.append(key)
+    rb.append(vals)
+
+ran = ','.join(ra)
+rb = list(map(str, rb))
+rbn = ','.join(rb)
+
+
+labela = 'from file airmass({})+aerosol'.format(atmos_trans_file.airmass)
+labelb = '({})=({})'.format(ran, rbn)
+
+fig, ax = plt.subplots(figsize=(12, 8))
+atmos_trans_file.plot_atmospheric_transmission(
+    plt, fig=fig, ax=ax, label=labela)
+atmos_trans_obsatmo.plot_atmospheric_transmission(
+    plt, fig=fig, ax=ax, label=labelb, color='k', linestyle='dashed')
+
+telescope = Telescope()
+
+
+plt.show()
