e537e926cf628a0363f220c0786eb28d9e2830e5
pgris <gris@clermont.in2p3.fr>
2025-03-12 10:02:06 +0100

    code update

--- run_scripts/scheduler/ddf_rubin_scheduler_auto.py
+++ run_scripts/scheduler/ddf_rubin_scheduler_auto.py
@@ -261,7 +261,7 @@ def get_val_field(df, field, colName):
 
     Parameters
     ----------
-    df : pandas df
+    df : numpy array
         Survey parameters.
     field : str
         DDF field name.
@@ -275,71 +275,27 @@ def get_val_field(df, field, colName):
 
     """
 
-    return df[df['field'] == field][colName].to_list()
-
-
-def get_survey(survey_field_config, survey_field_visits):
-    """
-    Function to load the DDF survey config
-
-    Parameters
-    ----------
-    survey_field_config : str
-        Survey field config file.
-    survey_field_visits : str
-        survey config visits file
-
-    Returns
-    -------
-    df : pandas df
-        survey.
-
-    """
-
-    dfa = pd.read_csv(survey_field_config, comment='#')
-
-    dfb = pd.read_csv(survey_field_visits, comment='#')
-
-    df = dfa.merge(dfb, left_on=['fieldType'], right_on=[
-                   'target'], suffixes=['', ''])
-
-    df = df.drop(columns=['fieldType', 'target'])
-    df = df.rename(columns={'fieldName': 'field'})
-
-    # correct for EDFs
-    df['field_str'] = df['field'].str.split('_', expand=True)[0]
-    idx = df['field_str'] == 'EDFS'
-    sel = pd.DataFrame(df[idx])
-    sel['season_seq'] /= 2
-    sel['season_seq'] = sel['season_seq'].astype(int)
-
-    dfb = df[~idx]
-    dfb = pd.concat((dfb, sel))
-    return dfb
+    return df[df['field'] == field][colName].tolist()
 
 
 parser = OptionParser(
     description='Script to produce DDF observations for the LSST scheduler')
 
 parser.add_option('--inputDir', type=str,
-                  default='input/scheduler',
+                  default='../survey_lsst_scheduler',
                   help='Location dir of input files [%default]')
 parser.add_option('--outputDir', type=str,
                   default='../desc_ddf_deep_rolling_auto',
                   help='output dir of the produced files [%default]')
-parser.add_option('--ddf_survey_fields', type=str,
-                  default='deep_rolling_survey.csv',
-                  help='config file for fields [%default]')
-parser.add_option('--ddf_survey_visits', type=str,
-                  default='ddf_desc_0.70_sn.csv',
+parser.add_option('--survey', type=str,
+                  default='ddf_desc_0.70_sn.npy',
                   help='config file for visits [%default]')
 
 opts, args = parser.parse_args()
 
 inputDir = opts.inputDir
 outputDir = opts.outputDir
-ddf_survey_fields = opts.ddf_survey_fields
-ddf_survey_visits = opts.ddf_survey_visits
+survey = opts.survey
 
 
 """
@@ -351,16 +307,13 @@ plt.show()
 # check if output dir exist
 checkDir(outputDir)
 
-# load the survey config
+# load the survey
 
-df_survey = get_survey(f'{inputDir}/{ddf_survey_fields}',
-                       f'{inputDir}/{ddf_survey_visits}')
-
-print(df_survey)
+ddf_survey = np.load(f'{inputDir}/{survey}', allow_pickle=True)
 
 
 # grab ddf configuration for rubin survey
-ddf_kwargs = ddf_config(survey=df_survey)
+ddf_kwargs = ddf_config(survey=ddf_survey)
 
 print(ddf_kwargs)
 
@@ -369,5 +322,5 @@ print(ddf_kwargs)
 observations = generate_ddf_scheduled_obs_auto(dist_tol=1,
                                                ddf_kwargs=ddf_kwargs)
 # save the file
-outName = '{}/{}.npy'.format(outputDir, ddf_survey_visits.split('.csv')[0])
+outName = '{}/{}'.format(outputDir, survey)
 np.save(outName, observations)
