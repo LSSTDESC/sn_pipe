348e43cd37f1fa97077207b89c61aea3718835d3
pgris <gris@clermont.in2p3.fr>
2025-04-02 15:44:58 +0200

    bug corr

--- for_batch/scripts/wfd_os_pixels/run_wfd_pixels.py
+++ for_batch/scripts/wfd_os_pixels/run_wfd_pixels.py
@@ -69,8 +69,8 @@ for i, row in dbs.iterrows():
 
         mybatch = BatchIt(processName=procName, time=proctime, mem=procmem)
 
-        procDict['RA_min'] = RA_min
-        procDict['RA_max'] = RA_min
+        procDict['RAmin'] = RA_min
+        procDict['RAmax'] = RA_min
         procDict['prodID'] = '{}.hdf5'.format(procName)
 
         mybatch.add_batch(scriptref, procDict)
