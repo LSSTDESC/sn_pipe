fecef889e595e3ff493b2908ca65d31b35531b77
pgris <gris@clermont.in2p3.fr>
2025-04-09 10:52:56 +0200

    add multiprocessing

--- run_scripts/sn_studies/extract_pixels.py
+++ run_scripts/sn_studies/extract_pixels.py
@@ -10,6 +10,47 @@ from optparse import OptionParser
 import pandas as pd
 import glob
 from sn_tools.sn_io import checkDir
+from sn_tools.sn_utils import multiproc
+
+
+def process_file(fis, params, j=0, output_q=None):
+    """
+    Function to process files
+
+    Parameters
+    ----------
+    fis : list(str)
+        List of files to process (full path).
+    params : dict
+        parameters.
+    j : int, optional
+        internal tag for multiprocessing. The default is 0.
+    output_q : multiprocessing queue, optional
+        where to put the results. The default is None.
+
+    Returns
+    -------
+    dict/pandas df
+        results.
+
+    """
+
+    cols = params['cols']
+    drop_duplicates = params['drop_duplicates']
+
+    df_res = pd.DataFrame()
+    for fi in fis:
+        df = pd.read_hdf(fi)
+        df = df[cols]
+        if drop_duplicates:
+            df = df.drop_duplicates()
+        df_res = pd.concat((df_res, df))
+
+    if output_q is not None:
+        return output_q.put({j: df_res})
+    else:
+        return df_res
+
 
 parser = OptionParser(description='Script to extract pixel infos')
 
@@ -28,6 +69,12 @@ parser.add_option('--runType', type=str,
 parser.add_option('--outDir', type=str,
                   default='../pixels_DDF',
                   help='output Dir [%default]')
+parser.add_option('--nproc', type=int,
+                  default=8,
+                  help='nproc for multiprocessing [%default]')
+parser.add_option('--drop_duplicate', type=int,
+                  default=0,
+                  help='to drop duplicate [%default]')
 
 opts, args = parser.parse_args()
 
@@ -36,6 +83,8 @@ dbList = opts.dbList
 fieldType = opts.fieldType
 runType = opts.runType
 outDir = opts.outDir
+nproc = opts.nproc
+drop_duplicate = opts.drop_duplicate
 
 # create outDir if necessary
 checkDir(outDir)
@@ -43,24 +92,30 @@ checkDir(outDir)
 # load config
 df_config = pd.read_csv(dbList, comment='#')
 
-print(df_config)
-
 cols = ['healpixID', 'pixRA', 'pixDec', 'season', 'season_length']
 cols += ['survey_area', 'x1', 'color']
+
+params = {}
+params['cols'] = cols
+params['drop_duplicates'] = drop_duplicate
 
 for i, row in df_config.iterrows():
     dbName = row['dbName']
     dirName = '{}/{}/{}_{}'.format(dbDir, dbName, fieldType, runType)
     df_res = pd.DataFrame()
     fis = glob.glob('{}/*.hdf5'.format(dirName))
+    df_res = multiproc(fis, params, process_file, nproc)
+
+    """
     for fi in fis:
         df = pd.read_hdf(fi)
         df = df[cols]
         # df = df.drop_duplicates()
         df_res = pd.concat((df_res, df))
-    print(len(df_res))
     # df_res = df_res.drop_duplicates()
-    print(len(df_res))
+    """
+    if drop_duplicate:
+        df_res = df_res.drop_duplicates()
     print(df_res)
     outName = '{}/{}.hdf5'.format(outDir, dbName)
     df_res.to_hdf(outName, key='pixels')
