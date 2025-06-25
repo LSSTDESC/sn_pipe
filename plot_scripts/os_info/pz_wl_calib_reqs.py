4da2d690ac70ef6076d61dec81ff40eec2e95343
pgris <gris@clermont.in2p3.fr>
2025-06-25 14:00:08 +0200

    code update

--- plot_scripts/os_info/pz_wl_calib_reqs.py
+++ plot_scripts/os_info/pz_wl_calib_reqs.py
@@ -39,6 +39,8 @@ def plot_nvisits_all(data, dfconfig, fields, df_calib=pd.DataFrame(),
     None.
 
     """
+    print('alors', data.columns)
+    print(df_calib.columns)
 
     for field in fields:
         for b in bands:
@@ -76,9 +78,10 @@ def plot_nvisits(data, df_config, b, field='DD:XMM_LSS',
 
     bband = '$'+b+'$-band'
     laby = '$\Sigma N_{visits}$ - ' + bband
+    thevar = 'Nvisits_{}'.format(b)
     ax = plot_summary(data, field=field,
                       varx='year', labx='year',
-                      vary='Nvisits_{}'.format(b), laby=laby,
+                      vary=thevar, laby=laby,
                       figtit=field,
                       df_config=df_config, cumsum=cumsum)
 
@@ -92,29 +95,33 @@ def plot_nvisits(data, df_config, b, field='DD:XMM_LSS',
               ncol=1, fontsize=12, frameon=False)
 
 
-def get_reqs(nvisits=[48, 72, 184, 184, 152, 160]):
+def get_reqs(bands='ugrizy',
+             nvisits=[48, 72, 184, 184, 152, 160],
+             years=range(1, 11)):
     """
     Function to convert a list of visits per year to a df.
 
     Parameters
     ----------
+    bands : list(str), optional
+        colnames for output df. The default is 'ugrizy'.
     nvisits : list(int), optional
-        Number of visits per band. The default is [48, 72, 184, 184, 152, 160].
+        List of visits. The default is [48, 72, 184, 184, 152, 160].
+    years : list(int), optional
+        List of years. The default is range(1, 11).
 
     Returns
     -------
     df_calib : pandas df
-        output data.
+        Outout df.
 
     """
-    bands = 'ugrizy'
 
     nvisits = map(lambda x: [x], nvisits)
     dd = dict(zip(bands, nvisits))
     df_a = pd.DataFrame.from_dict(dd)
 
     ddb = {}
-    years = range(1, 11)
     # years = map(lambda x: [x], years)
     ddb['year'] = years
     df_b = pd.DataFrame.from_dict(ddb)
@@ -124,7 +131,11 @@ def get_reqs(nvisits=[48, 72, 184, 184, 152, 160]):
     return df_calib
 
 
-def ana_reqs(data, reqs, bands='ugrizy', dbName='ddf_ocean_ocean6_v4.3.5_10yrs'):
+def ana_reqs(data, reqs,
+             req_name='WL reqs',
+             bands='ugrizy',
+             dbName='ddf_ocean_ocean6_v4.3.5_10yrs',
+             fields=['DD:COSMOS']):
     """
     Function to analyse reqs wrt OS results
 
@@ -134,6 +145,77 @@ def ana_reqs(data, reqs, bands='ugrizy', dbName='ddf_ocean_ocean6_v4.3.5_10yrs')
         Data to process.
     reqs : pandas df
         Requirements.
+    req_name : str, optional
+        Req name (for the plot). The default is 'WL reqs'.
+    bands : list(str), optional
+        Bands to consider. The default is 'ugrizy'.
+    dbName : str, optional
+        OS of interest. The default is 'ddf_ocean_ocean6_v4.3.5_10yrs'.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    idx = data['dbName'] == dbName
+    idx = data['target_name'].isin(fields)
+
+    data = pd.DataFrame(data[idx])
+
+    # take the cumulative
+    for b in bands:
+        reqs = reqs.sort_values(by=['year'])
+        reqs['Nvisits_sum_{}'.format(b)] = np.cumsum(
+            reqs['{}'.format(b)])
+
+    data = data.sort_values(by=['year'])
+    data = data.groupby(['dbName', 'target_name']
+                        ).apply(lambda x: get_cumsum(x, bands), include_groups=False).reset_index()
+    cols = ['year', 'Nvisits_y', 'Nvisits_sum_y']
+
+    data = clean_level(data)
+    # select years 5, 7 and 10
+    years = [5, 8, 10]
+
+    idxa = reqs['year'].isin(years)
+
+    reqs_sel = pd.DataFrame(reqs[idxa])
+
+    idxb = data['year'].isin(years)
+
+    data_sel = pd.DataFrame(data[idxb])
+
+    data_m = data_sel.merge(reqs_sel, left_on=['year'], right_on=[
+                            'year'], suffixes=['', '_ref'])
+
+    for b in bands:
+        data_m['diff_sum_{}'.format(b)] = data_m['Nvisits_sum_{}'.format(
+            b)]-data_m['Nvisits_sum_{}_ref'.format(b)]
+
+    # select ddf_ocean_ocean6_v4.3.5_10yrs
+    idx = data_m['dbName'] == 'ddf_ocean_ocean6_v4.3.5_10yrs'
+
+    sel_m = data_m[idx]
+
+    plot_calib(sel_m, bands=bands, req_name=req_name)
+
+
+def ana_reqs_m5(data, reqs,
+                req_name='PZ reqs',
+                bands='ugrizy',
+                dbName='ddf_ocean_ocean6_v4.3.5_10yrs',):
+    """
+    Function to analyse reqs wrt OS results
+
+    Parameters
+    ----------
+    data : pandas df
+        Data to process.
+    reqs : pandas df
+        Requirements.
+    req_name : str, optional
+        Req name (for the plot). The default is 'WL reqs'.
     bands : list(str), optional
         Bands to consider. The default is 'ugrizy'.
     dbName : str, optional
@@ -148,54 +230,77 @@ def ana_reqs(data, reqs, bands='ugrizy', dbName='ddf_ocean_ocean6_v4.3.5_10yrs')
     idx = data['dbName'] == dbName
     data = pd.DataFrame(data[idx])
 
-    # take the cumulative
-    for b in bands:
-        reqs = reqs.sort_values(by=['year'])
-        reqs['Nvisits_sum_{}'.format(b)] = np.cumsum(
-            reqs['{}'.format(b)])
-        """
-        data['Nvisits_sum_{}'.format(b)] = np.cumsum(
-            data['Nvisits_{}'.format(b)])
-        """
-
-    print('allo', reqs)
     data = clean_level(data)
     data = data.sort_values(by=['year'])
-    data = data.groupby(['dbName', 'target_name']
-                        ).apply(lambda x: get_cumsum(x, bands), include_groups=False).reset_index()
-    cols = ['year', 'Nvisits_y', 'Nvisits_sum_y']
-    print(data[cols])
+    datab = data.groupby(['dbName', 'target_name']
+                         ).apply(lambda x: get_cum_m5(x, bands), include_groups=False).reset_index()
+    cols = ['year', 'target_name', 'fiveSigmaDepth_r', 'fiveSigmaDepth_z']
+    # print(datab[cols])
 
     data = clean_level(data)
-    # select years 5, 7 and 10
-    years = [5, 8, 10]
 
-    idxa = reqs['year'].isin(years)
+    data_m = datab.merge(reqs, left_on=['year'], right_on=[
+        'year'], suffixes=['', '_ref'])
 
-    reqs_sel = pd.DataFrame(reqs[idxa])
-
-    idxb = data['year'].isin(years)
-
-    data_sel = pd.DataFrame(data[idxb])
-
-    print(data_sel)
-
-    data_m = data_sel.merge(reqs_sel, left_on=['year'], right_on=[
-                            'year'], suffixes=['', '_ref'])
-
-    print(data_m)
+    # print(data_m)
+    thevar = 'fiveSigmaDepth'
     for b in bands:
-        data_m['diff_sum_{}'.format(b)] = data_m['Nvisits_sum_{}'.format(
-            b)]-data_m['Nvisits_sum_{}_ref'.format(b)]
+        data_m['diff_{}_{}'.format(thevar, b)] = data_m['{}_{}'.format(thevar,
+                                                                       b)]-data_m['{}_{}_ref'.format(thevar, b)]
 
-    print(data_m)
+    # print(data_m)
 
     # select ddf_ocean_ocean6_v4.3.5_10yrs
     idx = data_m['dbName'] == 'ddf_ocean_ocean6_v4.3.5_10yrs'
 
     sel_m = data_m[idx]
 
-    plot_calib(sel_m, bands=bands)
+    plot_calib(sel_m, bands=bands, req_name=req_name,
+               prefix='diff_fiveSigmaDepth', laby='$\Delta m_5=m_5^{obs}-m_5^{req}$')
+
+
+def get_cum_m5(grp, bands, years=[[1, 1], [2, 10]]):
+    """
+    get the m5_cumul for a set of years
+
+    Parameters
+    ----------
+    grp : pandas df
+        Data to process.
+    bands : list(str)
+        Bands to consider.
+    years : list(list(int)), optional
+        Year ranges [ymin,ymax] to consider for the estimation.
+        The default is [[1, 1], [2, 10]].
+
+    Returns
+    -------
+    df : TYPE
+        DESCRIPTION.
+
+    """
+
+    df = pd.DataFrame()
+    for yy in years:
+        y_min = yy[0]
+        y_max = yy[1]
+
+        idx = grp['year'] >= y_min
+        idx &= grp['year'] <= y_max
+
+        sel = grp[idx]
+
+        dd = {}
+        for b in bands:
+            vv = 'fiveSigmaDepth_{}'.format(b)
+            tt = 1.25*np.log10(np.sum(10**(0.8*sel[vv])))
+            dd[vv] = [tt]
+
+        dd['year'] = y_max
+        dfa = pd.DataFrame.from_dict(dd)
+        df = pd.concat((df, dfa))
+
+    return df
 
 
 def get_cumsum(grp, bands):
@@ -224,7 +329,9 @@ def get_cumsum(grp, bands):
     return grp
 
 
-def plot_calib(data, bands):
+def plot_calib(data, bands, req_name,
+               prefix='diff_sum',
+               laby='$\Delta N_{visits}=N_{visits}^{obs}-N_{visits}^{req}$'):
     """
     Function to plot calb req results
 
@@ -234,7 +341,10 @@ def plot_calib(data, bands):
         Data to process.
     bands : list(str)
         Filters to consider.
-
+    req_name : str
+        req name (for the plot).
+    prefix : str, optional
+        What to plot. The default is 'diff_sum'.
     Returns
     -------
     None.
@@ -252,7 +362,9 @@ def plot_calib(data, bands):
     for b in bands:
         fig, ax = plt.subplots(figsize=(16, 8))
         # fig.suptitle(figtit)
-        fig.suptitle('{}-band'.format(b))
+        figtit = req_name
+        figtit += ' \n {}-band'.format(b)
+        fig.suptitle(figtit)
         fig.subplots_adjust(right=0.75)
 
         target = data['target_name'].unique()
@@ -261,12 +373,14 @@ def plot_calib(data, bands):
             idx = data['target_name'] == tt
             sel = data[idx]
             ii = fields.index(tt)
-            ax.plot(sel['year'], sel['diff_sum_{}'.format(b)],
+            ax.plot(sel['year'], sel['{}_{}'.format(prefix, b)],
                     marker=markers[ii],
                     color=colors[ii],
                     linestyle=linestyles[ii], label=tt, markersize=8, mfc='None')
 
         ax.grid(visible=True)
+        ax.set_xlabel(r'year')
+        ax.set_ylabel(r'{}'.format(laby))
 
         ax.legend(loc='upper center',
                   bbox_to_anchor=(1.20, 0.7),
@@ -281,14 +395,17 @@ parser.add_option('--config', type=str,
 parser.add_option("--dirFile", type="str",
                   default='../nvisits_m5',
                   help="file directory [%default]")
-parser.add_option("--fields", type="str", default='COSMOS,CDFS,XMM-LSS,ELAISS1,EDFSa,EDFSb',
+parser.add_option("--fields", type="str", default='DD:COSMOS,DD:ECDFS,DD:XMM_LSS,DD:ELAISS1,DD:EDFS_a,DD:EDFS_b',
                   help="fields to process [%default]")
+parser.add_option("--plots", type="str", default='plot_global_wl,plot_global_agn,summary_reqs_wl,summary_reqs_agn,summary_reqs_pz',
+                  help="plots to draw [%default]")
 
 opts, args = parser.parse_args()
 
 dirFile = opts.dirFile
 config = opts.config
 fields = opts.fields.split(',')
+plots = opts.plots.split(',')
 
 # load config
 df_config = pd.read_csv(config, comment='#')
@@ -300,17 +417,38 @@ for i, row in df_config.iterrows():
     data = pd.concat((data, df))
 
 # WL and requirements per year
+bands = list('ugrizy')
 nvisits_wl = [48, 72, 184, 184, 152, 160]
 nvisits_agn = [360, 90, 90, 270, 450, 360]
-df_wl = get_reqs(nvisits_wl)
-df_agn = get_reqs(nvisits_agn)
+df_wl = get_reqs(bands, nvisits_wl)
+df_agn = get_reqs(bands, nvisits_agn)
 
-idx = data['dbName'] == 'ddf_ocean_ocean6_v4.3.5_10yrs'
-plot_nvisits_all(data[idx], df_config, fields,
-                 df_agn, calib_label='WL calib reqs')
-"""
-plot_nvisits_all(data, df_config, fields, df_agn, calib_label='AGN reqs')
-"""
+if 'plot_global_wl' in plots:
+    plot_nvisits_all(data, df_config, fields,
+                     df_wl, calib_label='WL calib reqs')
 
-ana_reqs(data, df_agn)
+if 'plot_global_agn' in plots:
+    plot_nvisits_all(data, df_config, fields, df_agn, calib_label='AGN reqs')
+
+if 'summary_reqs_wl' in plots:
+    ana_reqs(data, df_wl, req_name='WL reqs.', fields=fields)
+
+if 'summary_reqs_agn' in plots:
+    ana_reqs(data, df_agn, req_name='AGN reqs.', fields=fields)
+
+
+# pz requirements
+
+
+pz_y1 = [26.7, 27.0, 26.2, 25.8, 25.6, 24.7]
+pz_y10 = [27.8, 28.1, 27.8, 27.6, 27.2, 26.5]
+
+bands = list(map(lambda x: 'fiveSigmaDepth_' + x, bands))
+req_pz_y1 = get_reqs(bands, pz_y1, years=[1])
+req_pz_y10 = get_reqs(bands, pz_y10, years=[10])
+req_pz = pd.concat((req_pz_y1, req_pz_y10))
+
+if 'summary_reqs_pz' in plots:
+    ana_reqs_m5(data, req_pz, req_name='PZ reqs.')
+
 plt.show()
