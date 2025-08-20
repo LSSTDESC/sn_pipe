3d024a1e93bca0ff45b6a9ac537d109eff3b2e8d
pgris <gris@clermont.in2p3.fr>
2025-08-20 15:18:14 +0200

    add a script to fit atmos transmission

---
+++ run_scripts/telescope/fit_atmos_from_file.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 11:15:04 2024
+
+@author: philippe.gris@clermont.in2p3.fr
+"""
+
+import numpy as np
+from sn_telmodel.sn_atmosphere import Atmos_Transmission
+import matplotlib.pyplot as plt
+from optparse import OptionParser
+from iminuit import Minuit
+
+
+class Fit_Atmos:
+    def __init__(self, airmass=1.0,
+                 fitparNames=['ozone', 'aerosol', 'pwv'],
+                 fitparValues=[300., 0.05, 4.],
+                 fitparLimits=[(100., 500.), (0., 1.), (0.5, 12.)]):
+        """
+        class to fit atmospheric parameters on a spectrum
+
+        Parameters
+        ----------
+        airmass : float, optional
+            airmass value. The default is 1.0.
+        fitparNames : list(str), optional
+            List of the names of the parameters to fit. 
+            The default is ['ozone', 'aerosol', 'pwv'].
+        fitparValues : list(float), optional
+            list of initial values of parameters to fit. 
+            The default is [300., 0.05, 4.].
+        fitparLimits : list(pair(float)), optional
+            parameter limits. The default is [(100.,500.),(0.,1.),(0.5,12.)].
+        Returns
+        -------
+        None.
+
+        """
+
+        self.airmass = airmass
+
+        self.fitparNames = fitparNames
+        self.fitparLimits = fitparLimits
+        self.par_default = dict(zip(fitparNames, fitparValues))
+        """
+        dataValues = [data[key] for key in dataNames]
+        for i, vals in enumerate(dataNames):
+            exec('self.{} = dataValues[{}]'.format(vals, i))
+        """
+        # grab data from file
+        self.atmos_trans_file = Atmos_Transmission(
+            atmos_dir=atmosDir, atmos_type='from_file')
+        self.atmos_trans_file.load_atmosphere(airmass=airmass)
+
+        self.ndata = len(self.atmos_trans_file.atmosphere.wavelen)
+
+    def __call__(self):
+        """
+        call function: where the fit is made
+
+        Returns
+        -------
+        res : TYPE
+            DESCRIPTION.
+
+        """
+
+        params = []
+        for pp in self.fitparNames:
+            params.append(self.par_default[pp])
+
+        res = self.minuit_fit(params)
+
+        print(res)
+
+        return res
+
+    def minuit_fit(self, parameters):
+        """
+        fit using minuit
+
+        Parameters
+        ----------
+        parameters : list(float)
+            Initial parameter values.
+
+        Returns
+        -------
+        dict_out : dict
+            Fit results.
+
+        """
+
+        m = Minuit(self.xi_square, *parameters,
+                   name=self.fitparNames)
+
+        for i, vv in enumerate(self.fitparNames):
+            limits = self.fitparLimits[i]
+            print('limits', vv, limits)
+            m.limits[vv] = limits
+
+        m.migrad()
+        m.hesse()
+
+        # grab the results: param values
+        dict_out = {}
+        res = m.values
+
+        print('alors', res)
+        for name in self.fitparNames:
+            dict_out['{}_fit'.format(name)] = res[name]
+        fitpars = []
+        for pp in self.fitparNames:
+            fitpars.append(dict_out['{}_fit'.format(pp)])
+
+        dict_out['Chi2_fit'] = self.xi_square(*fitpars)
+        dict_out['NDoF'] = self.ndata-len(self.fitparNames)
+        dict_out['Chi2_fit_red'] = dict_out['Chi2_fit']/dict_out['NDoF']
+
+        print(dict_out)
+        # covariance matrix
+        cov = m.covariance
+        print(cov)
+        for i, vala in enumerate(self.fitparNames):
+            for j, valb in enumerate(self.fitparNames):
+                if j <= i:
+                    dict_out['Cov_{}_{}_fit'.format(vala, valb)] = cov[i, j]
+
+        return dict_out
+
+    def xi_square(self, *parameters):
+        """
+        the log likelihood to minimize
+
+        Parameters
+        ----------
+        *parameters : list(float)
+            parameter values.
+
+        Returns
+        -------
+        Xmat : float
+            the log likelihood value.
+
+        """
+
+        ozone = parameters[self.fitparNames.index('ozone')]
+        pwv = parameters[self.fitparNames.index('pwv')]
+        aerosol = parameters[self.fitparNames.index('aerosol')]
+
+        if 'airmass' not in self.fitparNames:
+            airmass = self.airmass
+        else:
+            airmass = parameters[self.fitparNames.index('airmass')]
+        """
+        ozone = parameters[self.fitparNames.index('ozone')]
+        pwv = parameters[self.fitparNames.index('pwv')]
+        aerosol = parameters[self.fitparNames.index('aerosol')]
+
+        
+        airmass = self.airmass
+        if 'airmass' in self.fitparNames:
+            airmass = parameters[self.fitparNames.index('airmass')]
+        """
+        print('allo', airmass, pwv, ozone, aerosol)
+
+        atmos_trans_obsatmo = Atmos_Transmission(atmos_type='obsatmo')
+        atmos_trans_obsatmo.load_atmosphere(
+            airmass=airmass, pwv=pwv, ozone=ozone, aerosol=aerosol)
+
+        vva = self.atmos_trans_file.atmosphere.sb
+        vvb = atmos_trans_obsatmo.atmosphere.sb
+
+        sigma = 0.012
+        Xmat = np.sum((vva-vvb)**2/sigma**2)
+
+        return Xmat
+
+
+parser = OptionParser(description='Script to plot a&tmos transmission')
+
+parser.add_option('--atmosDir', type=str, default='atmos',
+                  help='atmosphere location dir [%default]')
+parser.add_option('--tag', type=str, default='1.9',
+                  help='tag version of the throughputs [%default]')
+parser.add_option('--airmass', type=float, default=1.0,
+                  help='airmass value [%default]')
+parser.add_option('--aerosol', type=float, default=0.0,
+                  help='aerosol value [%default]')
+parser.add_option('--pwv', type=float, default=4.0,
+                  help='precipitable water vapor value [%default]')
+parser.add_option('--ozone', type=float, default=300.,
+                  help='ozone value [%default]')
+
+opts, args = parser.parse_args()
+
+atmosDir = 'throughputs_{}/{}'.format(opts.tag, opts.atmosDir)
+airmass = opts.airmass
+aerosol = opts.aerosol
+pwv = opts.pwv
+ozone = opts.ozone
+
+parNames = ['airmass', 'pwv', 'ozone', 'aerosol']
+parValues = [1.2, 4., 100, 0.1]
+parLimits = [(1., 3.), (0.5, 12.), (100., 500.), (0., 1.)]
+
+myfit = Fit_Atmos(airmass,
+                  fitparNames=parNames,
+                  fitparValues=parValues,
+                  fitparLimits=parLimits)
+
+resfit = myfit()
+
+
+# from file
+atmos_trans_file = Atmos_Transmission(
+    atmos_dir=atmosDir, atmos_type='from_file')
+atmos_trans_file.load_atmosphere(airmass=airmass)
+
+# from getObsAtmo
+pwv = resfit['pwv_fit']
+ozone = resfit['ozone_fit']
+aerosol = resfit['aerosol_fit']
+atmos_trans_obsatmo = Atmos_Transmission(atmos_type='obsatmo')
+atmos_trans_obsatmo.load_atmosphere(
+    airmass=airmass, pwv=pwv, ozone=ozone, aerosol=aerosol)
+
+
+params = {}
+par_names = ['airmass', 'aerosol', 'pwv', 'ozone', 'beta', 'pressure']
+par_plotnames = ['am', 'aer', 'pwv', 'ozone', 'beta', 'P']
+pars = dict(zip(par_names, par_plotnames))
+for key, vals in pars.items():
+    sstr = 'params[\'{}\'] = atmos_trans_obsatmo.{}'.format(vals, key)
+    exec(sstr)
+
+ra = []
+rb = []
+for key, vals in params.items():
+    ra.append(key)
+    rb.append(vals)
+
+ran = ','.join(ra)
+rb = list(map(str, rb))
+rbn = ','.join(rb)
+
+# superimpose atmospheric transmission curves
+labela = 'from file airmass({})+aerosol'.format(atmos_trans_file.airmass)
+labelb = '({})=({})'.format(ran, rbn)
+
+fig, ax = plt.subplots(figsize=(12, 8))
+atmos_trans_file.plot_atmospheric_transmission(
+    plt, fig=fig, ax=ax, label=labela)
+atmos_trans_obsatmo.plot_atmospheric_transmission(
+    plt, fig=fig, ax=ax, label=labelb, color='k', linestyle='dashed')
+
+# residuals
+vva = atmos_trans_file.atmosphere.sb
+vvb = atmos_trans_obsatmo.atmosphere.sb
+
+figb, axb = plt.subplots(figsize=(12, 8))
+axb.plot(atmos_trans_file.atmosphere.wavelen, vva-vvb)
+
+print(np.sum((vva-vvb)**2))
+
+plt.show()
