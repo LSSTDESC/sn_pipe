aa857fb5069f86075fe1e58f08bc6cf83aafd65c
pgris <gris@clermont.in2p3.fr>
2025-08-20 16:45:19 +0200

    add FWHMeff values as input param

--- run_scripts/telescope/telescope_params.py
+++ run_scripts/telescope/telescope_params.py
@@ -15,9 +15,9 @@ parser.add_option('--tag', type=str, default='1.9',
                   help='tag version of the throughputs [%default]')
 parser.add_option('--airmass', type=float, default=1.2,
                   help='airmass value [%default]')
-parser.add_option('--aerosol', type=float, default=0.0,
+parser.add_option('--aerosol', type=float, default=0.04,
                   help='aerosol value [%default]')
-parser.add_option('--pwv', type=float, default=4.0,
+parser.add_option('--pwv', type=float, default=5.0,
                   help='precipitable water vapor value [%default]')
 parser.add_option('--ozone', type=float, default=300.,
                   help='ozone value [%default]')
@@ -27,6 +27,9 @@ parser.add_option('--pressure', type=float, default=743.,
                   help='pressure on the Cerro Pachon [%default]')
 parser.add_option('--atmos_type', type=str, default='obsatmo',
                   help='how is the atmos estimated (obsatmo, from_file) [%default]')
+parser.add_option('--fwhmeff', type=str, default='0.92,0.87,0.83,0.80,0.78,0.76',
+                  help='FWHMeff values for ugrizy bands [%default]')
+
 opts, args = parser.parse_args()
 
 # config = dict(zip(['tag','label'],[['1.5','1.9'],['Al_Ag_Al','Ag_Ag_Ag']]))
@@ -42,6 +45,8 @@ pwv = opts.pwv
 ozone = opts.ozone
 pressure = opts.pressure
 atmos_type = opts.atmos_type
+fwhmeff = opts.fwhmeff.split(',')
+fwhmeff = list(map(float, fwhmeff))
 
 telb = '{}_{}'.format(tel_dir, tag)
 through_dir = '{}/{}'.format(telb, throughputsDir)
@@ -60,11 +65,14 @@ bands = 'ugrizy'
 exptime = 30
 nexp = 1
 plateScale = 0.2  # pixel size ''
+bands = 'ugrizy'
+
+telescope.data['FWHMeff'] = dict(
+    zip('ugrizy', fwhmeff))
 
 telescope.etc(exptime, plateScale, nexp)
 
 
-bands = 'ugrizy'
 m5_from_file = [23.697, 24.973, 24.516, 24.127, 23.556, 22.550]
 dict_m5_ref = dict(zip(bands, m5_from_file))
 
