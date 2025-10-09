879cc05ee02ea2535c37ab2dad0ff0778e625ce7
pgris <gris@clermont.in2p3.fr>
2025-10-09 10:37:20 +0200

    add the sum(nsn) for display

--- plot_scripts/sn/sn_analyzer_sel_wfd.py
+++ plot_scripts/sn/sn_analyzer_sel_wfd.py
@@ -10,9 +10,108 @@ from optparse import OptionParser
 import pandas as pd
 from sn_analysis import plt
 from sn_tools.sn_io import checkDir
-from sn_tools.sn_utils import get_val
+from sn_tools.sn_utils import get_val, multiproc
 import os
 from sn_plotter_analysis.sn_analyser_tools import print_nsn_latex
+
+
+def cumul_multiproc(df):
+    """
+    Estimate sum(nsn,nsn_cosmo) per pixel
+
+    Parameters
+    ----------
+    df : pandas df
+        Data to process.
+
+    Returns
+    -------
+    res : pandas df
+        result.
+
+    """
+
+    hpixes = df['healpixID'].unique()
+
+    params = {}
+
+    params['data'] = df
+    params['timescale'] = timescale
+
+    res = multiproc(hpixes, params, cumul_set_pixels, nproc=8)
+
+    return res
+
+
+def cumul_set_pixels(hpixes, params, j, output_q=None):
+    """
+    Process to estimate sum(nsn) for a set of pixels
+
+    Parameters
+    ----------
+    hpixes : list(int)
+        list of pixels to consider.
+    params : dict
+        parameters for the processing.
+    j : int
+        internal tag for multiprocessing.
+    output_q : multiprocessing queue, optional
+        where to pu the result. The default is None.
+
+    Returns
+    -------
+    pandas df
+        output data.
+
+    """
+
+    timescale = params['timescale']
+    data = params['data']
+
+    idx = data['healpixID'].isin(hpixes)
+    data = pd.DataFrame(data[idx])
+    res = data.groupby(['dbName', 'healpixID']).apply(
+        lambda x: cumul_pixel(x, timescale=timescale), include_groups=False).reset_index()
+
+    if output_q is not None:
+        return output_q.put({j: res})
+    else:
+        return res
+
+
+def cumul_pixel(grp, cols=['nsn', 'nsn_cosmo'], timescale='year'):
+    """
+    Functio to estimate sum(nsn) for a pixel
+
+    Parameters
+    ----------
+    grp : pandas df
+        Data to process.
+    cols : list(str), optional
+        df columns to consider. The default is ['nsn', 'nsn_cosmo'].
+    timescale : str, optional
+        Timescale for the processing (year/season). The default is 'year'.
+
+    Returns
+    -------
+    df : pandas df
+        output data.
+
+    """
+
+    df = pd.DataFrame()
+    for i in range(1, 11):
+        idx = grp[timescale] <= i
+        sel = grp[idx]
+        dd = dict(zip(cols, [0, 0]))
+        if len(sel) > 0:
+            for cc in cols:
+                dd[cc] = [sel[cc].sum()]
+        dd[timescale] = [i]
+        dfa = pd.DataFrame.from_dict(dd)
+        df = pd.concat((df, dfa))
+
+    return df
 
 
 parser = OptionParser(description='Script to analyze SN prod after selection')
@@ -50,6 +149,9 @@ parser.add_option('--OS_ref', type=str,
 parser.add_option('--print_nsn', type=int,
                   default=0,
                   help='to print nsn as a latex table [%default]')
+parser.add_option('--cumul', type=int,
+                  default=0,
+                  help='to plot sum(nsn) [%default]')
 
 opts, args = parser.parse_args()
 
@@ -65,11 +167,14 @@ nside = opts.nside
 vartoplot = opts.vartoplot
 OS_ref = opts.OS_ref
 print_nsn = opts.print_nsn
+cumul = opts.cumul
 
 # read config file
 conf = pd.read_csv(config, comment='#')
 
 dbNames = conf['dbName'].unique()
+
+dict_leg = dict(zip(['nsn', 'nsn_cosmo'], ['N$_{SN}$', 'N$_{SN}^{COSMO}$']))
 
 wfd = pd.DataFrame()
 for dbName in dbNames:
@@ -102,8 +207,15 @@ if len(res) > 0:
     for_ffmpeg = False
     if 'mollweid_ffmpeg' in res:
         for_ffmpeg = True
+
+    pp = dict_leg[vartoplot]
+    pp = '{}='.format(pp)
+    if cumul:
+        wfd = cumul_multiproc(wfd)
+        pp = '$\Sigma$'+pp
     plot_mollview_wfd(wfd, timescale, timeslots, nside,
-                      varp=vartoplot, outDir=outDir, for_ffmpeg=for_ffmpeg)
+                      varp=vartoplot, varleg=pp,
+                      outDir=outDir, for_ffmpeg=for_ffmpeg)
 
 if 'density' in plots:
     from sn_plotter_analysis.sn_analyser_wdf import plot_density_wfd
