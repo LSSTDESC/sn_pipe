a03107f316f1b8a07ed5569001f4bad5b0b5967f
pgris <gris@clermont.in2p3.fr>
2025-10-27 10:08:54 +0100

    update

--- plot_scripts/os_info/plot_radius_dd_pixels.py
+++ plot_scripts/os_info/plot_radius_dd_pixels.py
@@ -11,9 +11,24 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 from sn_plotter_metrics.utils import get_dist
+from scipy.interpolate import interp1d
 
 
 def load_db(dbDir):
+    """
+    Function to load db data
+
+    Parameters
+    ----------
+    dbDir : str
+        OS location dir.
+
+    Returns
+    -------
+    df : pandas df
+        loaded data.
+
+    """
 
     search_path = '{}/*.hdf5'.format(dbDir)
     print('path', search_path)
@@ -29,6 +44,24 @@ def load_db(dbDir):
 
 
 def rebin(df_dist, distval='dist_center', yvar='nvisits'):
+    """
+    Function used to rebin data
+
+    Parameters
+    ----------
+    df_dist : pandas df
+        Data to rebin.
+    distval : str, optional
+        x-axis value. The default is 'dist_center'.
+    yvar : str, optional
+        y-axis value. The default is 'nvisits'.
+
+    Returns
+    -------
+    dd : pandas df
+        Rebinned data.
+
+    """
 
     # rebin to have a "better" plot
     xmin, xmax = df_dist[distval].min(), df_dist[distval].max()
@@ -45,21 +78,74 @@ def rebin(df_dist, distval='dist_center', yvar='nvisits'):
     return dd
 
 
-def get_radius(grp, distval='dist_center', yvar='nvisits'):
+def get_radius(grp, distval='dist_center', yvar=['nvisits', 'cadence']):
+    """
+    Function to estimate nvisits and cadence vs radius
+
+    Parameters
+    ----------
+    grp : pandas df
+        Data to process.
+    distval : str, optional
+        distance to use. The default is 'dist_center'.
+    yvar : str, optional
+        columns to estimate. The default is ['nvisits','cadence'].
+
+    Returns
+    -------
+    ddc : pandas df
+        output data.
+
+    """
+
+    # print('processing', grp.name, grp)
 
     df_dist = get_dist(grp)
 
-    dda = rebin(df_dist)
-    ddb = rebin(df_dist, yvar='cadence')
+    idx = df_dist['cadence'] > 0
+    df_dist = df_dist[idx]
+    """
+    fig, ax = plt.subplots()
+    ax.plot(df_dist[distval], df_dist['cadence'])
+    plt.show()
+    """
+    dd = {}
+    for i, val in enumerate(yvar):
+        dd[i] = rebin(df_dist, yvar=val)
+
+    ddc = dd[0].merge(dd[1], left_on=[distval], right_on=[
+        distval], suffixes=['', ''])
+    return ddc
+
+    """
+    idx = ddb['cadence'] > 0.
+    ddb = ddb[idx]
+
+    idb = ddb[distval] <= 0.7  # selection at 0.7 deg
+    mean_cad = ddb[idb]['cadence'].mean()
 
     print(df_dist)
     fig, ax = plt.subplots(nrows=2)
     tt = np.array(np.cumsum(dda[yvar]))
     # nt = np.cumsum(dd[yvar])[-1]
     ax[0].plot(dda[distval], tt/tt[-1], color='k', marker='o')
-    ax[1].plot(ddb[distval], ddb['cadence'], color='k', marker='o')
+    ax[1].plot(ddb[distval], ddb['cadence']-mean_cad, color='k', marker='o')
+
+    vv = tt/tt[-1]
+    interp_nvisits = interp1d(
+        dda[distval], vv, bounds_error=False, fill_value=0)
+    interp_cadence = interp1d(
+        ddb[distval], ddb['cadence']-mean_cad, bounds_error=False, fill_value=0)
+
+    ddist_max = np.max(df_dist['dist_center'])
+    ddist = np.arange(0., ddist_max, 0.01)
+
+    fig, ax = plt.subplots()
+    ax.plot(interp_cadence(ddist), interp_nvisits(ddist),  'ko')
+
     plt.show()
-    print(test)
+
+    """
 
 
 parser = OptionParser(description='Script to plot pixel level OS infos')
@@ -77,12 +163,18 @@ dbDir = opts.dbDir
 # load dbList
 df_db = pd.read_csv(dbList, comment='#')
 
+df_tot = pd.DataFrame()
 for i, row in df_db.iterrows():
     dbName = row['dbName']
     dbName_dir = '{}/{}'.format(dbDir, dbName)
     data = load_db(dbName_dir)
-    print(data)
+    # print(data)
     idx = data['year'] >= 0
+    idx &= data['year'] <= 10
     data = data[idx]
     dd = data.groupby(['field', 'year']).apply(
         lambda x: get_radius(x), include_groups=False).reset_index()
+    dd['dbName'] = dbName
+    df_tot = pd.concat((df_tot, dd))
+
+df_tot.to_hdf('data_radius.hdf5', key='radius')
