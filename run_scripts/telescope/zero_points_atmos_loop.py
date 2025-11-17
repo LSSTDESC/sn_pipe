89420968ccb8db1f88fbcd2ac011da0fabb7f67d
pgris <gris@clermont.in2p3.fr>
2025-11-17 16:10:40 +0100

    add some doc

--- run_scripts/telescope/zero_points_atmos_loop.py
+++ run_scripts/telescope/zero_points_atmos_loop.py
@@ -13,6 +13,7 @@ import time
 from sn_telmodel.sn_throughtools import Sigma_zp_meanwave
 from optparse import OptionParser
 from sn_tools.sn_io import make_dict_from_config, add_parser
+from sn_tools.sn_io import checkDir
 
 
 def get_combi(dict_mean, dict_sigma, parList):
@@ -21,8 +22,10 @@ def get_combi(dict_mean, dict_sigma, parList):
 
     Parameters
     ----------
-    thedict : dict
-        input dict with parameters.
+    dict_mean : dict
+        input dict with mean parameters.
+    dict_sigma : dict
+        input dict with sigma parameters.
     parList : str
         parameter list.
 
@@ -44,6 +47,22 @@ def get_combi(dict_mean, dict_sigma, parList):
 
 
 def get_df(theDict, parList):
+    """
+    Build df from dict
+
+    Parameters
+    ----------
+    theDict : dict
+        parameter dict.
+    parList : str
+        parameter list.
+
+    Returns
+    -------
+    df : pandas df
+        output data.
+
+    """
 
     df = pd.DataFrame()
     for vv in parList:
@@ -68,6 +87,9 @@ add_parser(parser, confDict)
 opts, args = parser.parse_args()
 
 params = vars(opts)
+
+# create outDir
+checkDir(params['outDir'])
 
 time_ref = time.time()
 tel_dir = params['telDir']
@@ -111,14 +133,8 @@ for vv in par_names:
                       params[sigma_step]))
     dict_sigma[vv] = sig
 
-print(dict_mean)
-print(dict_sigma)
-
 
 combi_sigma = get_combi(dict_mean, dict_sigma, par_names)
-
-print(combi_sigma)
-
 
 df = pd.DataFrame()
 
@@ -126,11 +142,15 @@ print('nb de combinaisons', len(combi_sigma), combi_sigma.columns)
 time_ref = time.time()
 for i, row in combi_sigma.iterrows():
 
+    time_ref_b = time.time()
     par_means = row[par_names].to_list()
     colsb = list(map(lambda x: 'sigma_' + x, par_names))
     par_sigmas = row[colsb].to_list()
+    """
+    print(par_names)
     print(par_means)
     print(par_sigmas)
+    """
     sigma_zp = Sigma_zp_meanwave(through_dir, site_name, pressure,
                                  par_names, par_means, par_sigmas,
                                  save_throughputs_dir='ty_through')
@@ -139,10 +159,15 @@ for i, row in combi_sigma.iterrows():
 
     df = pd.concat((df, res))
 
+    print('combi', time.time()-time_ref_b)
 print('finally', time.time()-time_ref)
 print('end of processing', time.time()-time_ref)
 
 print(df[['std_zp_y', 'std_zp_z', 'std_mean_wave_y', 'std_mean_wave_z']])
+
+outName = '{}/{}'.format(params['outDir'], params['outName'])
+
+df.to_hdf(outName, key='zp_atmos')
 
 """
 for b in bands:
