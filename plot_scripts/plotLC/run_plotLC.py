bfb4fea2e41e4bd20b125a1f3d64de6be47739ae
pgris <gris@clermont.in2p3.fr>
2025-12-04 16:50:43 +0100

    add the coaddition for LC fits

--- plot_scripts/plotLC/run_plotLC.py
+++ plot_scripts/plotLC/run_plotLC.py
@@ -19,6 +19,9 @@ parser.add_option('--tag_tel', type=str, default='1.9',
                   help='tag for telescope version [%default]')
 parser.add_option('--remove_sat', type=int, default=0,
                   help='to remove saturated LC points [%default]')
+parser.add_option('--fit_coadd', type=int, default=0,
+                  help='to perform coadd  [%default]')
+
 opts, args = parser.parse_args()
 
 opts_dict = vars(opts)
