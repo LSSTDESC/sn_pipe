6ffc1b12097c9aed26e4bb1b8aed15c0cc01f2e4
pgris <gris@clermont.in2p3.fr>
2026-01-05 16:37:13 +0100

    add mem as input script param

--- for_batch/scripts/sim_to_fit/prodIt.py
+++ for_batch/scripts/sim_to_fit/prodIt.py
@@ -56,6 +56,8 @@ parser.add_option("--InstrumentSimu_ntrial_zp", type=int,
 parser.add_option("--tag_script", type=str,
                   default='no_tag',
                   help="script name tag [%default]")
+parser.add_option("--mem", type=str, default='8Gb',
+                  help="memory for batch jobs [%default]")
 
 opts, args = parser.parse_args()
 
@@ -80,7 +82,7 @@ script = 'python for_batch/scripts/sn_prod/prod_sn_dd_wfd.py'
 
 cct = ['SN_smearFlux', 'Fitter_sigmaz',
        'Observations_coadd', 'saturation_effect',
-       'InstrumentSimu_ntrial_zp', 'tag_script']
+       'InstrumentSimu_ntrial_zp', 'tag_script', 'mem']
 for vv in ['airmass', 'pwv', 'ozone', 'aerosol']:
     cct.append('sigma_{}'.format(vv))
 
