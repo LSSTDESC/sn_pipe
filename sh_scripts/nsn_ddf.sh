9eef230e7fb99327792b7ffb9f784220a6041e26
pgris <gris@clermont.in2p3.fr>
2026-01-21 11:13:20 +0100

    add a script to illustrate how to estimate nsn for DDF

---
+++ sh_scripts/nsn_ddf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run_scripts/sn_analysis/nsn_ddf.py --dbDir=/sps/lsst/users/gris/Output_SN_DD_sigmaInt_0.0_Hounsell_z_smflux_notelrot_airmass_G10_JLA --dbList=list_OS_new_ddf.csv --outDir=/sps/lsst/users/gris/sn_dd_airmass
