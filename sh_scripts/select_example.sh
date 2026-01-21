90b13d2643135dd359ec74e00d1e7ac5cb477f3c
pgris <gris@clermont.in2p3.fr>
2026-01-21 14:40:06 +0100

    bug correction

--- sh_scripts/select_example.sh
+++ sh_scripts/select_example.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python for_batch/scripts/select/select_sn.py --dataDir=/sps/lsst/groups/cadence/LSST_SN_PhG/prod_simu/Output_SN_DD_sigmaInt_0.0_Hounsell_z_smflux_notelrot_airmass_newatm_lc_coadd  --outDir_pre=/sps/lsst/users/gris/Output_SN_WFD_sigmaInt_0.0_Hounsell_z_smflux_notelrot_airmass_newatm_lc_coadd --dbList=list_OS_test_DDF.csv --scriptName=select_nsn.sh
+python for_batch/scripts/select/select_sn.py --dataDir=/sps/lsst/groups/cadence/LSST_SN_PhG/prod_simu/Output_SN_DD_sigmaInt_0.0_Hounsell_z_smflux_notelrot_airmass_newatm_lc_coadd  --outDir_pre=/sps/lsst/users/gris/Output_SN_DD_sigmaInt_0.0_Hounsell_z_smflux_notelrot_airmass_newatm_lc_coadd --dbList=list_OS_test_DDF.csv --scriptName=select_nsn.sh
 
-python for_batch/scripts/select/select_sn.py --dataDir=/sps/lsst/groups/cadence/LSST_SN_PhG/prod_simu/Output_SN_DD_sigmaInt_0.0_Hounsell_z_smflux_notelrot_airmass_obs_coadd  --outDir_pre=/sps/lsst/users/gris/Output_SN_WFD_sigmaInt_0.0_Hounsell_z_smflux_notelrot_airmass_obs_coadd --dbList=list_OS_test_DDF.csv --scriptName=select_nsn.sh
+python for_batch/scripts/select/select_sn.py --dataDir=/sps/lsst/groups/cadence/LSST_SN_PhG/prod_simu/Output_SN_DD_sigmaInt_0.0_Hounsell_z_smflux_notelrot_airmass_obs_coadd  --outDir_pre=/sps/lsst/users/gris/Output_SN_DD_sigmaInt_0.0_Hounsell_z_smflux_notelrot_airmass_obs_coadd --dbList=list_OS_test_DDF.csv --scriptName=select_nsn.sh
