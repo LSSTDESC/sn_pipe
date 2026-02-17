8b1c80610ea3c8d58f44a3aae34efc34c62e1afe
pgris <gris@clermont.in2p3.fr>
2026-02-17 16:29:20 +0100

    add LC_coadd as input parameter

--- for_batch/scripts/sim_to_fit/prodIt_x1_color_unique.py
+++ for_batch/scripts/sim_to_fit/prodIt_x1_color_unique.py
@@ -53,6 +53,9 @@ parser.add_option("--SN_x1_min", type=float,
 parser.add_option("--SN_color_min", type=float,
                   default=0.2,
                   help="color SN unique value [%default]")
+parser.add_option("--LC_coadd", type=int,
+                  default=0,
+                  help="coadd lc points [%default]")
 
 opts, args = parser.parse_args()
 
@@ -76,7 +79,7 @@ if 'WFD' in params['runType']:
 script = 'python for_batch/scripts/sn_prod/prod_sn_dd_wfd.py'
 
 cct = ['SN_smearFlux', 'Fitter_sigmaz',
-       'Observations_coadd', 'saturation_effect',
+       'Observations_coadd', 'saturation_effect','LC_coadd',
        'SN_z_min','SN_z_max','SN_z_step','SN_x1_min','SN_color_min']
 
 nsn_fact_str = 'SN_NSNabsolute_DD'
