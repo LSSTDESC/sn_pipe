73c1155a34a00d9c98c4e94c886554c3dd82b20d
pgris <gris@clermont.in2p3.fr>
2026-03-13 10:42:04 +0100

    add script for interactive session at cc

--- setup_conda_new.sh
+++
@@ -1,6 +0,0 @@
-source /usr/share/Modules/init/bash
-
-module load Programming_Languages/anaconda/3.8
-conda activate
-which anaconda
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pbs/software/redhat-9-x86_64/anaconda/3.8/lib
