53be0781bb582ef54d2f6cc65ebd15bf51a42aba
pgris <gris@clermont.in2p3.fr>
2026-03-13 16:06:41 +0100

    update

--- plot_scripts/sn/sn_flux.py
+++ plot_scripts/sn/sn_flux.py
@@ -8,10 +8,98 @@ Created on Thu Mar 12 15:08:10 2026
 
 from optparse import OptionParser
 import glob
+from sn_tools.sn_io import load_astro_table
+from sn_tools.sn_lcana import get_bands_vs_z
+import numpy as np
+from scipy.integrate import quad
+from scipy.interpolate import interp1d
+import pandas as pd
+
+def get_flux_sum_band(grp,exptime=30):
+    
+    phase_min = grp['phase'].min()
+    phase_max = grp['phase'].max()
+    
+    grp['flux_int'] = grp['flux']*exptime
+    def integrand(p):
+        ph = grp['phase'].tolist()
+        flux = grp['flux_int'].tolist()
+        myinterp = interp1d(ph,flux,bounds_error=False, fill_value=0.)
+        
+        return myinterp(p)
+    
+    res,abserr = quad(integrand, phase_min, phase_max)[:2]
+    dictout = {}
+    dictout['flux'] = [res]
+    dictout['fluxerr'] = [abserr]
+
+    df = pd.DataFrame.from_dict(dictout)
+
+    return df
+    
+
+
+def get_flux_sum(tab,z,exptime=30.):
+    
+    #grab the bands of interest
+    bands = get_bands_vs_z(z)
+    
+    idx = np.in1d(tab['filter_notel'],list(bands))
+    idx &= tab['flux'] > 0
+    
+    sel = tab[idx]
+    sel['flux_int'] = sel['flux']*exptime
+    
+    r = []
+    for b in bands:
+        idx = sel['filter_notel'] == b
+        grp = sel[idx]
+        dphase = np.mean(np.diff(grp['phase']))
+        print(b, dphase,np.sum(grp['flux_int']*dphase))
+        phase_min = grp['phase'].min()
+        phase_max = grp['phase'].max()
+        
+        def integrand(p):
+            ph = grp['phase'].tolist()
+            flux = grp['flux_int'].tolist()
+            myinterp = interp1d(ph,flux,bounds_error=False, fill_value=0.)
+            
+            return myinterp(p)
+        res,abserr = quad(integrand, phase_min, phase_max)[:2]
+        print(b,res,abserr)
+        r.append([b,res,abserr,z])
+
+    df = pd.DataFrame(r, columns=['band','flux','fluxerr'])
+    return df
+
+def process_data(z,fis):
+    
+    dft = pd.DataFrame()
+    colby = 'filter_notel'
+    for fi in fis:
+        print('loading',fi)
+        zval = fi.split('/')[-1].split('_')[3]
+        if zval == str(z):
+            tab = load_astro_table(fi)
+            print(tab.meta)
+            df = tab.to_pandas()
+            bands = get_bands_vs_z(z)
+            idx = df['filter_notel'].isin(list(bands))
+            
+            dfr = df[idx].groupby(colby).apply(lambda x: get_flux_sum_band(x),
+                                           include_groups=False).reset_index()
+            dfr['airmass'] = tab.meta['airmass']
+            dfr['z'] = tab.meta['z']
+            dfr['x1'] = tab.meta['x1']
+            dfr['color'] = tab.meta['color']
+            dfr['daymax'] = tab.meta['daymax']
+            dft = pd.concat((dft,dfr))
+            
+    return dft
 
 parser = OptionParser(description='analyze and plot of LC flux files')
 
-parser.add_option('--fluxDir', type=str, default='../sn_flux_z',
+parser.add_option('--fluxDir', type=str, default='../sn_flux_z_airmass',
                   help='data dir [%default]')
 
 opts, args = parser.parse_args()
@@ -20,9 +108,17 @@ fluxDir = opts.fluxDir
 
 fis = glob.glob('{}/*.hdf5'.format(fluxDir))
 
-print(fis)
-
 #loop on files and grab tables
+z = np.arange(0.0,1.2,0.1)
 
-for fi in fis:
-    print('loading',fi)
+df = pd.DataFrame()
+
+for zv in z:
+    if zv < 0.01:
+        zv = 0.01
+    vv = np.round(zv,2)
+    dfa = process_data(vv,fis)
+    df = pd.concat((df,dfa))
+        
+print(df)
+    
--- run_scripts/simulation/run_loop_sudy_snflux.py
+++ run_scripts/simulation/run_loop_sudy_snflux.py
@@ -20,15 +20,15 @@ outDir = '../sn_flux_z_airmass'
 script += ' --outDir={}'.format(outDir)
 
 z = np.arange(0.0,1.2,0.1)
-airmass = np.arange(1.0,3.,0.1)
-nfi = 0
+airmass = np.arange(1.0,2.6,0.1)
+
 for i, vv in enumerate(z):
     if vv < 0.01:
         vv = 0.01
     vv = np.round(vv,2)
     for airm in airmass:
-        nfi +=1
-        outName = 'simu_{}'.format(nfi)
+        airm = np.round(airm,1)
+        outName = 'simu_{}_{}'.format(vv,airm)
         scr_ = script
         scr_ += ' --z={}'.format(vv)
         scr_ += ' --outName={}'.format(outName)
