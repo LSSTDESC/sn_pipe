4d2a70de4a08b28a1f42e4f7c14476d813647b13
pgris <gris@clermont.in2p3.fr>
2026-04-16 17:21:20 +0200

    update

--- run_scripts/telescope/get_zp_wave.py
+++ run_scripts/telescope/get_zp_wave.py
@@ -11,8 +11,60 @@ from sn_analysis.sn_fit_tools import load_fit_atmos_data
 from sn_analysis.sn_atmos_tools import process_obs_data 
 from sn_analysis.sn_atmos_tools import merge_zp_wave
 import pandas as pd
+import matplotlib.pyplot as plt    
+
+def plot_results(df,obs_param='zp',unit='mmag'):
+
+    
+    bands = 'grizy'
+    indx = range(len(bands))
+    df_index = pd.DataFrame(list(bands),columns=['band'])
+    df_index['index'] = indx
+    
+    print(df_index)
+    
+    df = df.merge(df_index,left_on=['band'],right_on=['band'])
+    
+    df = df.sort_values(by=['index'])
     
     
+
+    fig, ax = plt.subplots(figsize=(12,8))
+    
+    configs = df['config'].unique()
+    airmass = df['airmass'].unique().tolist()
+    airmass = list(map(float, airmass))
+    
+    yvar= 'sigma_{}_tot'.format(obs_param)
+    
+    lstyles = dict(zip(airmass,['solid','dotted']))
+    markers = ['o','s','P','h','v']
+    
+    mmarks = dict(zip(configs,markers[:len(configs)]))
+    
+    for airm in airmass:
+        idx = df['airmass'] == airm
+        sel = df[idx]
+        for config in configs:
+            idxb = sel['config'] == config
+            selb = sel[idxb]
+            ax.plot(selb['band'],selb[yvar],
+                    marker=mmarks[config],mfc='None',
+                    linestyle=lstyles[airm])
+            
+    ax.grid(visible=True)
+    ax.set_xlabel(r'band')
+    ylabel = '$\sigma_{'+obs_param+'}$'+ '[{}]'.format(unit)
+    ax.set_ylabel(r'{}'.format(ylabel))
+    plt.show()
+            
+    
+    
+    
+
+
+    
+
 parser = OptionParser(description='Scan the atmos parameter sigma space')
                       
 parser.add_option('--dataDir', type=str, default='../zp_atmos',
@@ -45,3 +97,5 @@ combi_wave = process_obs_data(df_wave,sigma,atmos_params,do_combi=False)
 combi_tot = merge_zp_wave(combi_zp,combi_wave,atmos_params)
 
 print(combi_tot)
+
+plot_results(combi_tot)
