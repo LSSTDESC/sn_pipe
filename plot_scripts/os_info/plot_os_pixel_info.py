27d2d8e113e7e838d55b6e6cccbbfa6e53ff1e5a
pgris <gris@clermont.in2p3.fr>
2026-06-10 11:05:32 +0200

    update

--- plot_scripts/os_info/plot_os_pixel_info.py
+++ plot_scripts/os_info/plot_os_pixel_info.py
@@ -177,7 +177,121 @@ def load_data(dbDir, dbName, fields, fieldType='DD'):
 
     return df
 
+def plot_histos_deprecated(hist_var,df,seasons):
+    """
+    Function to plot histograms of data
 
+    Parameters
+    ----------
+    hist_var : list(str)
+        List of variables to plot.
+    df : pandas df
+        Data to plot.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    dbNames = df['dbName'].unique().tolist()
+    ccols = ['r','k','b']
+    lls = ['solid','dashed','dotted']
+    colors = dict(zip(dbNames,ccols[0:len(dbNames)]))
+    ls = dict(zip(dbNames,lls[0:len(dbNames)]))
+    
+    for vv in hist_var:
+        vary = ''
+        if seasons=='10yrs':
+            fig, ax = plt.subplots(figsize=(12, 8))
+        
+        for dbName in dbNames:
+            idx = df['dbName'] == dbName
+            sel = df[idx]
+            
+            if seasons == '10yrs':
+                bb = sel.groupby(['healpixID'])[vv].sum().reset_index()
+                idx = bb['nvisits'] < 1100.
+                ax.hist(bb[idx][vv], histtype='step', 
+                        bins=50,label=dbName,
+                        linestyle=ls[dbName],color=colors[dbName])
+            if vary == timescale:
+                for year in sel[vary].unique():
+                    idxb = sel[vary] == year
+                    if varx == 'cadence':
+                        idxb &= sel[varx] < 25.
+                        selb = sel[idxb]
+                        ax.hist(selb[vv], histtype='step', bins=50)
+
+        ax.set_xlabel(r'{}'.format(dict_leg[vv]))
+        ax.set_ylabel(r'Number of entries')
+        ax.grid(visible=True)
+        ax.set_xlim([0., None])
+        ax.legend(fontsize=15,frameon=False)
+        
+def plot_histos(hist_var,df,seasons,timescale='year'):
+    """
+    Function to plot histograms of data
+
+    Parameters
+    ----------
+    hist_var : list(str)
+        List of variables to plot.
+    df : pandas df
+        Data to plot.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    if seasons == '10yrs':
+        df = df.groupby(['healpixID','dbName'])[hist_var].sum().reset_index()
+    
+    for vv in hist_var:
+        if seasons == '10yrs':
+            plot_histos_db(df,vv,figtit='10 years')
+        else:
+            for seas in seasons:
+                idx = df[timescale] == seas
+                sel = df[idx]
+                plot_histos_db(sel,vv,figtit='year {}'.format(seas))
+                
+        
+        
+        
+def plot_histos_db(df,varx,fig=None,ax=None,figtit=''):
+    
+    ccols = ['r','k','b']
+    lls = ['solid','dashed','dotted']
+    
+    dbNames = df['dbName'].unique().tolist()
+    colors = dict(zip(dbNames,ccols[0:len(dbNames)]))
+    ls = dict(zip(dbNames,lls[0:len(dbNames)]))
+    
+    if fig is None:
+        fig, ax = plt.subplots(figsize=(12, 8))
+
+    if figtit != '':
+        fig.suptitle(figtit)
+        
+    for dbName in dbNames:
+           idx = df['dbName'] == dbName
+           sel = df[idx]
+           ax.hist(sel[varx], histtype='step', 
+                   bins=50,label=dbName,
+                   linestyle=ls[dbName],color=colors[dbName])
+    
+    
+    ax.set_xlabel(r'{}'.format(dict_leg[varx]))
+    ax.set_ylabel(r'Number of entries')
+    ax.grid(visible=True)
+    ax.set_xlim([0., None])
+    ax.legend(fontsize=15,frameon=False)
+    
+    
+    
 parser = OptionParser(description='Script to plot pixel level OS infos')
 
 parser.add_option('--dbName', type=str, default='test_newb',
@@ -189,9 +303,9 @@ parser.add_option('--nside', type=int, default=128,
 parser.add_option('--plots', type=str,
                   default='gen_plots,mollview',
                   help='plots to show [%default]')
-parser.add_option('--mollview_seasons', type=str,
+parser.add_option('--seasons', type=str,
                   default='1-5',
-                  help='plots to show [%default]')
+                  help='seasons to show [%default]')
 parser.add_option('--fields', type=str,
                   default='COSMOS,XMM-LSS,CDFS,ELAISS1,EDFS_a,EDFS_b',
                   help='fields to show [%default]')
@@ -217,32 +331,46 @@ parser.add_option('--mollview_outDir', type=str,
 opts, args = parser.parse_args()
 
 dbDir = opts.dbDir
-dbName = opts.dbName
+dbNames = opts.dbName.split(',')
 nside = opts.nside
 plots = opts.plots.split(',')
-mollview_seasons = opts.mollview_seasons
 mollview_var = opts.mollview_var.split(',')
 gen_var = opts.gen_var.split(',')
+seasons = opts.seasons
 hist_var = opts.hist_var.split(',')
 fields = opts.fields.split(',')
 fieldType = opts.fieldType
 timescale = opts.timescale
 mollview_outDir = opts.mollview_outDir
 
+"""
 if '-' in mollview_seasons:
     cad_brk = mollview_seasons.split('-')
     moll_seasons = list(range(int(cad_brk[0]), int(cad_brk[1])+1))
 else:
     moll_seasons = list(map(int, mollview_seasons.split(',')))
-
-print('seasons moll', moll_seasons)
+"""
+if '-' in seasons:
+    cad_brk = seasons.split('-')
+    seasons = list(range(int(cad_brk[0]), int(cad_brk[1])+1))
+else:
+    if ',' in seasons:
+        seasons = list(map(int, seasons.split(',')))
+    else:
+        if seasons != '10yrs':
+            seasons = [int(seasons)]
+print('seasons moll', seasons)
 
 # fName = '{}/{}.hdf5'.format(dbDir, dbName)
 
-df = load_data(dbDir, dbName, fields, fieldType=fieldType)
+df = pd.DataFrame()
+for dbName in dbNames:
+    df_ = load_data(dbDir, dbName, fields, fieldType=fieldType)
+    df_['dbName'] = dbName
+    df = pd.concat((df,df_))
 
 # df = pd.read_hdf(fName)
-df['dbName'] = dbName
+#df['dbName'] = dbName
 print(df.columns)
 # print(test)
 
@@ -256,7 +384,7 @@ idx &= df['cadence'] > 0.
 sel = df[idx]
 
 vvar = ['cadence', 'nvisits', 'm5_i']
-legvar = ['cadence [day]', 'N$_{visits}$', '$m_{5}^{i}$']
+legvar = ['cadence [day]', 'N$_{visits}$/pixel', '$m_{5}^{i}$']
 dict_leg = dict(zip(vvar, legvar))
 
 if 'gen_plots' in plots:
@@ -271,12 +399,37 @@ if 'gen_plots' in plots:
                            yleg=r'{}'.format(dict_leg[vary]), timescale=timescale)
 
 if 'mollview' in plots:
+    dbNames = sel['dbName'].unique()
     for vv in mollview_var:
-        plotMollview_seasons(nside, sel, dbName,
-                             yvar=vv, yleg=dict_leg[vv],
-                             op=np.mean, seasons=moll_seasons,outDir=mollview_outDir)
+        for dbName in dbNames:
+            idx = sel['dbName'] == dbName
+            selb=sel[idx]
+            plotMollview_seasons(nside, selb, dbName,
+                                 yvar=vv, yleg=dict_leg[vv],
+                                 op=np.mean, seasons=seasons,outDir=mollview_outDir)
+        ## add the diff
+        if len(dbNames) ==2:
+            dd = {}
+            for i,dbName in enumerate(dbNames):
+                idxa = df['dbName'] == dbName
+                dd[i] = pd.DataFrame(df[idxa])
+                
+            ddb = dd[0].merge(dd[1],left_on=['healpixID',timescale],
+                              right_on=['healpixID',timescale])
+            newvar = 'delta_{}'.format(vv)
+            ddb[newvar] = ddb['{}_x'.format(vv)]-ddb['{}_y'.format(vv)]
+            dbNa = ddb['dbName_x'].unique()[0]
+            dbNb = ddb['dbName_y'].unique()[0]
+            dbName = '{}-{}'.format(dbNa,dbNb)
+            plotMollview_seasons(nside, ddb, dbName,
+                                 yvar=newvar, yleg='$\Delta$ '+dict_leg[vv],
+                                 op=np.mean, seasons=seasons,outDir=mollview_outDir)
+            
+            
 
 if 'hist' in plots:
+    plot_histos(hist_var,sel,seasons)
+    """
     print(sel.columns)
     for vv in hist_var:
         varx = vv.split('_')[0]
@@ -298,7 +451,7 @@ if 'hist' in plots:
         ax.set_ylabel(r'Number of entries')
         ax.grid(visible=True)
         ax.set_xlim([0., None])
-
+    """
 """
 print_pixel_info(sel, 109384)
 print_pixel_info(sel, 109031)
