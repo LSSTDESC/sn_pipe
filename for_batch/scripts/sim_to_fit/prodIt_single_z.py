c095813a29871f9bd14e29526dc20c61e82b0d17
pgris <gris@clermont.in2p3.fr>
2026-08-13 14:54:58 +0200

    add conf atmos in outDir

--- for_batch/scripts/sim_to_fit/prodIt_single_z.py
+++ for_batch/scripts/sim_to_fit/prodIt_single_z.py
@@ -136,12 +136,13 @@ def process_from_simu(pp,dict_atmos):
     obs_coadd = pp['obs_coadd']
     lc_coadd = pp['lc_coadd']
     dbName = pp['dbName']
+    conf = pp['conf']
     
     z = float(simDir.split('/')[-1].split('_')[-1])
     z = np.round(z,2)
     pp['zmin'] = z
-    pp['outDir'] = '{}/z_{}_{}_{}'.format(pp['outDir'],z,obs_coadd,lc_coadd)
-    procName='sn_z_single_from_simu_{}_{}_{}_{}'.format(z,
+    pp['outDir'] = '{}/{}_z_{}_{}_{}'.format(conf,pp['outDir'],z,obs_coadd,lc_coadd)
+    procName='sn_z_single_from_simu_{}{}_{}_{}_{}'.format(conf,z,
                                                         config,
                                                         obs_coadd,lc_coadd)
 
