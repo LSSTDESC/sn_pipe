81643e902b5d9808bb6d12ecc64986c40b4e7686
pgris <gris@clermont.in2p3.fr>
2026-08-24 15:49:39 +0200

    update to account for new conda version

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 
 source /pbs/throng/lsst/users/gris/anaconda3/etc/profile.d/conda.sh
-conda activate myenv
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pbs/throng/lsst/users/gris/anaconda3/envs/myenv/lib
+conda activate
+#conda activate myenv
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pbs/throng/lsst/users/gris/anaconda3/envs/myenv/lib
