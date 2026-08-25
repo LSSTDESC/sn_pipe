cc3974914c252c45efee328b591a793f4b4f0130
pgris <gris@clermont.in2p3.fr>
2026-08-25 16:24:07 +0200

    update

--- plot_scripts/simulation/compare_sn_lc.py
+++ plot_scripts/simulation/compare_sn_lc.py
@@ -25,6 +25,10 @@ dira = '../test_LC_confe_coadd_bef_smearing_grizy/baseline_v5.3.0_10yrs/DDF_spec
 #dirb = '../test_LC_confd_coadd_before_smearing/baseline_v5.3.0_10yrs/DDF_spectroz/'
 dirb = '../test_LC_confe_obscoadd_grizy/baseline_v5.3.0_10yrs/DDF_spectroz/'
 #dirb = '../test_LC_confe_coadd_after_smearing/baseline_v5.3.0_10yrs/DDF_spectroz/'
+
+dira = '../prod_single/confe_z_0.81_1_0/baseline_v5.3.0_10yrs/DDF_spectroz'
+dirb = '../prod_single/confe_z_0.81_0_1/baseline_v5.3.0_10yrs/DDF_spectroz'
+
 snFile_a = 'SN_SN_DD_baseline_v5.3.0_10yrs_-2.0_0.2_1.hdf5'
 snFile_b = 'SN_SN_DD_baseline_v5.3.0_10yrs_-2.0_0.2_1.hdf5'
 
