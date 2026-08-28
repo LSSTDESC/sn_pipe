b70d75bee9fa29a1481e41007023138f21f8d78b
pgris <gris@clermont.in2p3.fr>
2026-08-28 11:03:23 +0200

    add OS and run type as params

--- plot_scripts/simulation/compare_sn_lc.py
+++ plot_scripts/simulation/compare_sn_lc.py
@@ -16,11 +16,18 @@ parser.add_option('--run_mode', type=str, default='sn_lc',
 parser.add_option('--master_dirs', type=str, 
                   default='../prod_single/confe_z_0.81_1_0,../prod_single/confe_z_0.81_0_1',
                   help='master dirs for data [%default]')
+parser.add_option('--dbName', type=str, default='baseline_v5.3.0_10yrs',
+                  help='OS to process [%default]')
+parser.add_option('--runType', type=str, default='DDF_spectroz',
+                  help='run type [%default]')
+
 
 opts, args = parser.parse_args()
 
 run_mode = opts.run_mode
 master_dirs =opts.master_dirs.split(',')
+dbName = opts.dbName
+runType = opts.runType
 
 dira = '../test_LC_confe_nocoadd/baseline_v5.3.0_10yrs/DDF_spectroz/'
 dirb = '../test_LC_confd_nocoadd/baseline_v5.3.0_10yrs/DDF_spectroz/'
@@ -35,9 +42,6 @@ master_b = master_dirs[1]
 
 #master_a = '../test_new_1_0'
 #master_a = '../test_new_0_1'
-
-dbName = 'baseline_v5.3.0_10yrs'
-runType = 'DDF_spectroz'
 
 dira = '{}/{}/{}'.format(master_a,dbName,runType)
 dirb = '{}/{}/{}'.format(master_b,dbName,runType)
